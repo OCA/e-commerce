f11f3cac7a8688790b0c2d9e37db393cedd4dc55
Raphaël Valyi <rvalyi@rvalyi-laptop>
2009-10-05 05:09:20 -0300

    [ADD] base_sale_multichannels: new generic module for ecommerce/POS and other externale sale platforms. Depends on the new base_external_referentials module for generic mapping with external systems. A first concrete example that would use it is tha new multi-instance/clean Magento connector from Akretion.com and Openlabs.co.in (Sharoon Thomas). But others like Spree connetor at least should follow using the same genric and compatible infrastructure

---
+++ base_sale_multichannels/__terp__.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2009 Akretion (<http://www.akretion.com>). All Rights Reserved
+#    authors: Raphaël Valyi, Sharoon Thomas
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Base Sale MultiChannels',
+    'version': '1.0',
+    'category': 'Generic Modules/Sale',
+    'description': """
+This module provide an abstract common minimal base to multi-channels sales.
+Say you want to expose your product catalog to
+* several instances of flashy-sluggish Magento web sites
+* a cutting edge Spree web shop
+* a Neteven online Marketplace
+* EBay
+* Amazon
+* Google Base
+* an external Point Of Sale system
+* ...
+Then this module allows you to:
+* use several external references ids on every OpenERP object matching those all those external referentials
+* per referential instance, use several sale sub platform entities (ex: several Magento websites per instance)
+* per sub platform, use several shops (ex: several Magento web shops per website)
+
+For each sale shop (matching OpenERP sale.shop object), this module abstract the interfaces to:
+* export the catalog, shop warehouse stock level wise, shop pricelist wise
+* import the catalog
+* import orders
+* export orders/picking status
+    """,
+    'author': 'Raphaël Valyi (Akretion.com), Sharoon Thomas (Openlabs.co.in)',
+    'website': 'http://www.akretion.com',
+    'depends': ['sale', 'base_external_referentials'],
+    'init_xml': [],
+    'update_xml': ['sale_view.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'certificate': '',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ base_sale_multichannels/sale.py
@@ -0,0 +1,132 @@
+# -*- encoding: utf-8 -*-
+#########################################################################
+#                                                                       #
+#########################################################################
+#                                                                       #
+# Copyright (C) 2009  Raphaël Valyi                                     #
+#                                                                       #
+#This program is free software: you can redistribute it and/or modify   #
+#it under the terms of the GNU General Public License as published by   #
+#the Free Software Foundation, either version 3 of the License, or      #
+#(at your option) any later version.                                    #
+#                                                                       #
+#This program is distributed in the hope that it will be useful,        #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#GNU General Public License for more details.                           #
+#                                                                       #
+#You should have received a copy of the GNU General Public License      #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################
+
+from osv import osv, fields
+from base_external_referentials import external_osv
+from sets import Set
+
+
+class referential_sub_entity(osv.osv):
+    _name = 'referential.sub.entity'
+    _description = 'Referential Sub Entity'
+    
+    _columns = {
+        'name': fields.char('Name', size=64, required=True),
+        'referential_id': fields.many2one('external.referential', 'Referential', select=True, ondelete='cascade'),
+        'shop_ids': fields.one2many('sale.shop', 'sub_entity_id', 'Sale Shops'),
+    }
+    
+referential_sub_entity()
+
+
+class external_referential(osv.osv):
+    _inherit = 'external.referential'
+    
+    _columns = {
+        'sub_entity_ids': fields.one2many('referential.sub.entity', 'referential_id', 'Sub Entities'),
+    }
+
+external_referential()
+
+
+class product_category(osv.osv):
+    _inherit = "product.category"
+    
+    def collect_children(self, category, children=[]):
+        for child in category.child_id:
+            children.append(child.id)
+            self.collect_children(child, children)
+        return children
+    
+    def _get_recursive_children_ids(self, cr, uid, ids, name, args, context=None):
+        res = {}
+        for category in self.browse(cr, uid, ids):
+            res[category.id] = self.collect_children(category, [category.id])
+        return res
+
+    _columns = {
+        'recursive_childen_ids': fields.function(_get_recursive_children_ids, method=True, type='one2many', relation="product.category", string='All Child Categories'),
+    }
+    
+product_category()
+
+
+class sale_shop(external_osv.external_osv):
+    _inherit = "sale.shop"
+
+    def _get_exportable_product_ids(self, cr, uid, ids, name, args, context=None):
+        
+        res = {}
+        for shop in self.browse(cr, uid, ids):
+            root_categories = [category for category in shop.exportable_root_category_ids]
+            all_categories = []
+            for category in root_categories:
+                all_categories += [category.id for category in category.recursive_childen_ids]
+            product_ids = self.pool.get("product.product").search(cr, uid, [('categ_id','in', all_categories)]) #TODO deal with product_m2mactegories module
+            res[shop.id] = product_ids
+        return res
+
+    _columns = {
+        #'exportable_category_ids': fields.function(_get_exportable_category_ids, method=True, type='one2many', relation="product.category", string='Exportable Categories'),
+        'exportable_root_category_ids': fields.many2many('product.category', 'shop_category_rel', 'categ_id', 'shop_id', 'Exportable Root Categories'),
+        'exportable_product_ids': fields.function(_get_exportable_product_ids, method=True, type='one2many', relation="product.product", string='Exportable Products'),
+        'referential_id': fields.related('sub_entity_id', 'referential_id', type='many2one', relation='external.referential', string='External Referential'),
+        'sub_entity_id':fields.many2one('referential.sub.entity', 'Referential Sub Entity')
+    }
+    
+    _defaults = {
+        'payment_default_id': lambda *a: 1, #required field that would cause trouble if not set when importing
+    }
+    
+    def export_categories(self, cr, uid, ids, ctx):
+        for shop in self.browse(cr, uid, ids):
+            categories = Set([])
+            for category in shop.exportable_root_category_ids:#TODO ensure order is from root to leaf
+                for child in category.recursive_childen_ids:
+                    categories.add(child)
+            self.export_categories_collection(cr, uid, shop, [categ for categ in categories], ctx)
+        
+    def export_categories_collection(self, cr, uid, shop, categories, ctx):
+        osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
+        
+    def export_products_collection(self, cr, uid, shop, products, ctx):
+        osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
+
+
+    def export_products(self, cr, uid, ids, ctx):
+        for shop in self.browse(cr, uid, ids):
+            self.export_products_collection(cr, uid, shop, shop.exportable_product_ids, ctx)
+    
+    def export_catalog(self, cr, uid, ids, ctx):
+        self.export_categories(cr, uid, ids, ctx)
+        self.export_products(cr, uid, ids, ctx)
+        
+    def import_catalog(self, cr, uid, ids, ctx):
+        #TODO import categories, then products
+        osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
+        
+    def import_orders(self, cr, uid, ids, ctx):
+        osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
+
+    def update_orders(self, cr, uid, ids, ctx):
+        osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
+
+sale_shop()
