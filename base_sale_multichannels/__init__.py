dc6a7d51569c4a4159876400ab68e50bdea2660e
Raphaël Valyi <rvalyi@gmail.com>
2010-08-25 21:35:50 -0300

    [FIX] Add the copyright of Raphael Valyi and Sharoon Thomas

--- base_sale_multichannels/__init__.py
+++ base_sale_multichannels/__init__.py
@@ -1,1 +1,24 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2009 Akretion (<http://www.akretion.com>). All Rights Reserved
+#    authors: Raphaël Valyi, Sharoon Thomas
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 import sale
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- base_sale_multichannels/sale.py
+++ base_sale_multichannels/sale.py
@@ -1,8 +1,6 @@
 # -*- encoding: utf-8 -*-
 #########################################################################
 #                                                                       #
-#########################################################################
-#                                                                       #
 # Copyright (C) 2009  Raphaël Valyi                                     #
 #                                                                       #
 #This program is free software: you can redistribute it and/or modify   #
@@ -24,7 +22,8 @@ from base_external_referentials import external_osv
 from sets import Set
 import netsvc
 from tools.translate import _
-from datetime import datetime
+#from datetime import datetime
+import time
 
 
 class external_shop_group(external_osv.external_osv):
@@ -82,7 +81,13 @@ class sale_shop(external_osv.external_osv):
             all_categories = []
             for category in root_categories:
                 all_categories += [category.id for category in category.recursive_childen_ids]
-            product_ids = self.pool.get("product.product").search(cr, uid, [('categ_id','in', all_categories)]) #TODO deal with product_m2mactegories module
+
+            # If product_m2mcategories module is installed search in main category and extra categories. If not, only in main category
+            cr.execute('select * from ir_module_module where name=%s and state=%s', ('product_m2mcategories','installed'))
+            if cr.fetchone():
+                product_ids = self.pool.get("product.product").search(cr, uid, ['|',('categ_id', 'in', all_categories),('categ_ids', 'in', all_categories)])
+            else:
+                product_ids = self.pool.get("product.product").search(cr, uid, [('categ_id', 'in', all_categories)])
             res[shop.id] = product_ids
         return res
 
@@ -92,9 +97,13 @@ class sale_shop(external_osv.external_osv):
         'exportable_product_ids': fields.function(_get_exportable_product_ids, method=True, type='one2many', relation="product.product", string='Exportable Products'),
         'shop_group_id': fields.many2one('external.shop.group', 'Shop Group', ondelete='cascade'),
         'last_inventory_export_date': fields.datetime('Last Inventory Export Time'),
+        'last_images_export_date': fields.datetime('Last Images Export Time'),
         'last_update_order_export_date' : fields.datetime('Last Order Update  Time'),
+        'last_products_export_date' : fields.datetime('Last Product Export  Time'),
         'referential_id': fields.related('shop_group_id', 'referential_id', type='many2one', relation='external.referential', string='External Referential'),
         'is_tax_included': fields.boolean('Prices Include Tax?', help="Requires sale_tax_include module to be installed"),
+        
+        #TODO all the following settings are deprecated and replaced by the finer grained base.sale.payment.type settings!
         'picking_policy': fields.selection([('direct', 'Partial Delivery'), ('one', 'Complete Delivery')],
                                            'Packing Policy', help="""If you don't have enough stock available to deliver all at once, do you accept partial shipments or not?"""),
         'order_policy': fields.selection([
@@ -113,12 +122,12 @@ class sale_shop(external_osv.external_osv):
     }
     
     _defaults = {
-        'payment_default_id': lambda *a: 1, #required field that would cause trouble if not set when importing
-        'picking_policy': lambda *a: 'direct',
-        'order_policy': lambda *a: 'manual',
-        'invoice_quantity': lambda *a: 'order',
-        'invoice_generation_policy': lambda *a: 'draft',
-        'picking_generation_policy': lambda *a: 'draft',
+        'payment_default_id': lambda * a: 1, #required field that would cause trouble if not set when importing
+        'picking_policy': lambda * a: 'direct',
+        'order_policy': lambda * a: 'manual',
+        'invoice_quantity': lambda * a: 'order',
+        'invoice_generation_policy': lambda * a: 'draft',
+        'picking_generation_policy': lambda * a: 'draft',
     }
 
     def _get_pricelist(self, cr, uid, shop):
@@ -138,7 +147,7 @@ class sale_shop(external_osv.external_osv):
         self.pool.get('product.category').ext_export(cr, uid, [categ_id for categ_id in categories], [shop.referential_id.id], {}, ctx)
        
     def export_products_collection(self, cr, uid, shop, products, ctx):
-        self.pool.get('product.product').ext_export(cr, uid, [product.id for product in shop.exportable_product_ids] ,[shop.referential_id.id], {}, ctx)
+        self.pool.get('product.product').ext_export(cr, uid, [product.id for product in shop.exportable_product_ids] , [shop.referential_id.id], {}, ctx)
 
     def export_products(self, cr, uid, shop, ctx):
         self.export_products_collection(cr, uid, shop, shop.exportable_product_ids, ctx)
@@ -149,6 +158,8 @@ class sale_shop(external_osv.external_osv):
             ctx['conn_obj'] = self.external_connection(cr, uid, shop.referential_id)
             self.export_categories(cr, uid, shop, ctx)
             self.export_products(cr, uid, shop, ctx)
+        self.export_inventory(cr, uid, ids, ctx)
+        return False
             
     def export_inventory(self, cr, uid, ids, ctx):
         for shop in self.browse(cr, uid, ids):
@@ -157,11 +168,14 @@ class sale_shop(external_osv.external_osv):
             product_ids = [product.id for product in shop.exportable_product_ids]
             if shop.last_inventory_export_date:
                 recent_move_ids = self.pool.get('stock.move').search(cr, uid, [('date', '>', shop.last_inventory_export_date), ('product_id', 'in', product_ids)])
-                product_ids = [move.product_id.id for move in self.pool.get('stock.move').browse(cr, uid, recent_move_ids)]
+            else:
+                recent_move_ids = self.pool.get('stock.move').search(cr, uid, [('product_id', 'in', product_ids)])
+            product_ids = [move.product_id.id for move in self.pool.get('stock.move').browse(cr, uid, recent_move_ids)]
+            product_ids = [x for x in set(product_ids)]
             res = self.pool.get('product.product').export_inventory(cr, uid, product_ids, '', ctx)
-            self.pool.get('sale.shop').write(cr, uid, shop.id, {'last_inventory_export_date': datetime.now()})
-            return res
-        
+            self.pool.get('sale.shop').write(cr, uid, shop.id, {'last_inventory_export_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+        return res
+    
     def import_catalog(self, cr, uid, ids, ctx):
         #TODO import categories, then products
         osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
@@ -181,6 +195,7 @@ class sale_shop(external_osv.external_osv):
                 defaults.update({'price_type': 'tax_included'})
 
             self.import_shop_orders(cr, uid, shop, defaults, ctx)
+        return False
             
     def import_shop_orders(self, cr, uid, shop, defaults, ctx):
         osv.except_osv(_("Not Implemented"), _("Not Implemented in abstract base module!"))
@@ -207,8 +222,9 @@ class sale_shop(external_osv.external_osv):
                     order = self.pool.get('sale.order').browse(cr, uid, id, ctx)            
                     order_ext_id = result[1].split('sale.order_')[1]
                     self.update_shop_orders(cr, uid, order, order_ext_id, ctx)
-                    logger.notifyChannel('ext synchro', netsvc.LOG_INFO, "Successfully updated order with OpenERP id %s and ext id %s in external sale system" %(id, order_ext_id))
-            self.pool.get('sale.shop').write(cr, uid, shop.id, {'last_update_order_export_date': datetime.now()})
+                    logger.notifyChannel('ext synchro', netsvc.LOG_INFO, "Successfully updated order with OpenERP id %s and ext id %s in external sale system" % (id, order_ext_id))
+            self.pool.get('sale.shop').write(cr, uid, shop.id, {'last_update_order_export_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+        return False
 
         
     def update_shop_orders(self, cr, uid, order, ext_id, ctx):
@@ -220,10 +236,23 @@ sale_shop()
 class sale_order(osv.osv):
     _inherit = "sale.order"
 
-    def generate_payment_with_pay_code(self, cr, uid, payment_code, partner_id, amount, payment_ref, entry_name, date, should_validate, ctx):
-        journal_ids = self.pool.get("account.journal").search(cr, uid, [('external_payment_codes', 'ilike', payment_code)])
-        if journal_ids and len(journal_ids) > 0:
-            return self.generate_payment_with_journal(cr, uid, journal_ids[0], partner_id, amount, payment_ref, entry_name, date, should_validate, ctx)
+    _columns = {
+                'ext_payment_method': fields.char('External Payment Method', size=32, help = "Spree, Magento, Oscommerce... Payment Method"),
+                'need_to_update': fields.boolean('Need To Update')
+    }
+    _defaults = {
+        'need_to_update': lambda *a: False,
+    }
+
+    def payment_code_to_payment_settings(self, cr, uid, payment_code, ctx=None):
+        payment_setting_ids = self.pool.get('base.sale.payment.type').search(cr, uid, [['name', 'ilike', payment_code]])
+        return payment_setting_ids and self.pool.get('base.sale.payment.type').browse(cr, uid, payment_setting_ids[0], ctx) or False
+
+    def generate_payment_with_pay_code(self, cr, uid, payment_code, partner_id, amount, payment_ref, entry_name, date, paid, ctx):
+        payment_settings = self.payment_code_to_payment_settings(cr, uid, payment_code, ctx)
+        if payment_settings and payment_settings.journal_id and (payment_settings.check_if_paid and paid or not payment_settings.check_if_paid):
+            return self.generate_payment_with_journal(cr, uid, payment_settings.journal_id.id, partner_id, amount, payment_ref, entry_name, date, payment_settings.validate_payment, ctx)
+        return False
         
     def generate_payment_with_journal(self, cr, uid, journal_id, partner_id, amount, payment_ref, entry_name, date, should_validate, ctx):
         statement_vals = {
@@ -235,7 +264,7 @@ class sale_order(osv.osv):
                         }
         statement_id = self.pool.get('account.bank.statement').create(cr, uid, statement_vals, ctx)
         statement = self.pool.get('account.bank.statement').browse(cr, uid, statement_id, ctx)
-        account_id = self.pool.get('account.bank.statement.line').onchange_partner_id(cr, uid, False, partner_id, "customer", statement.currency.id, ctx)['value']['account_id']
+        account_id = self.pool.get('account.bank.statement.line').onchange_partner_id(cr, uid, [], partner_id, "customer", statement.currency.id, ctx)['value']['account_id']
         statement_line_vals = {
                                 'statement_id': statement_id,
                                 'name': entry_name,
@@ -247,15 +276,89 @@ class sale_order(osv.osv):
         statement_line_id = self.pool.get('account.bank.statement.line').create(cr, uid, statement_line_vals, ctx)
         if should_validate:
             self.pool.get('account.bank.statement').button_confirm(cr, uid, [statement_id], ctx)
+            self.pool.get('account.move.line').write(cr, uid, [statement.move_line_ids[0].id], {'date': date})
+        return statement_line_id
+
+
+    def oe_status(self, cr, uid, order_id, paid = True, context = None):
+        wf_service = netsvc.LocalService("workflow")
+        order = self.browse(cr, uid, order_id, context)
+        payment_settings = self.payment_code_to_payment_settings(cr, uid, order.ext_payment_method, context)
+                
+        if payment_settings: 
+            if payment_settings.check_if_paid and not paid:
+                #TODO add update function indeed if automatique import is used sometime you will import an order which is not in a stable state (ex : the order is not payed (waiting for paypal confirmation), and can be payed or canceled later by your e-commerce website)
+                self.write(cr, uid, order.id, {'need_to_update': True})
+            else:
+                if payment_settings.validate_order:
+                    wf_service.trg_validate(uid, 'sale.order', order.id, 'order_confirm', cr)
+                    
+                    if order.order_policy == 'prepaid':
+                        if payment_settings.validate_invoice:
+                            for invoice in order.invoice_ids:
+                                wf_service.trg_validate(uid, 'account.invoice', invoice.id, 'invoice_open', cr)
+        
+                    if order.order_policy == 'manual':
+                        if payment_settings.create_invoice:
+                           invoice_id = self.pool.get('sale.order').action_invoice_create(cr, uid, [order_id])
+                           if payment_settings.validate_invoice:
+                               wf_service.trg_validate(uid, 'account.invoice', invoice_id, 'invoice_open', cr)
+        
+                    # IF postpaid DO NOTHING
+        
+                    if order.order_policy == 'picking':
+                        if payment_settings.create_invoice:
+                           invoice_id = self.pool.get('stock.picking').action_invoice_create(cr, uid, order.picking_ids)
+                           if payment_settings.validate_invoice:
+                               wf_service.trg_validate(uid, 'account.invoice', invoice_id, 'invoice_open', cr)
+
+        return True
+
+    def action_invoice_create(self, cr, uid, ids, grouped=False, states=['confirmed', 'done', 'exception']):
+        wf_service = netsvc.LocalService("workflow")
+        res = super(sale_order, self).action_invoice_create(cr, uid, ids, grouped, states)
+        for order_id in ids:
+            order = self.browse(cr, uid, order_id)
+            if order.order_policy == 'postpaid':
+                payment_settings = self.payment_code_to_payment_settings(cr, uid, order.ext_payment_method)
+                if payment_settings and payment_settings.validate_invoice:
+                    for invoice in order.invoice_ids:
+                        wf_service.trg_validate(uid, 'account.invoice', invoice.id, 'invoice_open', cr)
+        return res
 
 sale_order()
 
-class account_journal(osv.osv):
-    _inherit = "account.journal"
-    
+class base_sale_payment_type(osv.osv):
+    _name = "base.sale.payment.type"
+    _description = "Base Sale Payment Type"
+
     _columns = {
-                'external_payment_codes': fields.char('External Payment Codes', size=256, help="Enter the external payment codes, comma separated. They will be used to select the payment journal."),
+        'name': fields.char('Payment Codes', help="List of Payment Codes separated by ;", size=256, required=True),
+        'journal_id': fields.many2one('account.journal','Payment Journal'),
+        'picking_policy': fields.selection([('direct', 'Partial Delivery'), ('one', 'Complete Delivery')], 'Packing Policy'),
+        'order_policy': fields.selection([
+            ('prepaid', 'Payment Before Delivery'),
+            ('manual', 'Shipping & Manual Invoice'),
+            ('postpaid', 'Invoice on Order After Delivery'),
+            ('picking', 'Invoice from the Packing'),
+        ], 'Shipping Policy'),
+        'invoice_quantity': fields.selection([('order', 'Ordered Quantities'), ('procurement', 'Shipped Quantities')], 'Invoice on'),
+        'is_auto_reconcile': fields.boolean('Auto-reconcile?', help="if true will try to reconcile the order payment statement and the open invoice"),
+        'validate_order': fields.boolean('Validate Order?'),
+        'validate_payment': fields.boolean('Validate Payment?'),
+        'create_invoice': fields.boolean('Create Invoice?'),
+        'validate_invoice': fields.boolean('Validate Invoice?'),
+        'check_if_paid': fields.boolean('Check if Paid?'),
     }
     
-account_journal()
+    _defaults = {
+        'picking_policy': lambda *a: 'direct',
+        'order_policy': lambda *a: 'manual',
+        'invoice_quantity': lambda *a: 'order',
+        'is_auto_reconcile': lambda *a: False,
+        'validate_payment': lambda *a: False,
+        'validate_invoice': lambda *a: False,
+    }
 
+base_sale_payment_type()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
