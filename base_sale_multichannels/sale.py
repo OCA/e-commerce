61ba4bf4938e49b97a18c1839be5971bb701d866
Raphaël Valyi <rvalyi@gmail.com>
2010-09-09 15:47:39 -0300

    [FIX] base_sale_multichannels: fixed invoice on delivery

--- base_sale_multichannels/sale.py
+++ base_sale_multichannels/sale.py
@@ -362,7 +362,7 @@ class sale_order(osv.osv):
         
                     if order.order_policy == 'picking':
                         if payment_settings.create_invoice:
-                           invoice_id = self.pool.get('stock.picking').action_invoice_create(cr, uid, order.picking_ids)
+                           invoice_id = self.pool.get('stock.picking').action_invoice_create(cr, uid, [picking.id for picking in order.picking_ids])
                            if payment_settings.validate_invoice:
                                wf_service.trg_validate(uid, 'account.invoice', invoice_id, 'invoice_open', cr)
 
