a3afa606955dd88bef8e9894f04c7b1f51eb24be
sebastien beau <sebastien.beau@akretion.com.br>
2012-06-11 09:33:40 +0200

    [IMP] base_sale_multichannels : add wizard to import order manually

--- base_sale_multichannels/wizard/import_order.py
+++ base_sale_multichannels/wizard/import_order.py
@@ -18,6 +18,25 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import export_product
-import import_order
 
+
+from osv import fields,osv
+from tools.translate import _
+from base_external_referentials.external_osv import ExternalSession
+
+class sale_order_import_wizard(osv.osv_memory):
+    _name = 'sale.order.import.wizard'
+    _description = 'sale order import wizard'
+
+    _columns = {
+        'order_number': fields.char('Order Number', size=64),
+        }
+
+    def import_order(self, cr, uid, ids, context=None):
+        if not context:
+            context={}
+        shop = self.pool.get('sale.shop').browse(cr, uid, context['active_id'], context=context)
+        external_session = ExternalSession(shop.referential_id, shop)
+        wizard = self.browse(cr, uid, ids[0], context=context)
+        self.pool.get('sale.order')._import_one_resource(cr, uid, external_session, wizard.order_number, context=context)
+        return {'type': 'ir.actions.act_window_close'}
