b6c7a1312aa08412b536537fe17fb4b2b569e352
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-01-30 16:33:13 +0100

    [MIGR] product_links: license, formatting, manifest

--- product_links/__openerp__.py
+++ product_links/__openerp__.py
@@ -1,45 +1,47 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright Camptocamp SA
+#    Author: Guewen Baconnier
+#    Copyright 2011-2013 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Product links',
-    'version': '6.1.0',
-    'category': 'Generic Modules/Products',
+    'name': 'Product Links',
+    'version': '7.0.0',
+    'category': 'Generic Modules',
     'description': """
-This module adds links between products :
- - cross-selling
- - up-selling
- - related
+This module adds links between products:
+
+- cross-selling
+- up-selling
+- related
+
 These types of links are common in e-commerce shops.
 
-It can be used as a base to implement synchronisations with e-commerce (used in magentoerpconnect).
+It can be used as a base to implement synchronisations with
+e-commerce (as instance, it is used in magentoerpconnect).
     """,
     'author': 'Camptocamp',
     'website': 'http://www.camptocamp.com',
-    'depends': ['stock'],
-    'init_xml': [],
-    'update_xml': [
-                   'security/ir.model.access.csv',
-                   'product_links_view.xml',
-                   ],
-    'demo_xml': [],
-    'installable': False,
+    'depends': ['product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'product_links_view.xml'
+        ],
+    'installable': True,
     'active': False,
 }
--- product_links/product_links.py
+++ product_links/product_links.py
@@ -1,41 +1,55 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author Guewen Baconnier. Copyright Camptocamp SA
+#    Author: Guewen Baconnier
+#    Copyright 2011-2013 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from openerp.osv.orm import Model
-from openerp.osv import fields
+from openerp.osv import orm, fields
 
-class product_link(Model):
+
+class product_link(orm.Model):
     _name = 'product.link'
     _rec_name = 'linked_product_id'
 
     def get_link_type_selection(self, cr, uid, context=None):
-        # selection can be inherited
-        return [('cross_sell', 'Cross-Sell'), ('up_sell', 'Up-Sell'), ('related', 'Related')]
+        # selection can be inherited and extended
+        return [('cross_sell', 'Cross-Sell'),
+                ('up_sell', 'Up-Sell'),
+                ('related', 'Related')]
 
     def _get_link_type_selection(self, cr, uid, context=None):
         return self.get_link_type_selection(cr, uid, context=context)
 
     _columns = {
-        'product_id': fields.many2one('product.product', 'Source product', required=True, ondelete='cascade'),
-        'linked_product_id': fields.many2one('product.product', 'Linked product', required=True, ondelete='cascade'),
-        'type': fields.selection(_get_link_type_selection, 'Link type', required=True),
+        'product_id': fields.many2one(
+            'product.product',
+            'Source product',
+            required=True,
+            ondelete='cascade'),
+        'linked_product_id': fields.many2one(
+            'product.product',
+            'Linked product',
+            required=True,
+            ondelete='cascade'),
+        'type': fields.selection(
+            _get_link_type_selection,
+            string='Link type',
+            required=True),
         'is_active': fields.boolean('Active'),
     }
 
@@ -45,21 +59,20 @@ class product_link(Model):
     def _get_default_product_id(self, cr, uid, context=None):
         if context is None:
             context = {}
-        return context.get('product_id')
+        return context.get('product_id', False)
 
     _defaults = {
         'is_active': True,
         'product_id': _get_default_product_id,
     }
 
-class product(Model):
-    """Inherit product in order to manage product links"""
+
+class product(orm.Model):
     _inherit = 'product.product'
 
     _columns = {
         'product_link_ids': fields.one2many(
             'product.link',
             'product_id',
-            'Product links',
-            ),
+            string='Product links'),
         }
