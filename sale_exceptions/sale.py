499c85cc88e6fbd57826d9cc43bce75979c0cf4d
Sébastien Beau <sebastien.beau@akretion.com>
2013-06-08 20:32:55 +0200

    [FIX] fix missing context initialisation

--- sale_exceptions/sale.py
+++ sale_exceptions/sale.py
@@ -97,6 +97,8 @@ class sale_order(Model):
         return True
 
     def _popup_exceptions(self, cr, uid, order_id, context=None):
+        if context is None:
+            context = {}
         model_data_obj = self.pool.get('ir.model.data')
         list_obj = self.pool.get('sale.exception.confirm')
         ctx = context.copy()
