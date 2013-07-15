98f6dd3aaf4588cbc8e85a7b4c0856fe17d6b146
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-07-15 14:15:03 +0200

    [FIX] sale_exceptions wizard: pep8, complete openerp namespace, new models, asserts on the ids received

--- sale_exceptions/wizard/sale_exception_confirm.py
+++ sale_exceptions/wizard/sale_exception_confirm.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Copyright Camptocamp SA
@@ -19,38 +19,45 @@
 #
 ##############################################################################
 
-import netsvc
-
-from osv import osv, fields
+from openerp.osv import orm, fields
 
 
-class SaleExceptionConfirm(osv.osv_memory):
+class SaleExceptionConfirm(orm.TransientModel):
 
     _name = 'sale.exception.confirm'
 
     _columns = {
         'sale_id': fields.many2one('sale.order', 'Sale'),
-        'exception_ids': fields.many2many('sale.exception', string='Exceptions to resolve', readonly=True),
+        'exception_ids': fields.many2many('sale.exception',
+                                          string='Exceptions to resolve',
+                                          readonly=True),
         'ignore': fields.boolean('Ignore Exceptions'),
     }
 
     def default_get(self, cr, uid, fields, context=None):
-        res = super(SaleExceptionConfirm, self).default_get(cr, uid, fields, context=context)
+        if context is None:
+            context = {}
+        res = super(SaleExceptionConfirm, self).default_get(
+            cr, uid, fields, context=context)
         order_obj = self.pool.get('sale.order')
-        sale_id = context.get('active_id', False)
-        if sale_id:
-            sale = order_obj.browse(cr, uid, sale_id, context=context)
-            exception_ids = [e.id for e in sale.exceptions_ids]
-            res.update({'exception_ids': [(6, 0, exception_ids)]})
-
+        sale_id = context.get('active_ids')
+        assert len(sale_id) == 1, "Only 1 ID accepted, received %r" % ids
+        sale_id = sale_id[0]
+        sale = order_obj.browse(cr, uid, sale_id, context=context)
+        exception_ids = [e.id for e in sale.exceptions_ids]
+        res.update({'exception_ids': [(6, 0, exception_ids)]})
         res.update({'sale_id': sale_id})
         return res
 
     def action_confirm(self, cr, uid, ids, context=None):
-        form = self.browse(cr, uid, ids[0], context=context)
+        if hasattr(ids, '__iter__'):
+            assert len(ids) == 1, "Only 1 ID accepted, received %r" % ids
+            ids = ids[0]
+        form = self.browse(cr, uid, ids, context=context)
         if form.ignore:
-            self.pool.get('sale.order').write(cr, uid, form.sale_id.id,
-                    {'ignore_exceptions': True}, context=context)
+            self.pool.get('sale.order').write(
+                cr, uid,
+                form.sale_id.id,
+                {'ignore_exceptions': True},
+                context=context)
         return {'type': 'ir.actions.act_window_close'}
-
-SaleExceptionConfirm()
