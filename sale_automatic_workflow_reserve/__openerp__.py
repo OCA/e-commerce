b27a546ed4c53282d968926d31e9276ddb2e0f34
Sébastien BEAU <sebastien.beau@akretion.com>
2014-10-13 20:51:48 +0200

    [ADD] add module to automatically reserve sale order depending of the workflow method reservation

---
+++ sale_automatic_workflow_reserve/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP
+#   Copyright (C) 2014 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{'name': 'Sale Automatic Workflow Reserve',
+ 'version': '0.0.1',
+ 'author': 'Akretion',
+ 'website': 'www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'description': """Give the posibility to reserve the product of the sale order
+ automatically depending of the workflow assigned to the sale order
+ """,
+ 'depends': [
+     'sale_automatic_workflow',
+     'stock_reserve_sale',
+ ],
+ 'data': [
+     'sale_workflow_process_view.xml'
+ ],
+ 'installable': True,
+ 'application': True,
+ }
---
+++ sale_automatic_workflow_reserve/automatic_workflow_job.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP
+#   Copyright (C) 2014 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp.osv import orm
+
+
+class AutomaticWorkflowJob(orm.Model):
+    _inherit = 'automatic.workflow.job'
+
+    def _get_domain_for_sale_reservation(self, cr, uid, context=None):
+        return [
+            ('order_id.state', '=', 'draft'),
+            ('order_id.workflow_process_id.reserve_sale_order', '=', True),
+            ('reservation_ids', '=', False),
+        ]
+
+    def _get_reservable_line(self, cr, uid, context=None):
+        line_obj = self.pool['sale.order.line']
+        domain = self._get_domain_for_sale_reservation(
+            cr, uid, context=context)
+        return line_obj.search(cr, uid, domain, context=context)
+
+    def _prepare_reserve_wizard(self, cr, uid, context=None):
+        return {}
+
+    def _reserve_sale_order(self, cr, uid, context=None):
+        reserve_obj = self.pool['sale.stock.reserve']
+        vals = self._prepare_reserve_wizard(cr, uid, context=context)
+        reserve_id = reserve_obj.create(cr, uid, vals, context=context)
+        line_ids = self._get_reservable_line(cr, uid, context=context)
+        reserve_obj.stock_reserve(
+            cr, uid, [reserve_id], line_ids, context=context)
+
+    def run(self, cr, uid, ids=None, context=None):
+        """ Must be called from ir.cron """
+        super(AutomaticWorkflowJob, self).run(
+            cr, uid, ids, context=context)
+        self._reserve_sale_order(cr, uid, context=context)
+        return True
