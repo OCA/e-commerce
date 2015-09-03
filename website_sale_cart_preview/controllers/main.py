9fe1af935174ae03ea00f79de357741b4b9e291e
Leonardo Donelli <learts92@gmail.com>
2015-09-03 10:39:46 +0200

    Do not break inheritance of 'cart' controller.
    Call the super implementation and then, if needed,
    take the values from the rendered response's qcontext.
    This means that the qweb template will be rendered 2 times
    in some cases, but there is no way around it.
    
    Also change back 'website_sale_order' to 'order' in the qweb
    context so that we can return the super rendered response
    without modifications.

--- website_sale_cart_preview/controllers/main.py
+++ website_sale_cart_preview/controllers/main.py
@@ -6,35 +6,15 @@ from openerp.http import request
 import openerp.addons.website_sale.controllers.main
 
 
-class website_sale(openerp.addons.website_sale.controllers.main.website_sale):
+class WebsiteSale(openerp.addons.website_sale.controllers.main.website_sale):
 
     @http.route(['/shop/cart'], type='http', auth="public", website=True)
     def cart(self, **post):
-        cr, uid, context = request.cr, request.uid, request.context
-        pool = request.registry
-        order = request.website.sale_get_order()
-        if order:
-            from_currency = pool.get('product.price.type')._get_field_currency(
-                cr, uid, 'list_price', context)
-            to_currency = order.pricelist_id.currency_id
-            compute_currency = lambda price: pool['res.currency']._compute(
-                cr, uid, from_currency, to_currency, price, context=context)
-        else:
-            compute_currency = lambda price: price
-
-        values = {
-            'website_sale_order': order,
-            'compute_currency': compute_currency,
-            'suggested_products': [],
-        }
-        if order:
-            _order = order
-            if not context.get('pricelist'):
-                _order = order.with_context(pricelist=order.pricelist_id.id)
-            values['suggested_products'] = _order._cart_accessories()
-
+        res = super(WebsiteSale, self).cart(**post)
         if post.get('type') == 'popover':
+            # two qweb responses will be rendered in this case, the one by
+            # super and this one, but there is no way around it if
+            # we don't want to break inheritance
             return request.website.render(
-                "website_sale_cart_preview.cart_popover", values)
-
-        return request.website.render("website_sale.cart", values)
+                "website_sale_cart_preview.cart_popover", res.qcontext)
+        return res
