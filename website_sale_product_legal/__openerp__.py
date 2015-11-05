c6355a3f4e1e4546a69a933f7129163944c09c22
Jairo Llopis <yajo.sk8@gmail.com>
2015-11-05 15:36:15 +0100

    Add module website_sale_product_legal.

---
+++ website_sale_product_legal/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Legal terms per product",
+    "summary": "Make buyers to accept legal terms per product",
+    "version": "8.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://odoo-community.org/",
+    "author": "Antiun Ingeniería, S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/legal_term_view.xml",
+        "views/product_template_view.xml",
+        "views/templates.xml",
+    ],
+}
---
+++ website_sale_product_legal/models/product_template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class Producttemplate(models.Model):
+    _inherit = "product.template"
+
+    legal_term_ids = fields.Many2many(
+        "website_sale_product_legal.legal_term",
+        "website_sale_product_legal_rel",
+        string="Legal terms",
+        help="Online customers will be informed of these legal terms before "
+             "buying this product.")
