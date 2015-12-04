ca4b2209f85db416a57e3dd2ad70eb4245d567ae
Ildar Nasyrov <nasyrov@it-projects.info>
2015-12-04 12:10:50 +0500

    version in website_sale_require_login

--- website_sale_require_login/__openerp__.py
+++ website_sale_require_login/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Require login to checkout",
     "summary": "Force users to login for buying",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Website",
     "website": "http://www.antiun.com",
     "author": "Antiun Ingeniería, S.L., Odoo Community Association (OCA)",
