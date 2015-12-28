0acd900ecf82732d77870f1f82ffcdd2b26cf649
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-28 18:07:35 +0100

    Add module website_sale_default_country.

---
+++ website_sale_default_country/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "eCommerce Default Country",
+    "summary": "Give a default value for country at checkout",
+    "version": "8.0.1.0.0",
+    "category": "Website",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+}
