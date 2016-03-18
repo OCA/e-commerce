2a2e1b1f0666c5fbc8876516b617573653500fd5
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-03-18 15:14:07 +0530

    [PORT][9.0] product_links/product_multi_link

---
+++ product_multi_link/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2011 Guewen Baconnier,Camptocamp,Elico-Corp
+# © 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
---
+++ product_multi_link/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2011 Guewen Baconnier,Camptocamp,Elico-Corp
+# © 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product Links',
+    'version': '9.0.1.0.0',
+    'category': 'Generic Modules',
+    'author': "Camptocamp,Elico-Corp,Sodexis,Odoo Community Association (OCA)",
+    'website': 'http://www.camptocamp.com',
+    'license': 'AGPL-3',
+    'depends': ['product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_view.xml'
+    ],
+    'images': [],
+    'installable': True,
+}
