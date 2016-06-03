61ce7550ccc00c227ea3a561151a6848ba7701e8
sergio-incaser <sergio@incaser.es>
2016-06-03 03:23:42 +0200

    website_sale_checkout_country_vat: New module to autocomplete VAT code in website checkout form.

---
+++ website_sale_checkout_country_vat/models/website.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from openerp import fields, models
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    vat_code_id = fields.Many2one(
+        comodel_name='res.country', string="Default Country VAT Code", size=2)
