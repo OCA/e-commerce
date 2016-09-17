13980f3fa569380e097a7a2dbf8bfc3dd5c59f4a
Dave Lasley <dave@laslabs.com>
2016-09-16 20:10:47 -0700

    website_sale_require_login: Upgrade to v9 * Migrate website_sale_require_login to v9

--- website_sale_require_login/static/src/js/website_sale_tour_buy_login.tour.js
+++ website_sale_require_login/static/src/js/website_sale_tour_buy_login.tour.js
@@ -2,9 +2,14 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-"use strict";
-(function ($) {
-    var steps = openerp.Tour.tours.shop_buy_product.steps;
+odoo.define('website_sale_require_login.tour', function(require) {
+    
+    "use strict";
+    
+    var $ = require('$');
+    var Tour = require('web.Tour');
+    
+    var steps = Tour.tours.shop_buy_product.steps;
 
     // Gets user name from login menu (see openerp.Tour.log)
     function user() {
@@ -15,27 +20,29 @@
 
     // Alter expected buying workflow for public users
     if (!user()) {
+        var stepOnload = function () {
+            $("#login").val("demo");
+            $("#password").val("demo");
+        };
         for (var position = 0; position < steps.length; position++) {
             if (steps[position].title === "go to checkout") {
                 steps.splice(
                     position,
                     1,
                     {
-                        title: "go to checkout",
+                        title: "Go To Checkout",
                         waitFor: "#cart_products input.js_quantity[value=1]",
                         element: "a span:contains('Log in and checkout')",
                     },
                     {
-                        title: "Log in",
+                        title: "Log In",
                         element: "form.oe_login_form button[type=submit]",
-                        onload: function () {
-                            $("#login").val("demo");
-                            $("#password").val("demo");
-                        },
+                        onload: stepOnload,
                     }
                 );
                 break;
             }
         }
     }
-})(jQuery);
+    
+});
