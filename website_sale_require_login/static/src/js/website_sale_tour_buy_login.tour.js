9eeb9f2ddb64737866493e233b7dcde71067a36c
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-01 18:46:52 +0500

    [MIG] website_sale_require_login: Migrated to 10.0

--- website_sale_require_login/static/src/js/website_sale_tour_buy_login.tour.js
+++ website_sale_require_login/static/src/js/website_sale_tour_buy_login.tour.js
@@ -3,13 +3,13 @@
  */
 
 odoo.define('website_sale_require_login.tour', function(require) {
-    
+
     "use strict";
-    
-    var $ = require('$');
-    var Tour = require('web.Tour');
-    
-    var steps = Tour.tours.shop_buy_product.steps;
+
+    require("website_sale.tour");
+    var tour = require("web_tour.tour");
+    var base = require("web_editor.base");
+    var steps = tour.tours.shop_buy_product.steps;
 
     // Gets user name from login menu (see openerp.Tour.log)
     function user() {
@@ -25,24 +25,24 @@ odoo.define('website_sale_require_login.tour', function(require) {
             $("#password").val("demo");
         };
         for (var position = 0; position < steps.length; position++) {
-            if (steps[position].title === "go to checkout") {
+            if (steps[position].content === "go to checkout") {
                 steps.splice(
                     position,
                     1,
                     {
                         title: "Go To Checkout",
-                        waitFor: "#cart_products input.js_quantity[value=1]",
-                        element: "a span:contains('Log in and checkout')",
+                        extra_trigger: "#cart_products input.js_quantity[value=1]",
+                        trigger: "a span:contains('Log in and checkout')"
                     },
                     {
                         title: "Log In",
-                        element: "form.oe_login_form button[type=submit]",
-                        onload: stepOnload,
+                        trigger: "form.oe_login_form button[type=submit]",
+                        run: stepOnload
                     }
                 );
                 break;
             }
         }
     }
-    
+
 });
