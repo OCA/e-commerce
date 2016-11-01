edbde6e155e5d9a0dcd2f25330776ff9da7b7de5
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-01 18:46:52 +0500

    [MIG] website_sale_require_login: Migrated to 10.0

--- website_sale_require_login/controllers/main.py
+++ website_sale_require_login/controllers/main.py
@@ -2,11 +2,11 @@
 # © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import http
-from openerp.addons.website_sale.controllers.main import website_sale
+from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class RequireLoginToCheckout(website_sale):
+class RequireLoginToCheckout(WebsiteSale):
     @http.route(auth="user")
     def checkout(self, **post):
         return super(RequireLoginToCheckout, self).checkout(**post)
