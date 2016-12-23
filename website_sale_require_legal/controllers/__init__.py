587d2c0472734f00a6dee38234a7fdc19a014585
cubells <vicent@vcubells.net>
2016-12-23 07:54:24 +0100

    [9.0] [MIG] website_sale_require_legal module

--- website_sale_require_legal/controllers/__init__.py
+++ website_sale_require_legal/controllers/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import main
