3785a9b073d970be31fe7f8d1a5c6bca6c107e98
cubells <vicent@vcubells.net>
2016-12-23 07:54:24 +0100

    [9.0] [MIG] website_sale_require_legal module

--- website_sale_require_legal/controllers/main.py
+++ website_sale_require_legal/controllers/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.http import request
