4c37640f75b633b9ef6d907b221d8f83248e33ae
cubells <vicent@vcubells.net>
2016-12-23 07:54:24 +0100

    [9.0] [MIG] website_sale_require_legal module

--- website_sale_require_legal/__openerp__.py
+++ website_sale_require_legal/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Require accepting legal terms to checkout",
     "summary": "Force the user to accept legal tems to buy in the web shop",
-    "version": "8.0.1.1.0",
+    "version": "9.0.1.0.0",
     "category": "Website",
     "website": "http://www.tecnativa.com",
     "author": "Antiun Ingeniería S.L., "
@@ -13,17 +14,11 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
     "depends": [
         "website_legal_page",
         "website_sale",
     ],
     "data": [
         "views/website_sale.xml",
-    ],
-    "demo": [
     ],
 }
