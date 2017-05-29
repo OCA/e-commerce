44550ff89d14393ebbf7efbffd98442b8e34d42b
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-05-29 08:23:04 +0200

    Update __manifest__.py

--- website_sale_suggest_create_account/__manifest__.py
+++ website_sale_suggest_create_account/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería, S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Suggest to create user account when buying",
     "summary": "Suggest users to create an account when buying in the website",
