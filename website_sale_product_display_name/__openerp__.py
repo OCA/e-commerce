638d3160b57972630c3f2a4efb8b541e913bced9
Dave Lasley <dave@laslabs.com>
2017-06-09 10:39:24 -0700

    [ADD] website_sale_product_display_name: New module * Create new module to show product display name in website, instead of just the name

---
+++ website_sale_product_display_name/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Website Sale Product Display Name',
+    'summary': 'Use product display name instead of name in website sale.',
+    'version': '9.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://laslabs.com/',
+    'author': 'LasLabs',
+    'license': 'LGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/website_sale_template.xml',
+    ],
+}
