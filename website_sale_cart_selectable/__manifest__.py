c3788cc7a7b3df9e78f16dce150058fe7ef6a4dd
David Vidal <chienandalu@gmail.com>
2017-07-13 18:19:40 +0200

    [MIG] website_sale_cart_selectable: Migration to 10.0

--- website_sale_cart_selectable/__manifest__.py
+++ website_sale_cart_selectable/__manifest__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2016 OpenSynergy Indonesia
+# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Website Sale Cart Selectable',
-    'version': '8.0.1.0.0',
-    'summary': 'Enables to controlling button add to cart per product',
-    'author': 'OpenSynergy Indonesia,Odoo Community Association (OCA)',
+    'version': '10.0.1.0.0',
+    'summary': 'Enables to control button Add to cart display per product',
+    'author': 'OpenSynergy Indonesia, '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
     'website': 'https://opensynergy-indonesia.com',
     'category': 'Website',
     'depends': ['website_sale'],
