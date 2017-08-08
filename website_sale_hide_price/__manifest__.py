1e65f6056a9ed1743ff5f5dc10a9f3438baadc73
David Vidal <chienandalu@gmail.com>
2017-08-08 12:12:11 +0200

    [10.0] website_sale_hide_price: New module

---
+++ website_sale_hide_price/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Sale Hide Price',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.tecnativa.com',
+    'license': 'AGPL-3',
+    'summary': 'Hide product prices on the shop',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/partner_view.xml',
+        'views/website_sale_template.xml'
+    ],
+    'installable': True,
+}
