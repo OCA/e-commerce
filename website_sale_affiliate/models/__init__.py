6072d12f56cf2e19eb4d8595d9a7a40e68a89aa1
Brenton Hughes <brenton.hughes@gmail.com>
2017-08-25 09:30:03 -0700

    [ADD] website_sale_affiliate: Create module (#199)
    * [ADD] website_sale_affiliate: Create module
    * Add sale.affiliate and sale.affiliate.request models
    * Override WebsiteSale controller shop method to store affiliate
      id & key kwargs of shop urls in session
    * Override sale order create method to store affiliate request
    * Add affiliate and request views, links in Sales/Affiliates
    * Add affiliate_request_id to sale.order form view
    * Add request sequence record, make default for new affiliates
    * Add access rights to sales users/managers
    * Add tests
    * Add readme instructions
    
    * [IMP] website_sale_affiliate: Make requested changes
    * Update readme
    * Add controller override to catch product urls
    * Remove redundant return None
    * Replace if statements with try/except when appropriate
    * Change api decorator on sale.affiliate.find_from_session method
      from model -> model_cr_context
    * Add check to avoid unnecessary db search in find_from_session if
      no affiliate_id
    * Fix tests
    * Add new tests
    
    * [IMP] website_sale_affiliate: Make requested changes
    * Un-nest try/except statements for readability
    * Rename kwargs: ref, key -> aff_ref, aff_key
    * Improve request search speed for large recordsets
    * Document similarly-named methods for clarity
    * Tweak tests as needed
    * Add additional test
    * Add roadmap to readme
    
    * [REM] website_sale_affiliate: Remove affiliate request IP-matching
    
    * [IMP] website_sale_affiliate: Change affiliate request creation
    * Create affiliate requests regardless of whether a sale occurs
    * Add basic conversion/sales stats to affiliate model and views
    * Update tests
    * Add demo data
    * Clean up views
    * Remove line used for debugging
    * Add field string for clarity
    
    * [FIX] website_sale_affiliate: Fix compatibility issues, tests
    * Fix controller method overrides to avoid conflicts
    * Match assertAlmostEqual decimal places to amount used in fields
    * Remove/replace tests with unneeded url_open calls
    * Rename files, variables for clarity
    * Clean up demo/test data
    
    * [IMP] website_sale_affiliate: Clarify affiliate request name
    * Add help to name field on affiliate request model

---
+++ website_sale_affiliate/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+
+from . import sale_affiliate
+from . import sale_affiliate_request
+from . import sale_order
---
+++ website_sale_affiliate/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+
+from . import test_sale_affiliate
+from . import test_sale_affiliate_request
+from . import test_sale_order
+from . import test_website_sale
