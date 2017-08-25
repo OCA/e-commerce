b2911941f9a63e1ebcc8823dd92a8538b0851252
Brenton Hughes <brenton.hughes@gmail.com>
2017-08-25 09:30:03 -0700

    [ADD] website_sale_affiliate: Create module (#199)
    * [ADD] website_sale_affiliate: Create module
    * Add sale.affiliate and sale.affiliate.request models
    * Override WebsiteSale controller shop method to store affiliate
      id & key kwargs of shop urls in session
    * Override sale order create method to store affiliate request
    * Add affiliate and request views, links in Sales/Affiliates
    * Add affiliate_request_id to sale.order form view
    * Add request sequence record, make default for new affiliates
    * Add access rights to sales users/managers
    * Add tests
    * Add readme instructions
    
    * [IMP] website_sale_affiliate: Make requested changes
    * Update readme
    * Add controller override to catch product urls
    * Remove redundant return None
    * Replace if statements with try/except when appropriate
    * Change api decorator on sale.affiliate.find_from_session method
      from model -> model_cr_context
    * Add check to avoid unnecessary db search in find_from_session if
      no affiliate_id
    * Fix tests
    * Add new tests
    
    * [IMP] website_sale_affiliate: Make requested changes
    * Un-nest try/except statements for readability
    * Rename kwargs: ref, key -> aff_ref, aff_key
    * Improve request search speed for large recordsets
    * Document similarly-named methods for clarity
    * Tweak tests as needed
    * Add additional test
    * Add roadmap to readme
    
    * [REM] website_sale_affiliate: Remove affiliate request IP-matching
    
    * [IMP] website_sale_affiliate: Change affiliate request creation
    * Create affiliate requests regardless of whether a sale occurs
    * Add basic conversion/sales stats to affiliate model and views
    * Update tests
    * Add demo data
    * Clean up views
    * Remove line used for debugging
    * Add field string for clarity
    
    * [FIX] website_sale_affiliate: Fix compatibility issues, tests
    * Fix controller method overrides to avoid conflicts
    * Match assertAlmostEqual decimal places to amount used in fields
    * Remove/replace tests with unneeded url_open calls
    * Rename files, variables for clarity
    * Clean up demo/test data
    
    * [IMP] website_sale_affiliate: Clarify affiliate request name
    * Add help to name field on affiliate request model

---
+++ website_sale_affiliate/tests/test_sale_order.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+
+from mock import patch
+
+from ..models.sale_affiliate_request import AffiliateRequest
+from .common import SaleCase
+
+
+class SaleOrderCase(SaleCase):
+    def setUp(self):
+        super(SaleOrderCase, self).setUp()
+        self.partner = self.env.ref('base.res_partner_1')
+        self.sale_order_vals = {
+            'partner_id': self.partner.id,
+            'partner_invoice_id': self.partner.id,
+            'partner_shipping_id': self.partner.id,
+            'order_line': [
+                (
+                    0,
+                    0,
+                    {
+                        'name': self.demo_product.name,
+                        'product_id': self.demo_product.id,
+                        'product_uom_qty': 2,
+                        'product_uom': self.demo_product.uom_id.id,
+                        'price_unit': self.demo_product.list_price,
+                    },
+                )
+            ],
+            'pricelist_id': self.env.ref('product.list0').id,
+        }
+
+    @patch.object(AffiliateRequest, 'current_qualified')
+    def test_create_calls_current_qualified(self, current_qualified_mock):
+        """Calls current_qualified() on sale.affiliate.request model"""
+        current_qualified_mock.return_value = None
+        self.env['sale.order'].create(self.sale_order_vals)
+        current_qualified_mock.assert_called_once_with()
+
+    @patch.object(AffiliateRequest, 'current_qualified')
+    def test_create_adds_none(self, current_qualified_mock):
+        """Sets affiliate_request_id to False when no current qualified
+        affiliate request"""
+        current_qualified_mock.return_value = None
+        sale_order = self.env['sale.order'].create(self.sale_order_vals)
+        self.assertTrue(sale_order.exists(), 'Sale order not created')
+        self.assertFalse(
+            sale_order.affiliate_request_id,
+            'Sale order not created properly',
+        )
+
+    @patch.object(AffiliateRequest, 'current_qualified')
+    def test_create_adds_affiliate_request_id(self, current_qualified_mock):
+        """Adds id of qualified affiliate request to sale order"""
+        current_qualified_mock.return_value = self.demo_request
+        sale_order = self.env['sale.order'].create(self.sale_order_vals)
+        self.assertTrue(sale_order.exists(), 'Sale order not created')
+        self.assertEqual(
+            sale_order.affiliate_request_id,
+            self.demo_request,
+            'Request ID not added to sale order',
+        )
