5b38d704de2bb0e892884a5c0c0be0a0588e5d97
Brenton Hughes <brenton.hughes@gmail.com>
2017-08-25 09:30:03 -0700

    [ADD] website_sale_affiliate: Create module (#199)
    * [ADD] website_sale_affiliate: Create module
    * Add sale.affiliate and sale.affiliate.request models
    * Override WebsiteSale controller shop method to store affiliate
      id & key kwargs of shop urls in session
    * Override sale order create method to store affiliate request
    * Add affiliate and request views, links in Sales/Affiliates
    * Add affiliate_request_id to sale.order form view
    * Add request sequence record, make default for new affiliates
    * Add access rights to sales users/managers
    * Add tests
    * Add readme instructions
    
    * [IMP] website_sale_affiliate: Make requested changes
    * Update readme
    * Add controller override to catch product urls
    * Remove redundant return None
    * Replace if statements with try/except when appropriate
    * Change api decorator on sale.affiliate.find_from_session method
      from model -> model_cr_context
    * Add check to avoid unnecessary db search in find_from_session if
      no affiliate_id
    * Fix tests
    * Add new tests
    
    * [IMP] website_sale_affiliate: Make requested changes
    * Un-nest try/except statements for readability
    * Rename kwargs: ref, key -> aff_ref, aff_key
    * Improve request search speed for large recordsets
    * Document similarly-named methods for clarity
    * Tweak tests as needed
    * Add additional test
    * Add roadmap to readme
    
    * [REM] website_sale_affiliate: Remove affiliate request IP-matching
    
    * [IMP] website_sale_affiliate: Change affiliate request creation
    * Create affiliate requests regardless of whether a sale occurs
    * Add basic conversion/sales stats to affiliate model and views
    * Update tests
    * Add demo data
    * Clean up views
    * Remove line used for debugging
    * Add field string for clarity
    
    * [FIX] website_sale_affiliate: Fix compatibility issues, tests
    * Fix controller method overrides to avoid conflicts
    * Match assertAlmostEqual decimal places to amount used in fields
    * Remove/replace tests with unneeded url_open calls
    * Rename files, variables for clarity
    * Clean up demo/test data
    
    * [IMP] website_sale_affiliate: Clarify affiliate request name
    * Add help to name field on affiliate request model

---
+++ website_sale_affiliate/models/sale_affiliate_request.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+
+from datetime import timedelta
+
+from odoo import api, fields, models
+from odoo.http import request
+
+
+class AffiliateRequest(models.Model):
+    _name = 'sale.affiliate.request'
+    _order = 'create_date desc'
+
+    name = fields.Char(
+        required=True,
+        index=True,
+        help='Name corresponds with the "aff_key" value of the url used, if '
+        'present. Otherwise, name is determined by the sequence selected in '
+        'the parent affiliate record',
+    )
+    affiliate_id = fields.Many2one(
+        'sale.affiliate',
+        string='Affiliate',
+        required=True,
+        help='Affiliate that referred request',
+    )
+    date = fields.Datetime(
+        string='Start Date',
+        required=True,
+        default=lambda self: fields.Datetime.now(),
+        help='Date and time of initial request',
+    )
+    ip = fields.Char(
+        string='Client IP',
+        required=True,
+        default=lambda self: request.httprequest.headers.environ.get(
+            'REMOTE_ADDR',
+        ),
+    )
+    referrer = fields.Char(
+        default=lambda self: request.httprequest.headers.environ.get(
+            'HTTP_REFERER',
+        ),
+        help='Request session referrer header',
+    )
+    user_agent = fields.Char(
+        required=True,
+        default=lambda self: request.httprequest.headers.environ.get(
+            'HTTP_USER_AGENT',
+        ),
+        help='Request session user agent',
+    )
+    accept_language = fields.Char(
+        required=True,
+        default=lambda self: request.httprequest.headers.environ.get(
+            'HTTP_ACCEPT_LANGUAGE',
+        ),
+        help='Request session accept language',
+    )
+    sale_ids = fields.One2many(
+        'sale.order',
+        'affiliate_request_id',
+        string='Sales',
+        help='Qualified conversions generated as a result of affiliate request'
+    )
+
+    @api.multi
+    def _conversions_qualify(self):
+        self.ensure_one()
+
+        valid_hours = self.affiliate_id.valid_hours
+        valid_sales = self.affiliate_id.valid_sales
+        datetime_start = fields.Datetime.from_string(self.date)
+        datetime_delta = timedelta(hours=valid_hours)
+        expiration = fields.Datetime.to_string(datetime_start + datetime_delta)
+
+        qualified_sales = valid_sales < 0 or len(self.sale_ids) < valid_sales
+        qualified_time = valid_hours < 0 or fields.Datetime.now() < expiration
+
+        return qualified_sales and qualified_time
+
+    @api.model_cr_context
+    def current_qualified(self):
+        try:
+            current_id = request.session['affiliate_request']
+            current = self.search([('id', '=', current_id)], limit=1)
+        except KeyError:
+            return
+        if current._conversions_qualify():
+            return current
+        return
