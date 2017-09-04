c3539853ba212fd7ef38c1be4b8bac0bc4c59a8e
eLBati <lorenzo.battistini@agilebg.com>
2017-09-04 11:14:15 +0200

    Porting website_sale_checkout_skip_payment to 10.0

--- website_sale_checkout_skip_payment/controllers/main.py
+++ website_sale_checkout_skip_payment/controllers/main.py
@@ -3,22 +3,21 @@
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp import http
-from openerp.http import request
-from openerp.addons.website_sale.controllers.main import website_sale
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class CheckoutSkipPayment(website_sale):
+class CheckoutSkipPayment(WebsiteSale):
 
     @http.route()
     def payment(self, **post):
         if not request.website.checkout_skip_payment:
             return super(CheckoutSkipPayment, self).payment(**post)
-        context = request.context
-        order = request.website.sale_get_order(context=context)
+        order = request.website.sale_get_order()
         if order.force_quotation_send():
-            # Clean context and session, then redirect to the confirmation page
-            request.website.sale_reset(context=context)
+            # Clean session, then redirect to the confirmation page
+            request.website.sale_reset()
             return request.redirect('/shop/confirmation')
         else:
             return request.render(
