9cd5719692d33ab9c0cbab9d3b32e6a6532cb39e
Jairo Llopis <yajo.sk8@gmail.com>
2017-09-06 12:58:05 +0200

    [MIG][website_sale_require_legal] Migrate to v10

--- website_sale_require_legal/controllers/main.py
+++ website_sale_require_legal/controllers/main.py
@@ -1,44 +1,36 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2015-2017 Tecnativa - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.http import request
-from openerp.sql_db import TestCursor
-from openerp.addons.website_sale.controllers.main import website_sale
+from odoo.http import request
+from odoo.addons.website_sale.controllers import main
 
 
-class RequireLegalTermsToCheckout(website_sale):
-    def checkout_parse(self, address_type, data, remove_prefix=False,
-                       *args, **kwargs):
-        """Require accepting legal terms to buy."""
-        result = (super(RequireLegalTermsToCheckout, self).
-                  checkout_parse(address_type, data, remove_prefix,
-                                 *args, **kwargs))
+class WebsiteSale(main.WebsiteSale):
+    def _get_mandatory_billing_fields(self):
+        result = super(WebsiteSale, self)._get_mandatory_billing_fields()
+        return result + self._mandatory_legal_terms()
 
-        # Avoid PhantomJS errors in test mode
-        if (not isinstance(request.env.cr, TestCursor) and
-                address_type == "billing"):
-            result["accepted_legal_terms"] = (
-                bool(request.params.get("accepted_legal_terms")))
+    def _get_mandatory_shipping_fields(self):
+        result = super(WebsiteSale, self)._get_mandatory_shipping_fields()
+        return result + self._mandatory_legal_terms()
 
+    def _mandatory_legal_terms(self):
+        """Require ``accepted_legal_terms`` only if we are validating."""
+        result = []
+        if request.context.get("needs_legal"):
+            result.append("accepted_legal_terms")
         return result
 
-    def checkout_form_save(self, checkout, *args, **kwargs):
-        """Do not use accepting legal terms to save."""
-        if "accepted_legal_terms" in checkout:
-            del checkout["accepted_legal_terms"]
-
-        return super(RequireLegalTermsToCheckout, self).checkout_form_save(
-            checkout, *args, **kwargs)
-
-    def checkout_form_validate(self, data, *args, **kwargs):
-        """Require accepting legal terms to buy."""
-        errors = (super(RequireLegalTermsToCheckout, self)
-                  .checkout_form_validate(data, *args, **kwargs))
-
-        # If it is ``None``, then there is no need to check it
-        if data.get("accepted_legal_terms") is False:
-            errors["accepted_legal_terms"] = "missing"
-
-        return errors
+    def checkout_form_validate(self, mode, all_form_values, data):
+        """Require accepting legal terms to validate form."""
+        # Patch context
+        old_context = request.context
+        new_context = dict(request.context, needs_legal=True)
+        request.context = new_context
+        result = super(WebsiteSale, self).checkout_form_validate(
+            mode, all_form_values, data)
+        # Unpatch context
+        request.context = old_context
+        return result
