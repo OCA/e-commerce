8f853bccb25165bc929b41e3ca45108bacbab9c7
Jairo Llopis <yajo.sk8@gmail.com>
2017-09-06 12:58:05 +0200

    [MIG][website_sale_require_legal] Migrate to v10

--- website_sale_require_legal/__manifest__.py
+++ website_sale_require_legal/__manifest__.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2015, 2017 Tecnativa - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Require accepting legal terms to checkout",
     "summary": "Force the user to accept legal tems to buy in the web shop",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Website",
-    "website": "http://www.tecnativa.com",
-    "author": "Antiun Ingeniería S.L., "
-              "Tecnativa, "
+    "website": "https://www.tecnativa.com",
+    "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
+        "web_tour",
         "website_legal_page",
         "website_sale",
     ],
     "data": [
         "views/website_sale.xml",
+    ],
+    "demo": [
+        "demo/assets.xml",
     ],
 }
