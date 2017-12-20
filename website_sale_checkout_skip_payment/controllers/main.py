4eb984acc72300c114599a8191dc7c235854022c
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2017-12-20 23:35:27 +0100

    [9.0][FIX] website_sale_checkout_skip_paymen: Message error in checkout confirmation step when partner is has skip payment flag

--- website_sale_checkout_skip_payment/controllers/main.py
+++ website_sale_checkout_skip_payment/controllers/main.py
@@ -21,3 +21,16 @@ class CheckoutSkipPayment(WebsiteSale):
         else:
             return request.render(
                 'website_sale_skip_payment.confirmation_order_error')
+
+    @http.route()
+    def payment_get_status(self, sale_order_id, **post):
+        # When skip payment step, the transaction not exists so only render
+        # the waiting message in ajax json call
+        if not request.website.checkout_skip_payment:
+            return super(CheckoutSkipPayment, self).payment_get_status(
+                sale_order_id, **post)
+        return {
+            'recall': True,
+            'message': request.website._render(
+                'website_sale_checkout_skip_payment.order_state_message'),
+        }
