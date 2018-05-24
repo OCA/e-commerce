73edf57d5193d56776ae36bf50003a17ed4d3436
ernesto <ernesto.tejeda87@gmail.com>
2018-05-24 00:51:19 -0400

    [MIG] website_sale_vat_required: Migration to 11.0

--- website_sale_vat_required/__manifest__.py
+++ website_sale_vat_required/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,19 +5,22 @@
 {
     'name': 'e-commerce required VAT',
     'summary': 'VAT number required in checkout form',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Website',
     'author': "Agile Business Group, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
+    'website': 'https://github.com/OCA/e-commerce',
     'license': 'AGPL-3',
     'depends': [
         'website_sale',
         'base_vat',
     ],
+    'data': [
+        'views/templates.xml',
+    ],
     'demo': [
-        'demo/assets.xml',
+        'demo/demo_assets.xml',
     ],
     'installable': True,
     'auto_install': False,
--- website_sale_vat_required/controllers/__init__.py
+++ website_sale_vat_required/controllers/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
