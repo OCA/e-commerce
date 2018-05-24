c32898fb1e57405d9ebc374aa4e04cecb9222a16
ernesto <ernesto.tejeda87@gmail.com>
2018-05-24 00:51:19 -0400

    [MIG] website_sale_vat_required: Migration to 11.0

--- website_sale_vat_required/controllers/main.py
+++ website_sale_vat_required/controllers/main.py
@@ -1,22 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.addons.website_sale.controllers.main import website_sale
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class WebsiteSale(website_sale):
+class WebsiteSale(WebsiteSale):
     def _get_mandatory_billing_fields(self):
         result = super(WebsiteSale, self)._get_mandatory_billing_fields()
         result.append("vat")
-        return result
-
-    def _get_optional_billing_fields(self):
-        result = super(WebsiteSale, self)._get_optional_billing_fields()
-        try:
-            result.remove("vat")
-        except ValueError:  # pragma: no-cover
-            # If any other addon removed it already, do not die
-            pass
         return result
