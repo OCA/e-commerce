04a416b3fffb2149f074dd3b21eef0da2f9dd677
ernesto <ernesto.tejeda87@gmail.com>
2018-05-25 12:01:24 -0400

    [MIG] website_sale_default_country: Migration to 11.0

--- website_sale_default_country/__manifest__.py
+++ website_sale_default_country/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015, 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "eCommerce Default Country",
     "summary": "Give a default value for country at checkout",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Website",
-    "website": "https://www.tecnativa.com",
+    "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
