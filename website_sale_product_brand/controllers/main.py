b5b5b17c7b400c6b37006758db4f2a4bc477e1a5
ernesto <ernesto.tejeda87@gmail.com>
2018-06-02 00:23:57 -0400

    [MIG] website_sale_product_brand: Migration to 11.0
    [UPD] Update website_sale_product_brand.pot
    
    [UPD] README.rst
    
    Translated using Weblate (Persian)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: e-commerce-11.0/e-commerce-11.0-website_sale_product_brand
    Translate-URL: https://translation.odoo-community.org/projects/e-commerce-11-0/e-commerce-11-0-website_sale_product_brand/fa/

--- website_sale_product_brand/controllers/main.py
+++ website_sale_product_brand/controllers/main.py
@@ -1,15 +1,20 @@
-# -*- coding: utf-8 -*-
 # © 2016 Serpent Consulting Services Pvt. Ltd. (http://www.serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import http
-from openerp.http import request
-from openerp import SUPERUSER_ID
-from openerp.addons.website_sale.controllers.main import QueryURL
-from openerp.addons.website_sale.controllers.main import website_sale
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import QueryURL
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-class WebsiteSale(website_sale):
+class WebsiteSale(WebsiteSale):
+    def _get_search_domain(self, search, category, attrib_values):
+        domain = super(WebsiteSale, self)._get_search_domain(search, category,
+                                                             attrib_values)
+        if 'brand_id' in request.env.context:
+            domain.append(
+                ('product_brand_id', '=', request.env.context['brand_id']))
+        return domain
 
     @http.route(['/shop',
                  '/shop/page/<int:page>',
@@ -36,21 +41,16 @@ class WebsiteSale(website_sale):
         auth='public',
         website=True)
     def product_brands(self, **post):
-        cr, context, pool = (request.cr,
-                             request.context,
-                             request.registry)
-        b_obj = pool['product.brand']
+        b_obj = request.env['product.brand']
         domain = []
         if post.get('search'):
             domain += [('name', 'ilike', post.get('search'))]
-        brand_ids = b_obj.search(cr, SUPERUSER_ID, domain)
-        brand_rec = b_obj.browse(cr, SUPERUSER_ID, brand_ids, context=context)
+        brand_rec = b_obj.search(domain)
 
         keep = QueryURL('/page/product_brands', brand_id=[])
         values = {'brand_rec': brand_rec,
                   'keep': keep}
         if post.get('search'):
             values.update({'search': post.get('search')})
-        return request.website.render(
-            'website_sale_product_brand.product_brands',
-            values)
+        return request.render('website_sale_product_brand.product_brands',
+                              values)
