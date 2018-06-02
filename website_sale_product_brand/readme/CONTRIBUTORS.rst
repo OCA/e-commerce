f968a801a468fce2665fdada93e634c4e89cbc1d
ernesto <ernesto.tejeda87@gmail.com>
2018-06-02 00:23:57 -0400

    [MIG] website_sale_product_brand: Migration to 11.0
    [UPD] Update website_sale_product_brand.pot
    
    [UPD] README.rst
    
    Translated using Weblate (Persian)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: e-commerce-11.0/e-commerce-11.0-website_sale_product_brand
    Translate-URL: https://translation.odoo-community.org/projects/e-commerce-11-0/e-commerce-11-0-website_sale_product_brand/fa/

---
+++ website_sale_product_brand/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* Jay Vora <jay.vora@serpentcs.com>
+* Meet Dholakia <m.dholakia.serpentcs@gmail.com>
+* `Tecnativa <https://www.tecnativa.com>`_:
+
+    * Ernesto Tejeda <ernesto.tejeda87@gmail.com>
---
+++ website_sale_product_brand/static/src/js/tour.js
@@ -0,0 +1,40 @@
+/* License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
+
+ odoo.define("website_sale_product_brand.tour", function (require) {
+    "use strict";
+
+    var tour = require("web_tour.tour");
+
+    var steps = [
+        {
+            trigger: "a[href='/page/product_brands']",
+            content: "Go to 'Product brand' page",
+            position: "bottom",
+        },
+        {
+            content: "search Apple",
+            trigger: 'form input[name="search"]',
+            run: "text Apple",
+            position: "bottom",
+        },
+        {
+            content: "Click to search Apple",
+            trigger: 'form:has(input[name="search"]) button',
+            position: "bottom",
+        },
+        {
+            content: "select Apple",
+            trigger: 'section a div:contains("Apple")',
+        },
+    ];
+
+    tour.register(
+        "website_sale_product_brand",
+        {
+            url: "/",
+            test: true,
+        },
+        steps
+    );
+
+});
