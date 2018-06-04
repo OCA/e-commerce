bf19d5fa0a9fe9851e72c0b6c92968fd8f8b8dd0
eLBati <lorenzo.battistini@agilebg.com>
2018-06-04 16:54:54 +0200

    ADD website_sale_charge_payment_fee
    ADD website_sale_charge_payment_fee_delivery: link module
    
    ADD website_sale_charge_payment_fee_quote: link module

---
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "eCommerce: charge payment fee",
+    "summary": "Payment fee charged to customer",
+    "version": "10.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Website",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/payment_view.xml",
+        "templates/website_sale.xml"
+    ],
+}
