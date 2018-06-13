04df5200dfca432b260ddedcc44b42d0f4f7eebe
AbrahamTrey <abraham@trey.es>
2018-06-13 11:48:59 +0200

    [MIG] website_sale_hide_price: Migration to 11.0

--- website_sale_hide_price/models/res_partner.py
+++ website_sale_hide_price/models/res_partner.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import fields, models
 
 
@@ -10,5 +9,5 @@ class ResPartner(models.Model):
 
     website_show_price = fields.Boolean(
         string='Show prices on website',
-        default=True,
+        default=True
     )
