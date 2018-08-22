124ac45e44b944bc3fc27a274c8873d7b6a8efc0
David <david.vidal@tecnativa.com>
2018-08-22 14:45:50 +0200

    fixup! fixup! [ADD][10.0] website_sale_portal_show_company_data: New Module

--- website_sale_show_company_data/__manifest__.py
+++ website_sale_show_company_data/__manifest__.py
@@ -2,8 +2,8 @@
 # Copyright 2018 - Tecnativa
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Website Sale Portal Show Company Data",
-    "summary": "Show company partner data if any",
+    "name": "Website Sale Show Company Data",
+    "summary": "Show commercial partner data if any",
     "version": "10.0.1.0.0",
     "category": "E-Commerce",
     "website": "https://github.com/OCA/e-commerce",
