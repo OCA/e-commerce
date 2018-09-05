321c7c766c28c92575dea66a99a064c3e032a082
David Vidal <david.vidal@tecnativa.com>
2018-09-05 12:19:42 +0200

    [ADD][10.0] website_sale_show_company_data: New Module (#243)

---
+++ website_sale_show_company_data/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 - Tecnativa
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Show Company Data",
+    "summary": "Show commercial partner data if any",
+    "version": "10.0.1.0.0",
+    "category": "E-Commerce",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/website_sale_address.xml",
+    ],
+}
---
+++ website_sale_show_company_data/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+When portal users that belong to a company edit their invoicing address during
+the checkout proccess there is no clue whether the order is being billed to
+their company or to the user themselves.
+
+This module adds the company name and VAT info in readonly mode to give a
+reference to the user.
