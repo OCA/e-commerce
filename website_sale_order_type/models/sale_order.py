c7e83ef29f688ebfccba073b8ad4c61b48277f6c
SimoRubi <simone.rubino@agilebg.com>
2018-10-16 16:22:54 +0200

    [ADD] website_sale_order_type

---
+++ website_sale_order_type/models/sale_order.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _cart_update(
+            self, product_id=None, line_id=None, add_qty=0, set_qty=0,
+            attributes=None, **kwargs):
+        self.ensure_one()
+        res = super(SaleOrder, self)._cart_update(
+            product_id=product_id, line_id=line_id,
+            add_qty=add_qty, set_qty=set_qty, attributes=attributes, **kwargs)
+        self.onchange_partner_id()
+        self.onchange_type_id()
+        return res
---
+++ website_sale_order_type/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows `sale_order_type <https://github.com/OCA/sale-workflow/tree/10.0/sale_order_type>`__ to work with website_sale.
