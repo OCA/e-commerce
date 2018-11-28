1721520cb1db7f025f9be2ab7aabb599321c2f05
Francois Delarbre <francois@delarbre.biz>
2018-11-28 22:26:03 +0100

    [IMP] hide only categories that are "recurcively empty" A category is recurcively empty if it has no products and all its childs are "recurcively empty"

--- website_sale_hide_empty_category/models/product_public_category.py
+++ website_sale_hide_empty_category/models/product_public_category.py
@@ -1,7 +1,7 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductPublicCategory(models.Model):
@@ -16,3 +16,17 @@ class ProductPublicCategory(models.Model):
         column1='product_public_category_id',
         column2='product_template_id',
     )
+
+    has_products_recurcive = fields.Boolean(
+        string='This catégory or one of its children has product',
+        compute='_has_products_recurcive'
+    )
+
+    @api.depends('product_ids', 'child_id.has_products_recurcive')
+    def _has_products_recurcive(self):
+        for category in self:
+            category.has_products_recurcive = \
+                bool(category.product_ids or any((child.has_products_recurcive
+                                                  for child in
+                                                  category.child_id)))
+
