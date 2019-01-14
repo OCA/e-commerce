d79303d01c56553387c1589e84449cb2011d241c
Simone Rubino <simone.rubino@agilebg.com>
2019-01-14 11:53:16 +0100

    [ADD] website_sale_order_type (#253)

---
+++ website_sale_order_type/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website sale order type",
+    "summary": "This module allows sale_order_type to work with website_sale.",
+    "version": "10.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Website",
+    "website": "https://github.com/OCA/e-commerce/tree/"
+               "10.0/website_sale_order_type",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale",
+        "sale_order_type"
+    ],
+    "auto_install": True
+}
---
+++ website_sale_order_type/readme/USAGE.rst
@@ -0,0 +1,3 @@
+#. In the backend (Sales -> Customers) set a sale order type in any contact;
+#. Log in with the associated user in the e-commerce website and buy a product;
+#. Check that the created sale oder has the same sale order type that has been set in the contact.
