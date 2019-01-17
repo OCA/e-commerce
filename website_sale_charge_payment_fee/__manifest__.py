3db31a1fde4353d12b6e1d74338a919c7a0bf6f7
Simone Rubino <simone.rubino@agilebg.com>
2019-01-17 12:59:14 +0100

    [FIX] website_sale_charge_payment_fee: Use a browse record if there are no acquirers

--- website_sale_charge_payment_fee/__manifest__.py
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "eCommerce: charge payment fee",
     "summary": "Payment fee charged to customer",
-    "version": "10.0.1.0.1",
+    "version": "10.0.1.0.2",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
