f5fc4e2f3e562c2f110ecf78c2d23b24ae35d289
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-26 11:27:40 +0100

    [12.0][MIG] website_sale_require_legal: Migration to v12

--- website_sale_require_legal/__manifest__.py
+++ website_sale_require_legal/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2015, 2017 Tecnativa - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
+# Copyright 2019 Tecnativa - Cristina Martin R.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Require accepting legal terms to checkout",
     "summary": "Force the user to accept legal tems to buy in the web shop",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, "
@@ -19,6 +20,7 @@
     ],
     "data": [
         "views/website_sale.xml",
+        "data/auth_signup_data.xml"
     ],
     "demo": [
         "demo/assets.xml",
