4566e9379d8080ce47ec4af00982040abb65909b
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-03-04 12:51:21 +0100

    [11.0][ADD] website_sale_secondary_unit: New module

---
+++ website_sale_secondary_unit/tests/test_website_sale_secondary_unit.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.tests.common import HttpCase
+
+
+class WebsiteSaleSecondaryUnitHttpCase(HttpCase):
+
+    def test_ui_website(self):
+        """Test frontend tour."""
+        tour = (
+            "odoo.__DEBUG__.services['web_tour.tour']",
+            "website_sale_secondary_unit",
+        )
+        self.phantom_js(
+            url_path="/",
+            code="%s.run('%s')" % tour,
+            ready="%s.tours['%s'].ready" % tour,
+            login="admin")
