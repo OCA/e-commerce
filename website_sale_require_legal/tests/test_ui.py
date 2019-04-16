fc602c4126b356dda6409ee9b4ee4a49958d1d04
Jairo Llopis <yajo.sk8@gmail.com>
2019-04-16 13:22:37 +0100

    [FIX] website_sale_require_legal: Turn tour green

--- website_sale_require_legal/tests/test_ui.py
+++ website_sale_require_legal/tests/test_ui.py
@@ -7,24 +7,22 @@ from odoo.tests.common import HttpCase
 class UICase(HttpCase):
     def setUp(self):
         """Ensure website lang is en_US."""
-        super(UICase, self).setUp()
-        with self.cursor() as cr:
-            env = self.env(cr)
-            website = env["website"].get_current_website()
-            wiz = env["base.language.install"].create({
-                "lang": "en_US",
-            })
-            wiz.website_ids = website
-            wiz.lang_install()
-            website.default_lang_id = env.ref("base.lang_en")
+        super().setUp()
+        website = self.env["website"].get_current_website()
+        wiz = self.env["base.language.install"].create({
+            "lang": "en_US",
+        })
+        wiz.website_ids = website
+        wiz.lang_install()
+        website.default_lang_id = self.env.ref("base.lang_en")
 
     def test_ui_website(self):
         """Test frontend tour."""
         tour = "website_sale_require_legal"
-        self.phantom_js(
+        self.browser_js(
             url_path="/shop",
             code="odoo.__DEBUG__.services['web_tour.tour']"
                  ".run('%s')" % tour,
             ready="odoo.__DEBUG__.services['web_tour.tour']"
                   ".tours.%s.ready" % tour,
-            login="admin")
+        )
