11095cee3d40a3fbafee7fabafb51cd584622b10
Victor Martin <victor.martin@tecnativa.com>
2019-05-21 16:22:47 +0200

    [MIG] website_sale_show_company_data: Migration to 12.0

--- website_sale_show_company_data/__manifest__.py
+++ website_sale_show_company_data/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 - Tecnativa
+# Copyright 2018-2019 - Tecnativa
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Website Sale Show Company Data",
     "summary": "Show commercial partner data if any",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "E-Commerce",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, "
