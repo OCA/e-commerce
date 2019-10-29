db320df915b3fb2f09bf69f0ee29710a95696b60
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-10-29 12:00:09 +0100

    [MIG] website_sale_stock_available_display: Migration to v12.0

--- website_sale_stock_available_display/static/src/js/website_sale_stock_available_display_tour.js
+++ website_sale_stock_available_display/static/src/js/website_sale_stock_available_display_tour.js
@@ -6,43 +6,21 @@ odoo.define("website_sale_stock_available_display.tour", function (require) {
 
     var tour = require("web_tour.tour");
     var base = require("web_editor.base");
-    var rpc = require("web.rpc");
 
     var steps = [
         {
             trigger: "a:contains('Computer Motherboard')",
-            run: function(actions){
-                    rpc.query({
-                        model: 'res.partner',
-                        method: 'write',
-                        args: [[3], {'vat': false}]})
-                        .then(function(data){
-                            actions.auto();
-                        })
-                    }
         },
         {
             trigger: "a#add_to_cart",
             extra_trigger: ".availability_messages:has(span:contains('0 Unit(s) in stock')):has(div:contains('Available in 10 days'))",
         },
         {
-            trigger: "a[href='/shop/checkout']",
+            trigger: "span:contains('Process Checkout')",
             extra_trigger: ".availability_messages:has(span:contains('0 Unit(s) in stock'))",
         },
-        // To compatibility with website_sale_vat_required
         {
-            trigger: "div.div_vat input[name='vat']",
-            run: function(actions){
-                $('div.div_vat input[name="vat"]').val("BE0477472701");
-                $('#div_phone input').val('11111111');
-                $('div input[name="accepted_legal_terms"]').prop( "checked", true );
-            },
-        },
-        {
-            trigger: ".btn-primary:contains('Next')",
-        },
-        {
-            trigger: "a[href='/shop/confirm_order']",
+            trigger: ".btn-primary:contains('Confirm Order')",
         },
         {
             trigger: "a[href='/shop']",
@@ -56,11 +34,11 @@ odoo.define("website_sale_stock_available_display.tour", function (require) {
             extra_trigger: ".availability_messages:has(span:contains('10 Unit(s) in stock'))",
         },
         {
-            trigger: "a[href='/shop/checkout']",
+            trigger: "span:contains('Process Checkout')",
             extra_trigger: ".availability_messages:has(span:contains('10.0 Unit(s) in stock'))",
         },
         {
-            trigger: "a[href='/shop/confirm_order']",
+            trigger: ".btn-primary:contains('Confirm Order')",
         },
         {
             trigger: "a[href='/shop']",
