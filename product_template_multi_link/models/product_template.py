a26cd87f96014d6cd787057e8c91a472e7dd314c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-15 10:29:09 +0100

    [IMP] product_template_multi_link: : black, isort

--- product_template_multi_link/models/product_template.py
+++ product_template_multi_link/models/product_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-Today GRAP (http://www.grap.coop).
 # @author Sylvain LE GAL <https://twitter.com/legalsylvain>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -7,18 +6,21 @@ from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     product_template_link_ids = fields.One2many(
-        string='Product Links', comodel_name='product.template.link',
-        inverse_name='product_template_id')
+        string="Product Links",
+        comodel_name="product.template.link",
+        inverse_name="product_template_id",
+    )
 
     product_template_link_qty = fields.Integer(
-        string='Product Links Quantity', compute='_product_template_link_qty',
-        store=True)
+        string="Product Links Quantity",
+        compute="_compute_product_template_link_qty",
+        store=True,
+    )
 
-    @api.depends('product_template_link_ids')
-    def _product_template_link_qty(self):
+    @api.depends("product_template_link_ids")
+    def _compute_product_template_link_qty(self):
         for template in self:
-            template.product_template_link_qty =\
-                len(template.product_template_link_ids)
+            template.product_template_link_qty = len(template.product_template_link_ids)
