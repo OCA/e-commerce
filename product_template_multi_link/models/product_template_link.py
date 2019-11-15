52d58046a251fb074eac76535a7ee42fef44fcf1
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-15 10:29:09 +0100

    [IMP] product_template_multi_link: : black, isort

--- product_template_multi_link/models/product_template_link.py
+++ product_template_multi_link/models/product_template_link.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-Today GRAP (http://www.grap.coop).
 # @author Sylvain LE GAL <https://twitter.com/legalsylvain>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -7,37 +6,48 @@ from odoo import fields, models
 
 
 class ProductTemplateLink(models.Model):
-    _name = 'product.template.link'
-    _order = 'product_template_id, linked_product_template_id'
+    _name = "product.template.link"
+    _order = "product_template_id, linked_product_template_id"
 
-    _LINK_TYPE_SELECTION = [
-        ('cross_sell', 'Cross-Sell'),
-        ('up_sell', 'Up-Sell'),
-    ]
+    _LINK_TYPE_SELECTION = [("cross_sell", "Cross-Sell"), ("up_sell", "Up-Sell")]
 
     product_template_id = fields.Many2one(
-        string='Source Product', comodel_name='product.template',
-        required=True, ondelete='cascade')
+        string="Source Product",
+        comodel_name="product.template",
+        required=True,
+        ondelete="cascade",
+    )
 
     product_template_image_small = fields.Binary(
-        related='product_template_id.image_small')
+        related="product_template_id.image_small"
+    )
 
     linked_product_template_id = fields.Many2one(
-        string='Linked Product', comodel_name='product.template',
-        required=True, ondelete='cascade')
+        string="Linked Product",
+        comodel_name="product.template",
+        required=True,
+        ondelete="cascade",
+    )
 
     linked_product_template_image_small = fields.Binary(
-        related='linked_product_template_id.image_small')
+        related="linked_product_template_id.image_small"
+    )
 
     link_type = fields.Selection(
-        string='Link Type', selection=_LINK_TYPE_SELECTION, required=True,
-        default='cross_sell', help="* Cross-Sell : suggest your customer to"
+        string="Link Type",
+        selection=_LINK_TYPE_SELECTION,
+        required=True,
+        default="cross_sell",
+        help="* Cross-Sell : suggest your customer to"
         " purchase an additional product\n"
         "* Up-Sell : suggest your customer to purchase a higher-end product,"
-        "  an upgrade, etc.")
+        "  an upgrade, etc.",
+    )
 
-    sql_constraints = [(
-        'template_link_uniq',
-        'unique (product_template_id, linked_product_template_id, link_type)',
-        'The products and the link type combination must be unique')
+    sql_constraints = [
+        (
+            "template_link_uniq",
+            "unique (product_template_id, linked_product_template_id, link_type)",
+            "The products and the link type combination must be unique",
+        )
     ]
