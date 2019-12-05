26e102674f3e959575380c1eda0f254042928598
Nicolas JEUDY <njeudy@myceliandre.fr>
2019-12-05 22:37:55 +0100

    [REF] website_sale_firstname: Black python code

--- website_sale_firstname/__manifest__.py
+++ website_sale_firstname/__manifest__.py
@@ -7,18 +7,12 @@
             Allow to change name field in form to first name and last name
     """,
     "version": "11.0.1.0.0",
-    "author": "IT-Projects LLC, "
-              "Odoo Community Association (OCA)",
+    "author": "IT-Projects LLC, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "maintainer": "IT-Projects LLC",
     "category": "Extra Tools",
     "website": "https://github.com/OCA/e-commerce",
-    "depends": [
-        "website_sale",
-        "partner_firstname",
-    ],
-    "data": [
-        "data/data.xml",
-    ],
+    "depends": ["website_sale", "partner_firstname",],
+    "data": ["data/data.xml",],
     "installable": True,
 }
--- website_sale_firstname/controllers/main.py
+++ website_sale_firstname/controllers/main.py
@@ -6,12 +6,14 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleExtended(WebsiteSale):
     def _get_mandatory_billing_fields(self):
         return [
-            fname for fname in super()._get_mandatory_billing_fields()
-            if fname != 'name'
+            fname
+            for fname in super()._get_mandatory_billing_fields()
+            if fname != "name"
         ]
 
     def _get_mandatory_shipping_fields(self):
         return [
-            fname for fname in super()._get_mandatory_billing_fields()
-            if fname != 'name'
+            fname
+            for fname in super()._get_mandatory_billing_fields()
+            if fname != "name"
         ]
