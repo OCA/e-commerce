b310bc19984c59d3dab01dcd7f63df957872e417
Carles Antoli <carlesantoli@hotmail.com>
2020-01-06 18:07:40 +0000

    Added translation using Weblate (Catalan)

--- website_sale_suggest_create_account/__manifest__.py
+++ website_sale_suggest_create_account/__manifest__.py
@@ -1,21 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería, S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Suggest to create user account when buying",
     "summary": "Suggest users to create an account when buying in the website",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Website",
     "website": "https://www.tecnativa.com",
-    "author": "Tecnativa, "
-              "LasLabs, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "LasLabs, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "depends": [
-        "website_sale",
-    ],
-    "data": [
-        "views/website_sale.xml",
-    ],
+    "depends": ["website_sale"],
+    "data": ["views/website_sale.xml"],
 }
