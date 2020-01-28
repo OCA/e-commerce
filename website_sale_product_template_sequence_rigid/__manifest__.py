cae6a8f9f19d629b997bc4d70591773b62b701ec
Jordi Riera <jordi.riera@numigi.com>
2020-01-27 21:37:56 -0500

    [12.0][ADD] add website_sale_product_sequence_rigid

---
+++ website_sale_product_template_sequence_rigid/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2020 - Today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Website Sale Product Template Sequence Rigid',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Sale',
+    'summary': 'Make product template sequence harder to change',
+    'depends': [
+        "website_sale",
+    ],
+    'data': [
+        'product_template_views.xml',
+    ],
+    'installable': True,
+}
---
+++ website_sale_product_template_sequence_rigid/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module makes it harder to change the sequence of the product template.
+
+To do so, it remove the sequence handler from the tree view.
+
+Add sequence in the form view.
