300523883fe9747fe08158cb72be0833f76e7955
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-30 15:14:05 +0100

    [MIG] website_sale_checkout_skip_payment: Migration to 13.0

--- website_sale_checkout_skip_payment/models/website.py
+++ website_sale_checkout_skip_payment/models/website.py
@@ -1,16 +1,20 @@
 # Copyright 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.http import request
 
 
 class Website(models.Model):
     _inherit = "website"
 
+    website_sale_checkout_skip_message = fields.Text(
+        string="Website Sale SKip Message",
+        required=True,
+        default="Our team will check your order and send you payment information soon.",
+    )
     checkout_skip_payment = fields.Boolean(compute="_compute_checkout_skip_payment")
 
-    @api.multi
     def _compute_checkout_skip_payment(self):
         for rec in self:
             if request.session.uid:
---
+++ website_sale_checkout_skip_payment/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* Sergio Teruel <sergio.teruel@tecnativa.com>
+* David Vidal <david.vidal@tecnativa.com>
+* Martin Wilderoth <martin.wilderoth@linserv.se>
+* Alexandre Díaz <alexandre.diaz@tecnativa.com>
