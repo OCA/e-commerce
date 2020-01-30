30cff4f2e434efedd0fd2e592f3f8e5398e6ddf3
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-30 14:11:46 +0100

    [IMP] website_sale_checkout_skip_payment: black, isort, prettier

--- website_sale_checkout_skip_payment/controllers/main.py
+++ website_sale_checkout_skip_payment/controllers/main.py
@@ -4,33 +4,37 @@
 
 from odoo import http
 from odoo.http import request
+
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class CheckoutSkipPayment(WebsiteSale):
-
     @http.route()
     def payment_get_status(self, sale_order_id, **post):
         # When skip payment step, the transaction not exists so only render
         # the waiting message in ajax json call
         if not request.website.checkout_skip_payment:
             return super(CheckoutSkipPayment, self).payment_get_status(
-                sale_order_id, **post)
+                sale_order_id, **post
+            )
         return {
-            'recall': True,
-            'message': request.website._render(
-                'website_sale_checkout_skip_payment.order_state_message'),
+            "recall": True,
+            "message": request.website._render(
+                "website_sale_checkout_skip_payment.order_state_message"
+            ),
         }
 
     @http.route()
     def payment_confirmation(self, **post):
         if not request.website.checkout_skip_payment:
             return super().payment_confirmation(**post)
-        order = request.env['sale.order'].sudo().browse(
-            request.session.get('sale_last_order_id'))
+        order = (
+            request.env["sale.order"]
+            .sudo()
+            .browse(request.session.get("sale_last_order_id"))
+        )
         order.action_confirm()
         if not order.force_quotation_send():
-            return request.render(
-                'website_sale_skip_payment.confirmation_order_error')
+            return request.render("website_sale_skip_payment.confirmation_order_error")
         request.website.sale_reset()
-        return request.render("website_sale.confirmation", {'order': order})
+        return request.render("website_sale.confirmation", {"order": order})
