49e5b551a20b91bab19a90a6f1f8af0f35840262
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-30 20:01:11 +0100

    [IMP] website_sale_product_brand: black, isort

--- website_sale_product_brand/tests/test_ui.py
+++ website_sale_product_brand/tests/test_ui.py
@@ -4,13 +4,11 @@ from odoo.tests.common import HttpCase
 
 
 class UICase(HttpCase):
-
     def test_ui_website(self):
         """Test frontend tour."""
         tour = "website_sale_product_brand"
         self.phantom_js(
             url_path="/shop",
-            code="odoo.__DEBUG__.services['web_tour.tour']"
-                 ".run('%s')" % tour,
-            ready="odoo.__DEBUG__.services['web_tour.tour']"
-                  ".tours.%s.ready" % tour)
+            code="odoo.__DEBUG__.services['web_tour.tour']" ".run('%s')" % tour,
+            ready="odoo.__DEBUG__.services['web_tour.tour']" ".tours.%s.ready" % tour,
+        )
