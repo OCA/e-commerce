dadeb18598f614bcb6c9657e6cdd8b6b548d10b5
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-30 20:01:11 +0100

    [IMP] website_sale_product_brand: black, isort

--- website_sale_product_brand/__manifest__.py
+++ website_sale_product_brand/__manifest__.py
@@ -2,26 +2,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Brand Filtering in Website',
-    'category': 'e-commerce',
-    'author': "Serpent Consulting Services Pvt. Ltd., "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/e-commerce',
-    'version': '12.0.1.1.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'product_brand',
-        'website_sale'
-    ],
-    'data': [
+    "name": "Product Brand Filtering in Website",
+    "category": "e-commerce",
+    "author": "Serpent Consulting Services Pvt. Ltd., "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "version": "13.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["product_brand", "website_sale"],
+    "data": [
+        "security/ir.model.access.csv",
         "data/website_menu.xml",
         "views/product_brand.xml",
     ],
-    'demo': [
+    "demo": [
         "demo/assets.xml",
         "demo/product_brand_demo.xml",
         "demo/product_product_demo.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
