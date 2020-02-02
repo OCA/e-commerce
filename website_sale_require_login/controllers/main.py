7a0c32c3ea94b659f7fa31318d6eaac390f31f5f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-02 23:46:25 +0100

    [IMP] website_sale_require_login: black, isort

--- website_sale_require_login/controllers/main.py
+++ website_sale_require_login/controllers/main.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import http
+
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
--- website_sale_require_login/static/src/js/website_sale_tour_buy_login.tour.js
+++ website_sale_require_login/static/src/js/website_sale_tour_buy_login.tour.js
@@ -2,17 +2,16 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-odoo.define('website_sale_require_login.tour', function(require) {
+odoo.define('website_sale_require_login.tour', function (require) {
 
     "use strict";
 
     require("website_sale.tour");
     var tour = require("web_tour.tour");
-    var base = require("web_editor.base");
     var steps = tour.tours.shop_buy_product.steps;
 
     // Gets user name from login menu (see openerp.Tour.log)
-    function user() {
+    function user () {
         return $(".navbar .dropdown:has(>.js_usermenu) a:first, " +
                  ".navbar .oe_topbar_name, " +
                  ".pos .username").text();
@@ -21,14 +20,15 @@ odoo.define('website_sale_require_login.tour', function(require) {
     for (var position = 0; position < steps.length; position++) {
         if (steps[position].content === "go to checkout") {
             if (user()) {
-                // if user is authenticated -- add extra dummy step to don't break current_step counter
+                // If user is authenticated -- add extra dummy step
+                // to don't break current_step counter
                 steps.splice(
                     position,
                     0,
                     {
                         title: 'dummy',
                         auto: 1,
-                        trigger: "footer :contains(Copyright)"
+                        trigger: "footer :contains(Copyright)",
                     }
                 );
             } else {
@@ -44,12 +44,12 @@ odoo.define('website_sale_require_login.tour', function(require) {
                     {
                         title: "Go To Checkout",
                         extra_trigger: "#cart_products input.js_quantity[value=1]",
-                        trigger: "a span:contains('Log in and checkout')"
+                        trigger: "a span:contains('Log in and checkout')",
                     },
                     {
                         title: "Log In",
                         trigger: "#login",
-                        run: stepOnload
+                        run: stepOnload,
                     }
                 );
             }
