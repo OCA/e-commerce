222b7daabedaf1003c3491038e6f11456386ab45
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-18 21:41:27 +0100

    [IMP] website_sale_product_minimal_price: performance computation

--- website_sale_product_minimal_price/models/product_template.py
+++ website_sale_product_minimal_price/models/product_template.py
@@ -1,10 +1,29 @@
 # Copyright 2019 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import operator
 from odoo import fields, models, tools
+from odoo.tools import ormcache
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    # TODO: Use ormcache but we need invalidate cache in all models involved
+    #  in a price change
+    @ormcache('self.id', 'pricelist_id', 'qty', 'partner')
+    def _get_variants_price(self, pricelist_id, qty, partner):
+        pricelist = self.env['product.pricelist'].browse(pricelist_id)
+        variants_nbr = len(self.product_variant_ids)
+        quantities = [qty] * variants_nbr
+        partners = [partner] * variants_nbr
+        prices = pricelist.get_products_price(self.product_variant_ids,
+                                              quantities, partners)
+        return prices
+
+    def _get_minimal_price(self, pricelist_id, qty, partner):
+        prices = self._get_variants_price(pricelist_id, qty, partner)
+        minimal_price = sorted(prices.items(), key=operator.itemgetter(1))[:1]
+        return minimal_price
 
     def _get_combination_info(
         self, combination=False, product_id=False, add_qty=1, pricelist=False,
@@ -90,6 +109,16 @@ class ProductTemplate(models.Model):
             })
         return combination_info
 
+    # TODO: Active cache
+    @ormcache('self.id', 'product_attribute_values')
+    def _get_cheaper_combination(self, product_attribute_values):
+        ptav_obj = self.env['product.template.attribute.value']
+        cheaper_combination = ptav_obj.search([
+            ('product_tmpl_id', '=', self.id),
+            ('product_attribute_value_id', 'in', product_attribute_values),
+        ])
+        return cheaper_combination.ids
+
     def _get_first_possible_combination(
             self, parent_combination=None, necessary_values=None):
         """
@@ -104,12 +133,15 @@ class ProductTemplate(models.Model):
         if (self.env.context.get('website_id') and
                 self.product_variant_count > 1):
             ptav_obj = self.env['product.template.attribute.value']
-            pav = self.product_variant_ids.sorted(
-                'website_price')[:1].attribute_value_ids
-            cheaper_combination = ptav_obj.search([
-                ('product_tmpl_id', '=', self.id),
-                ('product_attribute_value_id', 'in', pav.ids),
-            ])
+            partner = self.env.context.get('partner')
+            pricelist_id = self.env.context.get('pricelist')
+            minimal_price = self._get_minimal_price(
+                pricelist_id, 1, partner)
+            cheaper_variant_id = minimal_price[0][0]
+            pav = self.env['product.product'].browse(
+                cheaper_variant_id).attribute_value_ids
+            cheaper_combination_ids = self._get_cheaper_combination(pav.ids)
+            cheaper_combination = ptav_obj.browse(cheaper_combination_ids)
             variant_combination = combination.filtered(
                 lambda x: x.attribute_id.create_variant == 'always')
             combination_returned = cheaper_combination + (
