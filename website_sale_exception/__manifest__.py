292521a94894c3527936131a89bdb56c4971ffd5
Katherine Zaoral <kz@adhoc.com.ar>
2020-04-28 17:21:54 -0300

    [ADD] website_sale_exception: new module
    [UPD] Update website_sale_exception.pot
    [UPD] README.rst
    [ADD] icon.png

---
+++ website_sale_exception/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 ADHOC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Website Sale Exception',
+    'summary': 'Support for sale exceptions on website',
+    'version': '12.0.1.0.0',
+    'category': 'Generic Modules/Sale',
+    'author': "ADHOC, Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/e-commerce',
+    'depends': ['website_sale', 'sale_exception'],
+    'license': 'AGPL-3',
+}
