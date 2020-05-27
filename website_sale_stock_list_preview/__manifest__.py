e6deeb394eb074c54ef1d1c4903b2720758edb2a
Carlos Roca <carlos.roca@tecnativa.com>
2020-05-27 13:14:55 +0200

    [IMP] website_sale_stock_list_preview: hide the add to cart button when no stock available

--- website_sale_stock_list_preview/__manifest__.py
+++ website_sale_stock_list_preview/__manifest__.py
@@ -5,12 +5,12 @@
 {
     "name": "Website Sale Stock List Preview",
     "summary": "Show the stock of products on the product previews",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce/",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["website_sale_stock"],
-    "data": ["views/assets.xml"],
+    "data": ["views/assets.xml", "views/templates.xml"],
 }
