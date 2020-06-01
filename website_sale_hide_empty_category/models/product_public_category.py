70f56d2506c57aa0da0c83c2d9d5b0726782500f
Carlos Roca <carlos.roca@tecnativa.com>
2020-06-01 11:48:22 +0200

    [IMP] : black, isort, prettier

--- website_sale_hide_empty_category/models/product_public_category.py
+++ website_sale_hide_empty_category/models/product_public_category.py
@@ -5,14 +5,14 @@ from odoo import fields, models
 
 
 class ProductPublicCategory(models.Model):
-    _inherit = 'product.public.category'
+    _inherit = "product.public.category"
 
     # This module relies on the M2M relation built from `product.template` to
     # `product.public.category` named `public_categ_ids`
     product_ids = fields.Many2many(
-        comodel_name='product.template',
-        string='Products',
-        relation='product_public_category_product_template_rel',
-        column1='product_public_category_id',
-        column2='product_template_id',
+        comodel_name="product.template",
+        string="Products",
+        relation="product_public_category_product_template_rel",
+        column1="product_public_category_id",
+        column2="product_template_id",
     )
