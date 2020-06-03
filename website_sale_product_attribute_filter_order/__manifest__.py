362857938d9468555ab461321e6890ab7280dd61
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-06-03 02:06:39 +0200

    website_sale_attribute_filter_order

---
+++ website_sale_product_attribute_filter_order/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Tecnativa - Alexandre D. Díaz
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Sale Attribute Filter Order",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "license": "LGPL-3",
+    "summary": "Move active checkbox options to the first place of the list",
+    "depends": ["website_sale"],
+    "data": ["templates/website_sale.xml"],
+    "installable": True,
+    "maintainers": ["Tardo"],
+}
