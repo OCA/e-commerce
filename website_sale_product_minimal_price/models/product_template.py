fe1990888405c7872c483818015345a9b3aace25
Carlos Roca <carlos.roca@tecnativa.com>
2020-06-04 10:01:58 +0200

    [IMP] website_sale_product_minimal_price: black, isort, prettier

--- website_sale_product_minimal_price/models/product_template.py
+++ website_sale_product_minimal_price/models/product_template.py
@@ -4,71 +4,90 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     has_distinct_variant_price = fields.Boolean(
-        compute='_compute_has_distinct_variant_price',
-        string='Has variants with distinct price extra',
+        compute="_compute_has_distinct_variant_price",
+        string="Has variants with distinct price extra",
     )
 
     def _compute_has_distinct_variant_price(self):
         for template in self:
             if template.product_variant_count > 1:
-                prices = template.product_variant_ids.mapped('website_price')
+                prices = template.product_variant_ids.mapped("website_price")
                 if len(prices) > 1:
                     template.has_distinct_variant_price = True
 
     def _get_combination_info(
-        self, combination=False, product_id=False, add_qty=1, pricelist=False,
-            parent_combination=False, only_template=False):
+        self,
+        combination=False,
+        product_id=False,
+        add_qty=1,
+        pricelist=False,
+        parent_combination=False,
+        only_template=False,
+    ):
         """
         Update product template prices for products items view in website shop
         render with cheaper variant prices.
         """
         combination_info = super()._get_combination_info(
-            combination=combination, product_id=product_id, add_qty=add_qty,
-            pricelist=pricelist, parent_combination=parent_combination,
-            only_template=only_template)
+            combination=combination,
+            product_id=product_id,
+            add_qty=add_qty,
+            pricelist=pricelist,
+            parent_combination=parent_combination,
+            only_template=only_template,
+        )
 
-        if (only_template and self.env.context.get('website_id') and
-                self.product_variant_count > 1):
+        if (
+            only_template
+            and self.env.context.get("website_id")
+            and self.product_variant_count > 1
+        ):
             cheaper_variant = self.product_variant_ids.sorted(
-                key=lambda p: p.website_price)[:1]
+                key=lambda p: p.website_price
+            )[:1]
 
             res = cheaper_variant._get_combination_info_variant()
 
-            combination_info.update({
-                'price': res.get('price'),
-                'list_price': res.get('list_price'),
-                'has_discounted_price': res.get('has_discounted_price'),
-            })
+            combination_info.update(
+                {
+                    "price": res.get("price"),
+                    "list_price": res.get("list_price"),
+                    "has_discounted_price": res.get("has_discounted_price"),
+                }
+            )
         return combination_info
 
     def _get_first_possible_combination(
-            self, parent_combination=None, necessary_values=None):
+        self, parent_combination=None, necessary_values=None
+    ):
         """
         Get the cheaper product combination for the product for website view.
         We only take into account attributes that generate variants and
         products with more than one variant.
         """
         combination = super()._get_first_possible_combination(
-            parent_combination=parent_combination,
-            necessary_values=necessary_values
+            parent_combination=parent_combination, necessary_values=necessary_values
         )
-        if (self.env.context.get('website_id') and
-                self.product_variant_count > 1):
-            ptav_obj = self.env['product.template.attribute.value']
-            pav = self.product_variant_ids.sorted(
-                'website_price')[:1].attribute_value_ids
-            cheaper_combination = ptav_obj.search([
-                ('product_tmpl_id', '=', self.id),
-                ('product_attribute_value_id', 'in', pav.ids),
-            ])
+        if self.env.context.get("website_id") and self.product_variant_count > 1:
+            ptav_obj = self.env["product.template.attribute.value"]
+            pav = self.product_variant_ids.sorted("website_price")[
+                :1
+            ].attribute_value_ids
+            cheaper_combination = ptav_obj.search(
+                [
+                    ("product_tmpl_id", "=", self.id),
+                    ("product_attribute_value_id", "in", pav.ids),
+                ]
+            )
             variant_combination = combination.filtered(
-                lambda x: x.attribute_id.create_variant == 'always')
+                lambda x: x.attribute_id.create_variant == "always"
+            )
             combination_returned = cheaper_combination + (
-                combination - variant_combination)
+                combination - variant_combination
+            )
             # Keep order to avoid This combination does not exist message
-            return combination_returned.sorted(
-                lambda x: x.attribute_id.sequence)
+            return combination_returned.sorted(lambda x: x.attribute_id.sequence)
         return combination
