bba9d8661f4e95beefcd5491ee1c68dc9fd3d820
Carlos Roca <carlos.roca@tecnativa.com>
2020-06-04 10:01:58 +0200

    [IMP] website_sale_product_minimal_price: black, isort, prettier

--- website_sale_product_minimal_price/static/src/js/website_sale_product_minimal_price_tour.js
+++ website_sale_product_minimal_price/static/src/js/website_sale_product_minimal_price_tour.js
@@ -1,7 +1,7 @@
 /* Copyright 2019 Sergio Teruel
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_product_minimal_price.tour", function (require) {
+odoo.define("website_sale_product_minimal_price.tour", function(require) {
     "use strict";
 
     var tour = require("web_tour.tour");
@@ -24,7 +24,8 @@ odoo.define("website_sale_product_minimal_price.tour", function (require) {
             extra_trigger: ".product_price:has(span:contains('125.00'))",
         },
     ];
-    tour.register("website_sale_product_minimal_price",
+    tour.register(
+        "website_sale_product_minimal_price",
         {
             url: "/shop",
             test: true,
