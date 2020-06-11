fcca6bd450e6c98b5458ef1d32ae961f4565ff19
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-06-11 09:35:31 +0100

    [ADD] website_sale_product_attachment: add attachments to products in website
    This module lets you publish downloadable attachments in a product page.
    
    This is useful if you want to publish firmwares, manuals, specs, warranties,
    or whatever document related to the product.
    
    @Tecnativa TT20984 TT23657

---
+++ website_sale_product_attachment/controllers/main.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Tecnativa - Jairo Llopis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo.http import request, route
+from odoo.addons.website_sale.controllers import main
+
+
+class WebsiteSale(main.WebsiteSale):
+    @route()
+    def product(self, *args, **kwargs):
+        """Add filtered product downloads to render context."""
+        result = super().product(*args, **kwargs)
+        # Doing product.website_attachment_ids does not respect the domain
+        # defined in the model; here we enforce that domain to make sure
+        # we only render current website's attachments, and that there's no
+        # difference between what the user sees in backend and what the visitor
+        # sees in the frontend
+        attachments_domain = request.env["product.template"].fields_get(
+            ["website_attachment_ids"], ["domain"]
+        )["website_attachment_ids"]["domain"]
+        attachments = request.env["ir.attachment"].search(
+            [("id", "in", result.qcontext["product"].website_attachment_ids.ids)]
+            + attachments_domain
+        )
+        result.qcontext["product_attachments"] = attachments
+        return result
