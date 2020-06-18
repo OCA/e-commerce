43b84aceb018a387ba49300135b5dc50092710ba
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-06-18 18:25:46 +0200

    [ADD] website_sale_porduct_detatul_attribute_value_image

---
+++ website_sale_product_detail_attribute_value_image/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* `Tecnativa <https://www.tecnativa.com>`_:
+
+    * Alexandre D. Díaz
+    * Sergio Teruel
---
+++ website_sale_product_detail_attribute_value_image/tests/__init__.py
@@ -0,0 +1,2 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import test_website_sale_product_detail_attribute_value_image
