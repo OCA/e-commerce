878be6b76c98733fb4a67a6a8b918db8a32a9d9f
Chafique <chafique.delli@akretion.com>
2020-06-22 21:41:18 +0200

    [12.0][MIG] website_sale_affiliate module

--- website_sale_affiliate/controllers/__init__.py
+++ website_sale_affiliate/controllers/__init__.py
@@ -1,4 +1,1 @@
-# Copyright 2017 LasLabs Inc.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
-
 from . import main
--- website_sale_affiliate/models/sale_affiliate.py
+++ website_sale_affiliate/models/sale_affiliate.py
@@ -1,5 +1,5 @@
 # Copyright 2017 LasLabs Inc.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
 
