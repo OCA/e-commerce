c98cbf0780059efc0275171ab7bf30cb81434c2b
david <david.vidal@tecnativa.com>
2020-06-25 16:08:37 +0200

    [MIG] website_sale_product_reference_displayed: Migration to 12.0

--- website_sale_product_reference_displayed/__manifest__.py
+++ website_sale_product_reference_displayed/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Display product reference in e-commerce",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, "
