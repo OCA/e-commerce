bd6d89555369499f5c2f17800d1552b141b48d22
Chafique <chafique.delli@akretion.com>
2020-07-02 12:59:24 +0200

    [12.0][ADD] website_sale_affiliate_product_restriction module

---
+++ website_sale_affiliate_product_restriction/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Commown SCIC SAS (https://commown.fr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website sale affiliate product restriction',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'author': "Commown SCIC SAS, Akretion, Odoo Community Association (OCA)",
+    'license': "AGPL-3",
+    'website': "https://github.com/OCA/e-commerce",
+    'depends': [
+        'website_sale_affiliate',
+    ],
+    'data': [
+        'views/sale_affiliate.xml',
+    ],
+    'installable': True,
+}
