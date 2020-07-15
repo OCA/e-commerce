2a0b08fb7670e2e01e9e6cba9b1e4206d5f3529a
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-07-15 16:54:23 +0200

    [NEW] website_sale_tax_toggle: Test more resilient to pricelists

--- website_sale_tax_toggle/tests/test_website_sale_tax_toggle.py
+++ website_sale_tax_toggle/tests/test_website_sale_tax_toggle.py
@@ -1,23 +1,34 @@
 # Copyright 2020 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests.common import HttpCase
+from odoo.tests.common import HttpCase, tagged
 
 
+@tagged('post_install', '-at_install')
 class WebsiteSaleTaxesToggleHttpCase(HttpCase):
 
     def setUp(self):
         super().setUp()
-        # Customizable desk
-        self.product_template = self.env.ref(
-            'product.product_product_4_product_template')
         self.tax = self.env['account.tax'].create({
             'name': 'Taxes tobble test tax',
             'amount_type': 'percent',
             'amount': 15,
             'type_tax_use': 'sale',
         })
-        self.product_template.list_price = 750.00
-        self.product_template.taxes_id = [(6, 0, self.tax.ids)]
+        self.product_template = self.env['product.template'].create({
+            'name': 'Product test tax toggle',
+            'list_price': 750.00,
+            'taxes_id': [(6, 0, self.tax.ids)],
+            'website_published': True,
+            'website_sequence': 9999,
+        })
+        pricelist = self.env['product.pricelist'].create({
+            'name': 'Price list for tests'
+        })
+        self.env.user.partner_id.property_product_pricelist = pricelist
+        # To avoid currency converter
+        self.env['res.currency.rate'].search([]).write({
+            'rate': 1,
+        })
 
     def test_ui_website(self):
         """Test frontend tour."""
