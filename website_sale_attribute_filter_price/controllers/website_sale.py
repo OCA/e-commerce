9864f54e0a6d79b189e8df513423ec42a0c96253
Manuel Marquez <buzondemam@gmail.com>
2020-07-15 02:34:43 -0400

    [IMP] website_sale_attribute_filter_price: black, isort, prettier

--- website_sale_attribute_filter_price/controllers/website_sale.py
+++ website_sale_attribute_filter_price/controllers/website_sale.py
@@ -3,8 +3,8 @@
 from odoo import http
 from odoo.http import request
 
-from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.website.controllers.main import QueryURL
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class WebsiteSale(WebsiteSale):
@@ -14,20 +14,16 @@ class WebsiteSale(WebsiteSale):
         if price_vals:
             to_add = []
             if price_vals[0] is not None:
-                to_add += [
-                    ("list_price", ">=", price_vals[0])
-                ]
+                to_add += [("list_price", ">=", price_vals[0])]
             if price_vals[1] is not None:
-                to_add += [
-                    ("list_price", "<=", price_vals[1])
-                ]
+                to_add += [("list_price", "<=", price_vals[1])]
             if len(to_add) == 2:
-                to_add.insert(0, '&')
+                to_add.insert(0, "&")
             domain += to_add
         return domain
 
     @http.route()
-    def shop(self, page=0, category=None, search='', ppg=False, **post):
+    def shop(self, page=0, category=None, search="", ppg=False, **post):
         # User values
         try:
             custom_min_price = float(post.get("min_price"))
@@ -43,8 +39,8 @@ class WebsiteSale(WebsiteSale):
             if custom_min_price > custom_max_price:
                 custom_max_price, custom_min_price = custom_min_price, custom_max_price
         request.context = dict(
-            request.context,
-            price_vals=[custom_min_price, custom_max_price])
+            request.context, price_vals=[custom_min_price, custom_max_price]
+        )
         response = super().shop(
             page=page, category=category, search=search, ppg=ppg, **post
         )
@@ -55,29 +51,36 @@ class WebsiteSale(WebsiteSale):
             request.context,
             pricelist=pricelist.id,
             partner=request.env.user.partner_id,
-            price_vals=None)
-        product_id = request.env['product.template'].with_context(
-            prefetch_fields=False
-        ).search(
-            self._get_search_domain(
-                search,
-                category,
-                response.qcontext.get('attrib_values')),
-            order='list_price DESC', limit=1)
+            price_vals=None,
+        )
+        product_id = (
+            request.env["product.template"]
+            .with_context(prefetch_fields=False)
+            .search(
+                self._get_search_domain(
+                    search, category, response.qcontext.get("attrib_values")
+                ),
+                order="list_price DESC",
+                limit=1,
+            )
+        )
         max_price = product_id.list_price
         # Price Filter QWeb Values
         keep = QueryURL(
-            '/shop',
+            "/shop",
             category=category and int(category),
-            search=post.get('search'),
-            attrib=post.get('atrib'),
-            order=post.get('order'),
+            search=post.get("search"),
+            attrib=post.get("atrib"),
+            order=post.get("order"),
             min_price=custom_min_price,
-            max_price=custom_max_price)
-        response.qcontext.update({
-            "custom_min_price": custom_min_price,
-            "custom_max_price": custom_max_price,
-            "max_price": max_price,
-            "keep": keep,
-        })
+            max_price=custom_max_price,
+        )
+        response.qcontext.update(
+            {
+                "custom_min_price": custom_min_price,
+                "custom_max_price": custom_max_price,
+                "max_price": max_price,
+                "keep": keep,
+            }
+        )
         return response
