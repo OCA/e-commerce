8569839ce519774fbf28c7e33da12762d12e7ac7
Manuel Marquez <buzondemam@gmail.com>
2020-07-15 02:34:43 -0400

    [IMP] website_sale_attribute_filter_price: black, isort, prettier

--- website_sale_attribute_filter_price/static/src/js/shop.js
+++ website_sale_attribute_filter_price/static/src/js/shop.js
@@ -1,17 +1,17 @@
 // Copyright 2020 Tecnativa - Alexandre Díaz
 // License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-odoo.define("website_sale_attribute_filter_price.Shop", function (require) {
+odoo.define("website_sale_attribute_filter_price.Shop", function(require) {
     "use strict";
 
     require("web.dom_ready");
 
-    $(function () {
+    $(function() {
         // Price Filter
         var $price_slider = $("#filter_price_slider");
         var $min_value_input = $("#price_range_min_value");
         var $max_value_input = $("#price_range_max_value");
         var $clean_btn = $("#clear_price_filter");
-        $price_slider.on("change", function (ev) {
+        $price_slider.on("change", function(ev) {
             ev.stopPropagation();
         });
         $price_slider.ionRangeSlider({
@@ -26,28 +26,28 @@ odoo.define("website_sale_attribute_filter_price.Shop", function (require) {
             step: 1,
             grid: false,
 
-            onChange: function (data) {
+            onChange: function(data) {
                 $min_value_input.val(data.from);
                 $max_value_input.val(data.to);
             },
         });
-        $min_value_input.on("change", function (ev) {
+        $min_value_input.on("change", function(ev) {
             ev.stopPropagation();
             var ionRange = $price_slider.data("ionRangeSlider");
             ionRange.update({from: $(this).val() || 0});
         });
-        $max_value_input.on("change", function (ev) {
+        $max_value_input.on("change", function(ev) {
             ev.stopPropagation();
             var ionRange = $price_slider.data("ionRangeSlider");
             ionRange.update({
-                to: $(this).val() || $price_slider.data("max_price")
+                to: $(this).val() || $price_slider.data("max_price"),
             });
         });
 
-        $clean_btn.on("click", function () {
-            $min_value_input.val('').trigger('change');
-            $max_value_input.val('').trigger('change');
-            $price_slider.closest('form').submit();
+        $clean_btn.on("click", function() {
+            $min_value_input.val("").trigger("change");
+            $max_value_input.val("").trigger("change");
+            $price_slider.closest("form").submit();
         });
     });
 });
