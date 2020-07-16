614c600ee0041d09bdef838abd101e49bf26b0e0
Angel Parra <aparra@aitic.com.ar>
2020-07-16 02:13:08 -0500

    [UPD] Module website_sale_charge_payment_fee: Version

--- website_sale_charge_payment_fee/__manifest__.py
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "eCommerce: charge payment fee",
     "summary": "Payment fee charged to customer",
-    "version": "11.0.1.0.1",
+    "version": "11.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
