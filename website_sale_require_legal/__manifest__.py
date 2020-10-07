6e8937d72d0256ffadbcaccdb4617b4e2d716a34
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-10-07 09:27:11 -0400

    [MIG] website_sale_require_legal: Migration to 13.0

--- website_sale_require_legal/__manifest__.py
+++ website_sale_require_legal/__manifest__.py
@@ -5,14 +5,13 @@
 {
     "name": "Require accepting legal terms to checkout",
     "summary": "Force the user to accept legal tems to buy in the web shop",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["web_tour", "website_legal_page", "website_sale",],
-    "data": ["views/website_sale.xml",],
-    "demo": ["demo/assets.xml",],
+    "depends": ["website_legal_page", "website_sale"],
+    "data": ["views/website_sale.xml", "views/assets.xml"],
 }
--- website_sale_require_legal/readme/CONTRIBUTORS.rst
+++ website_sale_require_legal/readme/CONTRIBUTORS.rst
@@ -1,8 +1,8 @@
 * `Tecnativa <https://www.tecnativa.com>`_:
 
-    * Rafael Blasco <rafael.blasco@tecnativa.com>
-    * Jairo Llopis <jairo.llopis@tecnativa.com>
-    * Vicent Cubells <vicent.cubells@tecnativa.com>
-    * David Vidal <david.vidal@tecnativa.com>
-    * Ernesto Tejeda <ernesto.tejeda87@gmail.com>
+    * Rafael Blasco
+    * Jairo Llopis
+    * Vicent Cubells
+    * David Vidal
+    * Ernesto Tejeda
     * Cristina Martin R.
