edca2962fecd5c079e47cbd17bce20686011291c
tslai <tl@quartile.co>
2020-10-20 09:55:38 +0800

    [IMP] Adjust coding style

--- website_sale_charge_payment_fee/__manifest__.py
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -1,24 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Lorenzo Battistini - Agile Business Group
 # Copyright 2020 AITIC S.A.S
+# Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
 {
     "name": "eCommerce: charge payment fee",
     "summary": "Payment fee charged to customer",
     "version": "12.0.1.0.1",
-    "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Agile Business Group, AITIC S.A.S, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "website_sale",
-    ],
-    "data": [
-        "views/payment_view.xml",
-        "templates/website_sale.xml"
-    ],
+    "depends": ["website_sale"],
+    "data": ["views/payment_acquirer_views.xml", "views/website_sale_templates.xml"],
 }
