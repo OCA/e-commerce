c32d5a777bdb4f4c69bdbafaf27e4c1fa2853db8
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-10-21 10:41:09 +0200

    [FIX] website_sale_category_description: development_status

--- website_sale_category_description/__manifest__.py
+++ website_sale_category_description/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Category Descriptions",
     "summary": "Display a description for each eCommerce category",
     "version": "12.0.1.0.2",
-    "development_status": "Stable",
+    "development_status": "Production/Stable",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, Odoo Community Association (OCA)",
