51063dd9a51eaeffd5babe193ff65ca6e4acf2a5
João Marques <joao.marques@tecnativa.com>
2020-10-26 08:05:04 +0000

    [MIG] website_sale_order_type: Migration to 11.0

--- website_sale_order_type/__manifest__.py
+++ website_sale_order_type/__manifest__.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Simone Rubino - Agile Business Group
+# Copyright 2020 Tecnativa - João Marques
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Website sale order type",
     "summary": "This module allows sale_order_type to work with website_sale.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce/tree/"
