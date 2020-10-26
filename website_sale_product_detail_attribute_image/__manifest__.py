0ec8dc70dbd3e5cf3984074f5e8f2d7ddd6aaf27
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-10-26 09:13:07 +0100

    [FIX] website_sale_product_detail_attribute_image: Maturity level

--- website_sale_product_detail_attribute_image/__manifest__.py
+++ website_sale_product_detail_attribute_image/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Website Sale Product Detail Attribute Image",
     "summary": "Display attributes images in shop product detail",
     "version": "13.0.1.1.0",
-    "development_status": "Stable",
+    "development_status": "Production/Stable",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, Odoo Community Association (OCA)",
