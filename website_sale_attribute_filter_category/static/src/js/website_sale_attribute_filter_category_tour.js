01c414bb7ae1310cc3e6972b45a690909c45903b
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-10-28 13:07:05 -0300

    [IMP] black, isort, prettier : pre-commit run -a

--- website_sale_attribute_filter_category/static/src/js/website_sale_attribute_filter_category_tour.js
+++ website_sale_attribute_filter_category/static/src/js/website_sale_attribute_filter_category_tour.js
@@ -1,7 +1,7 @@
 /* Copyright 2019 Sergio Teruel
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_attribute_filter_category.tour", function(require) {
+odoo.define("website_sale_attribute_filter_category.tour", function (require) {
     "use strict";
 
     var tour = require("web_tour.tour");
