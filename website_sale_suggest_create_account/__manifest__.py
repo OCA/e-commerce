5747769a0822d7ba0b943312a321231744e83770
dsolanki <dhara.solanki@initos.com>
2020-11-11 11:38:21 +0530

    [IMP] website_sale_suggest_create_account: black, isort, prettier

--- website_sale_suggest_create_account/__manifest__.py
+++ website_sale_suggest_create_account/__manifest__.py
@@ -3,9 +3,9 @@
 {
     "name": "Suggest to create user account when buying",
     "summary": "Suggest users to create an account when buying in the website",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Website",
-    "website": "https://github.com/OCA/e-commerce/",
+    "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, " "LasLabs, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
