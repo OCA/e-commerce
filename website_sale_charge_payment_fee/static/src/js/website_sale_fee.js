22836f117e1803c9149c96294279f7cff0813ae3
tslai <tl@quartile.co>
2020-11-11 21:12:09 +0800

    [IMP] Adjust Pay Now button javascript, remove tax

--- website_sale_charge_payment_fee/static/src/js/website_sale_fee.js
+++ website_sale_charge_payment_fee/static/src/js/website_sale_fee.js
@@ -6,15 +6,16 @@ odoo.define("website_sale_change_payment_fee.website_sale_fee", function(require
     require("web.core");
 
     $(".o_payment_form").each(function() {
-        var payment_form = this;
         var selected_acquirer = $("input[name='selected_acquirer_id']").val();
 
-        $(payment_form).on("click", 'input[name="pm_id"]', function() {
+        $(this).on("click", 'input[name="pm_id"]', function() {
             var clicked_acquirer = $(this)
                 .prop("value")
-                .substring(5);
+                .substring(5);  // Represents the form id.
             if (selected_acquirer !== clicked_acquirer) {
-                $("#o_payment_form_pay").addClass("disabled");
+                // Follow standard logic disabling the "Pay Now" button
+                // https://github.com/odoo/odoo/blob/c2cce50f75096fcc9f940d16bb0ecf40a2899d60/addons/website_sale_delivery/static/src/js/website_sale_delivery.js#L56
+                $("#o_payment_form_pay").prop('disabled', true);
                 window.location.href =
                     "/shop/payment?payment_fee_id=" + clicked_acquirer;
             }
