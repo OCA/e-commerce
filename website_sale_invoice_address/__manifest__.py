4c58a61ca1dd75700531e7d8afc40c5674de9970
david <david.vidal@tecnativa.com>
2020-11-12 18:21:02 +0100

    [ADD] website_sale_invoice_address: New module

---
+++ website_sale_invoice_address/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Invoice Address",
+    "summary": "Set e-Commerce sale orders invoice address as in backend",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/assets.xml",
+    ]
+}
