513f312142c34bdcc2e369b0f941bdb8f1c1e827
david <david.vidal@tecnativa.com>
2020-12-10 13:05:35 +0100

    [FIX] website_sale_stock_available: prevent payment blocking
    When the `virtual_available` is less than the `immediately_usable_qty`,
    we'll have a blocking situation when the order is paid if we could add
    more quantity to the order than the virtual one.

---
+++ website_sale_stock_available/controllers/main.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.addons.website_sale_stock.controllers.main import WebsiteSale
+from odoo.http import request, route
+
+
+class WebsiteSale(WebsiteSale):
+
+    @route()
+    def payment_transaction(self, *args, **kwargs):
+        """Inject a context when potencial or promised stock is set"""
+        request.website = request.website.with_context(
+            website_sale_stock_available=True)
+        return super().payment_transaction(*args, **kwargs)
