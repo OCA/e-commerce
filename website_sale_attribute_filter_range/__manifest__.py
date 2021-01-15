15ded87225ad34e6923f80092887bc22d7d6543c
miguels73 <55379877+miguels73@users.noreply.github.com>
2021-01-15 18:50:29 +0100

    [ADD] website_sale_attribute_filter_range

---
+++ website_sale_attribute_filter_range/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Studio73 - Miguel Gandia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Sale Attribute Filter Range",
+    "category": "Website",
+    "summary": "Add range display type for product and new range filter for it",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["website_sale_attribute_filter_price"],
+    "data": [
+        "templates/assets.xml",
+        "templates/website_sale.xml",
+        "templates/sale.xml",
+    ],
+    "author": "Studio73, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "installable": True,
+    "maintainers": ["Studio73"],
+}
