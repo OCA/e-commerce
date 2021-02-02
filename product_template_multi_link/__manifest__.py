3384c2520a9afe82b5f1a6732be83ac685cc9bb3
Chafique <chafique.delli@akretion.com>
2021-02-02 12:07:46 +0100

    [MIG] product_template_multi_link: Migration to 14.0

--- product_template_multi_link/__manifest__.py
+++ product_template_multi_link/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Multi Links (Template)",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules",
     "author": "GRAP, ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
