b4faf8d057c62e8c601591584f3d06c0cfe92173
Chafique <chafique.delli@akretion.com>
2021-02-02 18:59:30 +0100

    [MIG] product_variant_multi_link: Migration to 14.0

--- product_variant_multi_link/__manifest__.py
+++ product_variant_multi_link/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Multi Links (Variant)",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
