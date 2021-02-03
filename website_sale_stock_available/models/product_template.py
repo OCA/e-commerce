19fd1f75880d7fcc02f434ee31d38b526e5992c8
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-02-03 17:15:44 -0300

    [IMP] pre-commit run

--- website_sale_stock_available/models/product_template.py
+++ website_sale_stock_available/models/product_template.py
@@ -1,16 +1,27 @@
 # Copyright 2020 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    def _get_combination_info(self, combination=False, product_id=False,
-                              add_qty=1, pricelist=False,
-                              parent_combination=False, only_template=False):
+    def _get_combination_info(
+        self,
+        combination=False,
+        product_id=False,
+        add_qty=1,
+        pricelist=False,
+        parent_combination=False,
+        only_template=False,
+    ):
         template = self.with_context(website_sale_stock_available=True)
         return super(ProductTemplate, template)._get_combination_info(
-            combination, product_id, add_qty, pricelist, parent_combination,
-            only_template)
+            combination,
+            product_id,
+            add_qty,
+            pricelist,
+            parent_combination,
+            only_template,
+        )
