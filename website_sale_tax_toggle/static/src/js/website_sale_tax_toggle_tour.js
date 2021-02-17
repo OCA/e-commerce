ec414be42a393bb164f217ac99395f94cd917ac3
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-17 12:04:28 +0100

    [IMP] website_sale_tax_toggle: black, isort

--- website_sale_tax_toggle/static/src/js/website_sale_tax_toggle_tour.js
+++ website_sale_tax_toggle/static/src/js/website_sale_tax_toggle_tour.js
@@ -1,7 +1,7 @@
 /* Copyright 2020 Sergio Teruel
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_tax_toggle.tour", function (require) {
+odoo.define("website_sale_tax_toggle.tour", function(require) {
     "use strict";
 
     var tour = require("web_tour.tour");
@@ -13,24 +13,29 @@ odoo.define("website_sale_tax_toggle.tour", function (require) {
     var steps = [
         {
             content: "Toggle tax button click from list page",
-            trigger: '.js_tax_toggle_btn',
-            extra_trigger: ".oe_product_cart:contains('Product test tax toggle') b .oe_currency_value:containsExact('750.00')",
+            trigger: ".js_tax_toggle_btn",
+            extra_trigger:
+                ".oe_product_cart:contains('Product test tax toggle') b .oe_currency_value:containsExact('750.00')",
         },
         {
             content: "Enter the product page",
-            trigger: ".oe_product_cart:has(b .oe_currency_value:containsExact('862.50')) a:contains('Product test tax toggle')",
+            trigger:
+                ".oe_product_cart:has(b .oe_currency_value:containsExact('862.50')) a:contains('Product test tax toggle')",
         },
         {
             content: "Toggle tax button click from product page",
-            trigger: '.js_tax_toggle_btn',
-            extra_trigger: "#product_details .oe_price .oe_currency_value:containsExact('862.50')",
+            trigger: ".js_tax_toggle_btn",
+            extra_trigger:
+                "#product_details .oe_price .oe_currency_value:containsExact('862.50')",
         },
         {
             content: "Check the product price is back to what it should",
-            trigger: "#product_details .oe_price .oe_currency_value:containsExact('750.00')",
+            trigger:
+                "#product_details .oe_price .oe_currency_value:containsExact('750.00')",
         },
     ];
-    tour.register("website_sale_tax_toggle",
+    tour.register(
+        "website_sale_tax_toggle",
         {
             url: "/shop",
             test: true,
