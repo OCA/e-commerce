ce5e1c1e3d4d934114e03b75352fa24f7375a01d
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-17 12:04:28 +0100

    [IMP] website_sale_tax_toggle: black, isort

--- website_sale_tax_toggle/models/res_users.py
+++ website_sale_tax_toggle/models/res_users.py
@@ -10,23 +10,25 @@ class ResUsers(models.Model):
 
     @api.model
     def has_group(self, group_ext_id):
-        tax_included = 'account.group_show_line_subtotals_tax_included'
-        tax_excluded = 'account.group_show_line_subtotals_tax_excluded'
-        if (self.env.context.get('website_id') and
-                group_ext_id in [tax_included, tax_excluded] and
-                not self.env.context.get('skip_tax_toggle_check')):
+        tax_included = "account.group_show_line_subtotals_tax_included"
+        tax_excluded = "account.group_show_line_subtotals_tax_excluded"
+        if (
+            self.env.context.get("website_id")
+            and group_ext_id in [tax_included, tax_excluded]
+            and not self.env.context.get("skip_tax_toggle_check")
+        ):
             taxed = request.session.get(
-                'tax_toggle_taxed',
-                self.env.user.with_context(
-                    skip_tax_toggle_check=True
-                ).has_group(tax_included)
+                "tax_toggle_taxed",
+                self.env.user.with_context(skip_tax_toggle_check=True).has_group(
+                    tax_included
+                ),
             )
             return group_ext_id != (tax_excluded if taxed else tax_included)
         return super().has_group(group_ext_id)
 
     # HACK: To clear cache called from res.users write method
     @api.model
-    @ormcache('self._uid', 'group_ext_id')
+    @ormcache("self._uid", "group_ext_id")
     def _has_group(self, group_ext_id):
         return super()._has_group(group_ext_id)
 
