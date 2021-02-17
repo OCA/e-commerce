c58250735be0eed9738443f6ba3e4ebb5bbd141c
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-17 10:05:55 +0100

    [IMP] website_sale_secondary_unit: black, isort, prettier

--- website_sale_secondary_unit/hooks.py
+++ website_sale_secondary_unit/hooks.py
@@ -7,8 +7,10 @@ def post_init_hook(cr, registry):
     At installation time, set allow_uom_sell field as true for all products
     that have already been created.
     """
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE product_template
         SET allow_uom_sell=true
         WHERE allow_uom_sell IS NULL;
-    """)
+    """
+    )
