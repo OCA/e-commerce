76fe3ed6fd2937caeeda81d9c4edb1217763cb7b
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-17 09:36:57 +0100

    [MIG] website_sale_wishlist_keep: Migration to v13.0

--- website_sale_wishlist_keep/__manifest__.py
+++ website_sale_wishlist_keep/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2020 Tecnativa - Carlos Roca
+# Copyright 2021 Tecnativa - Carlos Roca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Website Sale Wishlist Keep",
     "category": "Website",
     "summary": 'Allows to add products to my cart but keep it in my wishlist"',
     "development_status": "Beta",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["website_sale_wishlist"],
-    "data": ["views/assets.xml", "views/website_sale_wishlist_template.xml"],
+    "data": ["templates/assets.xml", "views/website_sale_wishlist_template.xml"],
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "installable": True,
 }
