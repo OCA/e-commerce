e09f29548815f32fa184a9d9ca6be3500d9af596
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-17 12:04:28 +0100

    [IMP] website_sale_tax_toggle: black, isort

--- website_sale_tax_toggle/static/src/js/website_sale_tax_toggle.js
+++ website_sale_tax_toggle/static/src/js/website_sale_tax_toggle.js
@@ -1,7 +1,7 @@
 /* Copyright 2020 Sergio Teruel
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_tax_toggle.tax_toggle_button", function (require) {
+odoo.define("website_sale_tax_toggle.tax_toggle_button", function(require) {
     "use strict";
 
     var sAnimation = require("website.content.snippets.animation");
@@ -9,20 +9,18 @@ odoo.define("website_sale_tax_toggle.tax_toggle_button", function (require) {
     sAnimation.registry.tax_toggle_button = sAnimation.Class.extend({
         selector: ".js_tax_toggle_management",
         events: {
-            'click .js_tax_toggle_btn': '_onPublishBtnClick',
+            "click .js_tax_toggle_btn": "_onPublishBtnClick",
         },
-        _onPublishBtnClick: function (ev) {
+        _onPublishBtnClick: function(ev) {
             ev.preventDefault();
             var self = this;
             var $data = $(ev.currentTarget).parents(".js_tax_toggle_management:first");
             this._rpc({
-                route: $data.data('controller'),
-            })
-            .done(function (result) {
-                $data.find('input').prop("checked", result);
+                route: $data.data("controller"),
+            }).done(function(result) {
+                $data.find("input").prop("checked", result);
                 window.location.reload();
             });
         },
     });
-
 });
--- website_sale_tax_toggle/tests/test_website_sale_tax_toggle.py
+++ website_sale_tax_toggle/tests/test_website_sale_tax_toggle.py
@@ -4,35 +4,37 @@ from odoo.tests.common import HttpCase
 
 
 class WebsiteSaleTaxesToggleHttpCase(HttpCase):
-
     def setUp(self):
         super().setUp()
         # Get company for Mitchel Admin user
-        self.user_admin = self.env.ref('base.user_admin')
+        self.user_admin = self.env.ref("base.user_admin")
         user_company = self.user_admin.company_id
-        self.tax = self.env['account.tax'].create({
-            'name': 'Taxes toggle test tax',
-            'amount_type': 'percent',
-            'amount': 15,
-            'type_tax_use': 'sale',
-            'company_id': user_company.id,
-        })
-        self.product_template = self.env['product.template'].create({
-            'name': 'Product test tax toggle',
-            'list_price': 750.00,
-            'taxes_id': [(6, 0, self.tax.ids)],
-            'website_published': True,
-            'website_sequence': 9999,
-        })
-        pricelist = self.env['product.pricelist'].create({
-            'name': 'Price list for tests',
-            'currency_id': user_company.currency_id.id
-        })
+        self.tax = self.env["account.tax"].create(
+            {
+                "name": "Taxes toggle test tax",
+                "amount_type": "percent",
+                "amount": 15,
+                "type_tax_use": "sale",
+                "company_id": user_company.id,
+            }
+        )
+        self.product_template = self.env["product.template"].create(
+            {
+                "name": "Product test tax toggle",
+                "list_price": 750.00,
+                "taxes_id": [(6, 0, self.tax.ids)],
+                "website_published": True,
+                "website_sequence": 9999,
+            }
+        )
+        pricelist = self.env["product.pricelist"].create(
+            {"name": "Price list for tests", "currency_id": user_company.currency_id.id}
+        )
         self.env.user.partner_id.property_product_pricelist = pricelist
         # To avoid currency converter
-        self.env['res.currency.rate'].search([]).write({
-            'rate': 1,
-        })
+        self.env["res.currency.rate"].search([]).write(
+            {"rate": 1,}
+        )
 
     def test_ui_website(self):
         """Test frontend tour."""
@@ -44,5 +46,5 @@ class WebsiteSaleTaxesToggleHttpCase(HttpCase):
             url_path="/shop",
             code="%s.run('%s')" % tour,
             ready="%s.tours['%s'].ready" % tour,
-            login="admin"
+            login="admin",
         )
