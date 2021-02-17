3dee0c57fbe52fcdf278e3148188fcaa9572a91d
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-17 12:04:29 +0100

    [MIG] website_sale_tax_toggle: Migration to v13.0

--- website_sale_tax_toggle/static/src/js/website_sale_tax_toggle.js
+++ website_sale_tax_toggle/static/src/js/website_sale_tax_toggle.js
@@ -4,7 +4,7 @@
 odoo.define("website_sale_tax_toggle.tax_toggle_button", function(require) {
     "use strict";
 
-    var sAnimation = require("website.content.snippets.animation");
+    const sAnimation = require("website.content.snippets.animation");
 
     sAnimation.registry.tax_toggle_button = sAnimation.Class.extend({
         selector: ".js_tax_toggle_management",
@@ -13,11 +13,12 @@ odoo.define("website_sale_tax_toggle.tax_toggle_button", function(require) {
         },
         _onPublishBtnClick: function(ev) {
             ev.preventDefault();
-            var self = this;
-            var $data = $(ev.currentTarget).parents(".js_tax_toggle_management:first");
+            const $data = $(ev.currentTarget).parents(
+                ".js_tax_toggle_management:first"
+            );
             this._rpc({
                 route: $data.data("controller"),
-            }).done(function(result) {
+            }).then(function(result) {
                 $data.find("input").prop("checked", result);
                 window.location.reload();
             });
