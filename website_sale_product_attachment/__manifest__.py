9e697f681a543ad75460e023da571a856026e27a
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-25 10:39:03 +0100

    [MIG] website_sale_product_attachment: Migration to 13.0

--- website_sale_product_attachment/__manifest__.py
+++ website_sale_product_attachment/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2020 Tecnativa - Jairo Llopis
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "eCommerce product attachments",
     "summary": "Let visitors download attachments from a product page",
-    "version": "12.0.1.1.1",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
@@ -11,10 +12,11 @@
     "maintainers": ["Yajo"],
     "license": "LGPL-3",
     "installable": True,
-    "depends": ["website_sale",],
+    "depends": ["website_sale"],
     "data": [
         "templates/product_template.xml",
         "views/ir_attachment.xml",
         "views/product_template.xml",
     ],
+    "demo": ["demo/assets.xml"],
 }
