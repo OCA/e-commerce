962db557c8c3f244698065572a6cc11b7bd943b9
Carlos Roca <carlos.roca@tecnativa.com>
2021-03-24 15:50:50 +0100

    [IMP] website_sale_tax_toggle: Making test more resilient

--- website_sale_tax_toggle/static/src/js/website_sale_tax_toggle_tour.js
+++ website_sale_tax_toggle/static/src/js/website_sale_tax_toggle_tour.js
@@ -21,6 +21,7 @@ odoo.define("website_sale_tax_toggle.tour", function(require) {
             content: "Enter the product page",
             trigger:
                 ".oe_product_cart:has(span .oe_currency_value:containsExact('862.50')) a:contains('Product test tax toggle')",
+            extra_trigger: ".o_switch_danger:has(input:checked)",
         },
         {
             content: "Toggle tax button click from product page",
@@ -32,6 +33,7 @@ odoo.define("website_sale_tax_toggle.tour", function(require) {
             content: "Check the product price is back to what it should",
             trigger:
                 "#product_details .oe_price .oe_currency_value:containsExact('750.00')",
+            extra_trigger: ".o_switch_danger:has(input:not(:checked))",
         },
     ];
     tour.register(
