17570144c73131d9cc7f6859a9cbe84f1c4b1b9d
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-04-08 12:29:31 -0400

    [IMP] website_sale_stock_provisioning_date: black, isort, prettier

--- website_sale_stock_provisioning_date/tests/test_website_sale_stock_provisioning_date.py
+++ website_sale_stock_provisioning_date/tests/test_website_sale_stock_provisioning_date.py
@@ -1,31 +1,34 @@
 # Copyright 2020 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from datetime import datetime, timedelta
+
 from odoo.tests.common import Form, HttpCase
 
 
 class WebsiteSaleStockProvisioningDate(HttpCase):
-
     def setUp(self):
         super().setUp()
-        product = self.env['product.product'].create({
-            'name': 'product test - provisioning date',
-            'type': 'product',
-            'website_published': True,
-            'show_next_provisioning_date': True,
-        })
-        incoming_picking_type = self.env['stock.picking.type'].search(
+        product = self.env["product.product"].create(
+            {
+                "name": "product test - provisioning date",
+                "type": "product",
+                "website_published": True,
+                "show_next_provisioning_date": True,
+            }
+        )
+        incoming_picking_type = self.env["stock.picking.type"].search(
             [
-                ('code', '=', 'incoming'),
-                '|',
-                ('warehouse_id.company_id', '=', self.env.user.company_id.id),
-                ('warehouse_id', '=', False)
+                ("code", "=", "incoming"),
+                "|",
+                ("warehouse_id.company_id", "=", self.env.user.company_id.id),
+                ("warehouse_id", "=", False),
             ],
             limit=1,
         )
         picking_form = Form(
-            recordp=self.env['stock.picking'].with_context(
-                default_picking_type_id=incoming_picking_type.id),
+            recordp=self.env["stock.picking"].with_context(
+                default_picking_type_id=incoming_picking_type.id
+            ),
             view="stock.view_picking_form",
         )
         with picking_form.move_ids_without_package.new() as move:
@@ -47,5 +50,5 @@ class WebsiteSaleStockProvisioningDate(HttpCase):
             url_path="/shop",
             code="%s.run('%s')" % tour,
             ready="%s.tours['%s'].ready" % tour,
-            login="admin"
+            login="admin",
         )
