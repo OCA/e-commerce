df4a74cbf1f33b984b5e6d46226878cc3beb97d6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-04-09 11:24:11 +0200

    [FIX] website_sale_product_sort: Don't fail on empty POST order + use is_published
    POST data can contain an empty order, so previous code took that one as
    the valid, which is not correct. We need to check if there's a value,
    not only if the key exist.
    
    We use also `is_published` as the field for sorting, the same as
    upstream v13.

--- website_sale_product_sort/controllers/main.py
+++ website_sale_product_sort/controllers/main.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Tecnativa - David Vidal
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.http import request, route
 
@@ -8,15 +9,16 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSale(WebsiteSale):
     def _get_search_order(self, post):
         """We can configure default sort criteria for every website"""
-        return "website_published desc,%s , id desc" % post.get(
-            "order", request.website.default_product_sort_criteria
-        )
+        if post.get("order"):
+            return super()._get_search_order(post)
+        order = request.website.default_product_sort_criteria
+        return "is_published desc, %s, id desc" % order
 
     @route()
     def shop(self, page=0, category=None, search="", ppg=False, **post):
-        """We can configure default sort criteria for every website"""
+        """Transfer custom sort order to QWeb templates."""
         response = super().shop(page=page, category=category, search=search, **post)
-        response.qcontext["order"] = post.get(
-            "order", request.website.default_product_sort_criteria
+        response.qcontext["order"] = (
+            post.get("order") or request.website.default_product_sort_criteria
         )
         return response
