1ee611d781f450420e6758116b2888ef9d3f6ad7
Carlos Roca <carlos.roca@tecnativa.com>
2021-04-09 16:09:15 +0200

    [IMP] website_sale_product_reference_displayed: black, isort

--- website_sale_product_reference_displayed/__manifest__.py
+++ website_sale_product_reference_displayed/__manifest__.py
@@ -5,15 +5,10 @@
     "version": "12.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "website_sale",
-    ],
-    "data": [
-        "views/website_sale_views.xml",
-    ],
+    "depends": ["website_sale",],
+    "data": ["views/website_sale_views.xml",],
 }
