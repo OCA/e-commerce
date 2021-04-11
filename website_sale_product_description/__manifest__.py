6f0e9a9b5f2a1133c93dfcafefb255601135cbff
bosd <evbokhoven@gmail.com>
2021-04-11 02:17:45 +0200

    [MIG] website_sale_product_description: Migration to 13.0

--- website_sale_product_description/__manifest__.py
+++ website_sale_product_description/__manifest__.py
@@ -4,18 +4,13 @@
     "name": "Website Sale Product Description",
     "category": "Website",
     "summary": "Shows custom e-Commerce description for products",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Solvos, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": ["website_sale"],
-    "data": [
-        "views/website_sale_template.xml",
-        "views/product_template.xml",
-    ],
-    "demo": [
-        "data/demo_website_sale_product_description.xml",
-    ],
+    "data": ["views/website_sale_template.xml", "views/product_template.xml",],
+    "demo": ["data/demo_website_sale_product_description.xml",],
 }
