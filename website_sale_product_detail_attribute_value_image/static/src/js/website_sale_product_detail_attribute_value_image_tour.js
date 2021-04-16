4e5cf11177fcd70df12f62d60f95c4fd9ba86cca
Carlos Roca <carlos.roca@tecnativa.com>
2021-04-16 16:31:12 +0200

    [IMP] website_sale_product_detail_attribute_value_image: black, isort

--- website_sale_product_detail_attribute_value_image/static/src/js/website_sale_product_detail_attribute_value_image_tour.js
+++ website_sale_product_detail_attribute_value_image/static/src/js/website_sale_product_detail_attribute_value_image_tour.js
@@ -1,7 +1,9 @@
 /* Copyright 2020 Alexandre D. Díaz
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_product_detail_attribute_value_image.tour", function (require) {
+odoo.define("website_sale_product_detail_attribute_value_image.tour", function(
+    require
+) {
     "use strict";
 
     var tour = require("web_tour.tour");
@@ -13,18 +15,22 @@ odoo.define("website_sale_product_detail_attribute_value_image.tour", function (
         },
         {
             trigger: "*",
-            extra_trigger: ".product-detail-attributes-values:has(span:contains('Policy One Value 1 for website'))",
+            extra_trigger:
+                ".product-detail-attributes-values:has(span:contains('Policy One Value 1 for website'))",
         },
         {
             trigger: "*",
-            extra_trigger: ".product-detail-attributes-values:not(:has(span:contains('High dangerousness')))",
+            extra_trigger:
+                ".product-detail-attributes-values:not(:has(span:contains('High dangerousness')))",
         },
         {
             trigger: "*",
-            extra_trigger: ".product-detail-attributes-values:has(span:contains('Policy Two Value 1'))",
+            extra_trigger:
+                ".product-detail-attributes-values:has(span:contains('Policy Two Value 1'))",
         },
     ];
-    tour.register("website_sale_product_detail_attribute_value_image",
+    tour.register(
+        "website_sale_product_detail_attribute_value_image",
         {
             url: "/shop",
             test: true,
