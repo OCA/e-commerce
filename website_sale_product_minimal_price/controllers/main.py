f8471e49dad33fba2e6d560974ae75c6270f43a4
Carlos Roca <carlos.roca@tecnativa.com>
2021-05-17 16:39:50 +0200

    [IMP] website_sale_product_minimal_price: Forward port of improvements of 12.0
    Forward port adapting the minimal price computing to the new way added on version 13.0.
    
    With the changes added the price takes care on the website selected pricelist and it's added the grid of price scale for each product.variant.
    
    TT24175

--- website_sale_product_minimal_price/controllers/main.py
+++ website_sale_product_minimal_price/controllers/main.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Tecnativa - Carlos Roca
+# Copyright 2021 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import http
@@ -19,30 +19,70 @@ class WebsiteSaleVariantController(VariantController):
         """Special route to use website logic in get_combination_info override.
         This route is called in JS by appending _website to the base route.
         """
-
         res = []
         templates = request.env["product.template"].sudo().browse(product_template_ids)
+        pricelist = request.env["website"].get_current_website().get_current_pricelist()
         for template in templates.filtered(lambda t: t.is_published):
-            cheaper_variant = template.product_variant_ids.sorted(
-                key=lambda p: p._get_combination_info_variant().get("price")
-            )[:1]
+            product_id, add_qty, has_distinct_price = template._get_cheapest_info(
+                pricelist
+            )
+            combination = template._get_combination_info(
+                product_id=product_id, add_qty=add_qty, pricelist=pricelist
+            )
             res.append(
                 {
                     "id": template.id,
-                    "price": cheaper_variant._get_combination_info_variant().get(
-                        "price"
-                    ),
-                    "distinct_prices": self._compute_has_distinct_variant_price(
-                        template
-                    ),
+                    "price": combination.get("price"),
+                    "distinct_prices": has_distinct_price,
                 }
             )
-
         return res
 
-    def _compute_has_distinct_variant_price(self, template):
-        if template.product_variant_count > 1:
-            prices = template.product_variant_ids.mapped("price")
-            if len(prices) > 1:
-                return True
-        return False
+    @http.route(
+        ["/sale/get_combination_info_pricelist_atributes"],
+        type="json",
+        auth="public",
+        website=True,
+    )
+    def get_combination_info_pricelist_atributes(self, product_id, **kwargs):
+        """Special route to use website logic in get_combination_info override.
+        This route is called in JS by appending _website to the base route.
+        """
+        product = request.env["product.product"].browse(product_id)
+        pricelist = request.env["website"].get_current_website().get_current_pricelist()
+        # Getting all min_quantity of the current product to compute the possible
+        # price scale.
+        qty_list = request.env["product.pricelist.item"].search(
+            [
+                "|",
+                ("product_id", "=", product.id),
+                "|",
+                ("product_tmpl_id", "=", product.product_tmpl_id.id),
+                (
+                    "categ_id",
+                    "in",
+                    list(map(int, product.categ_id.parent_path.split("/")[0:-1])),
+                ),
+                ("min_quantity", ">", 0),
+            ]
+        )
+        qty_list = sorted(set(qty_list.mapped("min_quantity")))
+        res = []
+        ctx = dict(request.env.context, pricelist=pricelist.id, quantity=0)
+        last_price = product.with_context(ctx).price
+        for min_qty in qty_list:
+            ctx["quantity"] = min_qty
+            new_price = product.with_context(ctx).price
+            if new_price != last_price:
+                res.append(
+                    {
+                        "min_qty": min_qty,
+                        "price": new_price,
+                        "currency": {
+                            "position": product.currency_id.position,
+                            "symbol": product.currency_id.symbol,
+                        },
+                    }
+                )
+                last_price = new_price
+        return (res, product.uom_name)
