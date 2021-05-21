935ac5a343896fdf62848615fad8ff9c23497417
eLBati <lb@takobi.online>
2021-05-21 17:53:30 +0200

    Backporting website_sale_coupon_delivery to v12

--- website_sale_coupon_delivery/__manifest__.py
+++ website_sale_coupon_delivery/__manifest__.py
@@ -5,10 +5,9 @@
     'summary': """Allows to offer free shippings in coupon reward on eCommerce""",
     'description': """Allows to offer free shippings in coupon reward on eCommerce""",
     'category': 'Website/Website',
-    'version': '1.0',
+    'version': '12.0.1.0.0',
     'depends': ['website_sale_delivery', 'sale_coupon_delivery'],
     'data': [
-        'views/website_sale_coupon_delivery_templates.xml',
     ],
     'auto_install': True,
 }
