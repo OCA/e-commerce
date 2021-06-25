1c525319c678480647d1ff956f9feb9a936ec3c4
miguels73 <55379877+miguels73@users.noreply.github.com>
2021-06-25 13:41:33 +0200

    [MIG] website_sale_product_attribute_value_filter_existing: Migration to 14.0

--- website_sale_product_attribute_value_filter_existing/__manifest__.py
+++ website_sale_product_attribute_value_filter_existing/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Website Sale Attribute Value Existing",
     "summary": "Allow hide attributes values not used in variants",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
