56d4d205e58400da345d28d65afe867b80600c3e
Zar21 <ivanperezfita@gmail.com>
2021-07-27 10:23:44 +0200

    [MIG] website_sale_product_attachment: Migration to 14.0

--- website_sale_product_attachment/__manifest__.py
+++ website_sale_product_attachment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "eCommerce product attachments",
     "summary": "Let visitors download attachments from a product page",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
