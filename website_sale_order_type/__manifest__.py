1eba12294ac0b279d07bd9b3e143dc6b6eabeb64
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-09-16 16:01:29 +0200

    [MIG] website_sale_order_type: Migration to 13.0

--- website_sale_order_type/__manifest__.py
+++ website_sale_order_type/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Website sale order type",
     "summary": "This module allows sale_order_type to work with website_sale.",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce/",
