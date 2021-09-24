bacaec2c15c9f941827a72621cf7c2fb1da23482
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-09-24 16:46:29 +0200

    [IMP] website_sale_product_attachment: Introduce website name
    On previous version, attachments had 2 fields for adding both attachment
    name and file name.
    
    Now on v13, there's only one, that is fille with the file name. On
    initial migration, it was considered that this field is enough, but
    putting file names on the website product page can be ugly, limited and
    confusing, so we are adding here a new field to store the name we want
    to give it for the website e-commerce product page.
    
    It also includes migration script for recovering the old information if
    coming from v12.

--- website_sale_product_attachment/__manifest__.py
+++ website_sale_product_attachment/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2020 Tecnativa - Jairo Llopis
 # Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "eCommerce product attachments",
     "summary": "Let visitors download attachments from a product page",
-    "version": "13.0.1.1.0",
+    "version": "13.0.2.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
