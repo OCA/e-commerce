e694779173d1da9dcdb1c108acf41465ffe3f78a
Katherine Zaoral <kz@adhoc.com.ar>
2021-10-29 18:47:09 -0300

    version2

---
+++ website_sale_product_multi_website/models/website.py
@@ -0,0 +1,16 @@
+from odoo import api, models
+
+
+class Website(models.Model):
+
+    _inherit = "website"
+
+    @api.model
+    def website_domain(self, website_id=False):
+        if self._context.get('multi_website_domain'):
+            return ['|'] + [('website_ids', 'ilike', item) for item in (False, website_id or self.id)]
+        return super().website_domain(website_id=website_id)
+
+    def sale_product_domain(self):
+        """ We add a context in order to change the way that website_domain behavies """
+        return super(Website, self.with_context(multi_website_domain=True)).website_domain()
