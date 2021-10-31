df9bde6f4cc48a0c6899d52d53a1b28b41551f34
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-10-31 06:23:51 -0300

    [FIX] website_sale_product_multi_website

--- website_sale_product_multi_website/models/website.py
+++ website_sale_product_multi_website/models/website.py
@@ -8,9 +8,9 @@ class Website(models.Model):
     @api.model
     def website_domain(self, website_id=False):
         if self._context.get('multi_website_domain'):
-            return ['|'] + [('website_ids', 'ilike', item) for item in (False, website_id or self.id)]
+            return ['|', ('website_ids', '=', False), ('website_ids', '=', website_id or self.id)]
         return super().website_domain(website_id=website_id)
 
     def sale_product_domain(self):
         """ We add a context in order to change the way that website_domain behavies """
-        return super(Website, self.with_context(multi_website_domain=True)).website_domain()
+        return super(Website, self.with_context(multi_website_domain=True)).sale_product_domain()
