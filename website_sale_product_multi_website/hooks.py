be5a381040c9d756170405d6d657a01b53369f31
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-10-31 06:23:51 -0300

    [FIX] website_sale_product_multi_website

--- website_sale_product_multi_website/hooks.py
+++ website_sale_product_multi_website/hooks.py
@@ -5,4 +5,3 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     for rec in env["product.template"].with_context(active_test=False).search([]):
         rec.website_ids += rec.website_id
-        rec.website_id = False
