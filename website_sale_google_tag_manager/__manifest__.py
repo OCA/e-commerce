8fc076577de7841aea6c65bd1c2432e976952ae6
david <david.vidal@tecnativa.com>
2021-11-03 18:00:45 +0100

    [ADD] website_sale_google_tag_manager: New module
    TT32803

---
+++ website_sale_google_tag_manager/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Google Tag Manager Enhanced Conversions",
+    "summary": "Add support for Google Tag Manager Enhanced Conversions",
+    "version": "13.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "category": "eCommerce",
+    "website": "https://github.com/OCA/e-commerce",
+    "depends": ["website_sale", "website_google_tag_manager"],
+    "data": ["views/res_config_settings_view.xml", "views/website_templates.xml"],
+    "images": ["static/description/website_google_tag_manager.png"],
+}
---
+++ website_sale_google_tag_manager/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to configure your Odoo eCommerce to support the Google Tag Manager
+Enhanced Conversions. Read more about them in this link: https://support.google.com/google-ads/answer/9888145?hl=en&ref_topic=3165803
