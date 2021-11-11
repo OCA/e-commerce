99da7a6844291e989aa8c201f5230cff5ed0afe9
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-11 16:51:13 +0100

    [MIG] website_sale_product_brand: Migration to 14.0 TT31863

--- website_sale_product_brand/tests/test_ui.py
+++ website_sale_product_brand/tests/test_ui.py
@@ -1,14 +1,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import HttpCase
+import odoo.tests
 
 
-class UICase(HttpCase):
+@odoo.tests.tagged("post_install", "-at_install")
+class UICase(odoo.tests.HttpCase):
     def test_ui_website(self):
         """Test frontend tour."""
-        tour = "website_sale_product_brand"
-        self.phantom_js(
-            url_path="/shop",
-            code="odoo.__DEBUG__.services['web_tour.tour']" ".run('%s')" % tour,
-            ready="odoo.__DEBUG__.services['web_tour.tour']" ".tours.%s.ready" % tour,
-        )
+        self.start_tour("/shop", "website_sale_product_brand", login="portal")
