fd11ee3cd5af8456d81c729827b90247975ee0ae
Rad0van <radovan@skolnik.info>
2022-01-16 14:54:49 +0100

    [13.0][NEW]website_sale_category_breadcrumb

---
+++ website_sale_category_breadcrumb/__manifest__.py
@@ -0,0 +1,15 @@
+# ©  2021 Radovan Skolnik <radovan@skolnik.info>
+{
+    "name": "eCommerce Product Category Breadcrumb",
+    "category": "Website",
+    "summary": "Displays Product Category Breadcrumb(s) in eCommerce",
+    "version": "13.0.1.0.0",
+    "author": "Radovan Skolnik, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://www.github.com/OCA/e-commerce",
+    "depends": ["website_sale"],
+    "data": ["views/templates.xml"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["Rad0van"],
+}
