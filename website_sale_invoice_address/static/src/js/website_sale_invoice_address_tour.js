c8df1953e7527bdabe64116ce6242976f861066d
david <david.vidal@tecnativa.com>
2022-01-21 16:29:33 +0100

    [MIG] website_sale_invoice_address: Migration to 13.0
    TT28630

--- website_sale_invoice_address/static/src/js/website_sale_invoice_address_tour.js
+++ website_sale_invoice_address/static/src/js/website_sale_invoice_address_tour.js
@@ -1,15 +1,14 @@
 /* Copyright 2020 Tecnativa - David Vidal
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_invoice_address.tour", function(require) {
+odoo.define("website_sale_invoice_address.tour", function (require) {
     "use strict";
 
     var tour = require("web_tour.tour");
-    var base = require("web_editor.base");
 
     var steps = [
         {
-            trigger: "a:contains('Chair floor protection')",
+            trigger: "a:contains('Large Meeting Table')",
         },
         {
             trigger: "a:contains('Add to Cart')",
@@ -23,7 +22,6 @@ odoo.define("website_sale_invoice_address.tour", function(require) {
         {
             url: "/shop",
             test: true,
-            wait_for: base.ready(),
         },
         steps
     );
