ab5a428475900ab61fc67966a275db675aa25095
Reinier Quevedo Batista <reinier@mykubux.com>
2022-02-24 23:31:17 -0500

    [MIG] website_sale_suggest_create_account: Migration to 15.0

--- website_sale_suggest_create_account/__manifest__.py
+++ website_sale_suggest_create_account/__manifest__.py
@@ -3,13 +3,18 @@
 {
     "name": "Suggest to create user account when buying",
     "summary": "Suggest users to create an account when buying in the website",
-    "version": "13.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
-    "author": "Tecnativa, " "LasLabs, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, LasLabs, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
     "depends": ["website_sale"],
-    "data": ["views/website_sale.xml", "views/assets.xml"],
+    "data": ["views/website_sale.xml"],
+    "assets": {
+        "web.assets_tests": [
+            "website_sale_suggest_create_account/static/tests/tours/checkout.js",
+        ],
+    },
     "post_init_hook": "post_init_hook",
 }
