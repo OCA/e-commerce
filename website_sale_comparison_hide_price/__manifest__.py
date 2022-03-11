12597fd200d3e2180e1778373b3f9dea80560381
david <david.vidal@tecnativa.com>
2022-03-11 17:58:51 +0100

    [ADD] website_sale_comparison_hide_price: New module
    TT35076

---
+++ website_sale_comparison_hide_price/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Comparison Hide Price",
+    "version": "13.0.1.0.0",
+    "category": "Website",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "license": "AGPL-3",
+    "summary": "Hide product prices on the shop",
+    "depends": ["website_sale_hide_price", "website_sale_comparison"],
+    "data": ["views/website_sale_template.xml"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ website_sale_comparison_hide_price/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module extends the base of `website_sale_hide_price` to hide prices in the
+comparison views as well.
