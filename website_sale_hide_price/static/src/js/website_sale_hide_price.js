5d18f35cfbf16a34209fe106bf576aea0dabdc5d
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-06-02 10:29:48 +0200

    [MIG] website_sale_hide_price: Migration to 15.0

--- website_sale_hide_price/static/src/js/website_sale_hide_price.js
+++ website_sale_hide_price/static/src/js/website_sale_hide_price.js
@@ -1,6 +1,6 @@
 /* Copyright 2022 Tecnativa - David Vidal
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
-odoo.define("website_sale_hide_price.load", function(require) {
+odoo.define("website_sale_hide_price.load", function (require) {
     "use strict";
     const ajax = require("web.ajax");
     const core = require("web.core");
