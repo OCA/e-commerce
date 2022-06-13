56f07db5b308c725a320afe25a91e51def510c44
Nikunj <youremail@yourdomain.com>
2022-06-13 15:58:57 +0530

    [Add] README.rst in website_sale_stock_provisioning_time.

--- website_sale_stock_provisioning_time/models/product_template.py
+++ website_sale_stock_provisioning_time/models/product_template.py
@@ -4,6 +4,4 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    provisioning_time_in_workdays = fields.Integer(
-        string="Provisioning time in workdays"
-    )
+    provisioning_time_in_workdays = fields.Integer()
---
+++ website_sale_stock_provisioning_time/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Display provisioning time for a product in online shop 
