c1e4a8ce6973fbc16d948986a2f13af4fa9aa1c1
matiasperalta1 <matias_peralta1@hotmail.com>
2022-06-22 12:03:39 -0300

    [MIG] website_sale_exception: Migration to 15.0

--- website_sale_exception/models/sale.py
+++ website_sale_exception/models/sale.py
@@ -1,15 +1,14 @@
 # Copyright 2020 ADHOC
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
-    @api.multi
     def action_confirm(self):
         if self.detect_exceptions():
-            website_draft = self.filtered(lambda o: o.state == 'draft' and o.website_id)
+            website_draft = self.filtered(lambda o: o.state == "draft" and o.website_id)
             if website_draft:
-                website_draft.force_quotation_send()
+                website_draft.action_quotation_sent()
         return super().action_confirm()
