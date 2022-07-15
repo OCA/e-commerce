1778ce0052283ce17cb76815410daca63f794a4e
willytal <guillermo.gallego@tecnativa.com>
2022-07-15 12:00:46 +0200

    [MIG] website_sale_product_reference_displayed: Migration to 15.0

--- website_sale_product_reference_displayed/__manifest__.py
+++ website_sale_product_reference_displayed/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Display product reference in e-commerce",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, Odoo Community Association (OCA)",
