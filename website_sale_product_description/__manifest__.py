8a69ff596b1c6f83f80624ac625a27bdad3c11ec
Mantux11 <mantas@vialaurea.lt>
2022-08-03 11:00:36 +0000

    [15.0][MIG] website_sale_product_description: migration to 15.0

--- website_sale_product_description/__manifest__.py
+++ website_sale_product_description/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Website Sale Product Description",
     "category": "Website",
     "summary": "Shows custom e-Commerce description for products",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Solvos, Odoo Community Association (OCA)",
     "license": "AGPL-3",
