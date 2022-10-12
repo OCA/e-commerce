bb0319997e988092bbef6c29b48f3f7f4383b9ff
SodexisTeam <dev@sodexis.com>
2022-10-12 15:38:34 +0530

    [MIG] website_sale_charge_payment_fee

---
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018 Lorenzo Battistini - Agile Business Group
+# Copyright 2020 AITIC S.A.S
+# Copyright 2020 Quartile Limited
+# Copyright 2022 Sodexis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "eCommerce: charge payment fee",
+    "summary": "Payment fee charged to customer",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Agile Business Group, "
+    "AITIC S.A.S, "
+    "Quartile Limited, "
+    "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["website_sale"],
+    "data": ["views/payment_acquirer_views.xml", "views/website_sale_templates.xml"],
+}
