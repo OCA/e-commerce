ac82790d3cb6ddaf647a88b11e339bc86a7d082c
Hai Lang <hailn@trobz.com>
2022-10-18 14:22:26 +0700

    [IMP] website_sale_credit_point: black, isort, prettier

--- website_sale_credit_point/controllers/website_sale.py
+++ website_sale_credit_point/controllers/website_sale.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 from odoo.exceptions import UserError
 from odoo.http import request
 
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
 
 class WebsiteSale(WebsiteSale):
-
     def checkout_redirection(self, order):
         """Don't process checkout if credit check is not satisfied."""
         redirection = super(WebsiteSale, self).checkout_redirection(order)
@@ -20,5 +18,5 @@ class WebsiteSale(WebsiteSale):
                 order.credit_point_check()
             except UserError as exc:
                 # error msg if not enought points
-                request.session['credit_point_limit_error'] = exc.name
-                return request.redirect('/shop/cart/')
+                request.session["credit_point_limit_error"] = exc.name
+                return request.redirect("/shop/cart/")
