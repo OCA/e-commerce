b1daeda5c66e944449e1bc58950ee1c0f528f6c6
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-11-02 09:07:42 +0100

    [MIG] website_sale_product_attribute_value_filter_existing: Migration to 15.0
    [UPD] Update website_sale_product_attribute_value_filter_existing.pot

--- website_sale_product_attribute_value_filter_existing/static/src/js/website_sale_product_attribute_value_filter_existing_tour.js
+++ website_sale_product_attribute_value_filter_existing/static/src/js/website_sale_product_attribute_value_filter_existing_tour.js
@@ -11,26 +11,20 @@ odoo.define(
 
         var steps = [
             {
-                trigger: "a:contains('Customize')",
-            },
-            {
-                trigger: "a:contains('Product Attribute')",
-            },
-            {
                 trigger: "a[href='/shop']",
             },
             {
                 trigger: "input[name=search]",
                 run: "text Ipod",
-                extra_trigger: ".js_attributes:has(span:contains('Test blue'))",
+                extra_trigger: ".js_attributes:has(label:contains('Test blue'))",
             },
             {
                 trigger: ".oe_search_button",
-                extra_trigger: ".js_attributes:has(span:contains('Test blue'))",
+                extra_trigger: ".js_attributes:has(label:contains('Test blue'))",
             },
             {
                 trigger: "a[href='/shop']",
-                extra_trigger: "li:not(:has(span:contains('Test blue')))",
+                extra_trigger: ".js_attributes:not(:has(label:contains('Test blue')))",
             },
         ];
         tour.register(
