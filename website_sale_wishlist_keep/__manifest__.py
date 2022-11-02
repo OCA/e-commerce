0abe15dfb919d2746e806883031bb1d1986eaaf5
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-11-02 10:20:04 +0100

    [MIG] website_sale_wishlist_keep: Migration to 15.0

--- website_sale_wishlist_keep/__manifest__.py
+++ website_sale_wishlist_keep/__manifest__.py
@@ -4,12 +4,15 @@
     "name": "Website Sale Wishlist Keep",
     "category": "Website",
     "summary": 'Allows to add products to my cart but keep it in my wishlist"',
-    "development_status": "Beta",
-    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["website_sale_wishlist"],
     "website": "https://github.com/OCA/e-commerce",
-    "data": ["templates/assets.xml", "views/website_sale_wishlist_template.xml"],
+    "data": ["views/website_sale_wishlist_template.xml"],
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "installable": True,
+    "assets": {
+        "web.assets_tests": ["/website_sale_wishlist_keep/static/src/js/tour.js"]
+    },
 }
