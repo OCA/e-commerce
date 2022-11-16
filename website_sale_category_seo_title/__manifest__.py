9171bac69af77699edf750439f5cf957d6bc9f8b
miguels73 <55379877+miguels73@users.noreply.github.com>
2022-11-16 15:56:09 +0100

    [14.0][ADD] website_sale_category_seo_title: displays category in page title

---
+++ website_sale_category_seo_title/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Studio73 - Miguel Gandia <miguel@studio73.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Sale Category SEO Title",
+    "summary": "Displays the category name to the page title "
+    "when the user is inside a category in the web store",
+    "license": "AGPL-3",
+    "author": "Studio73, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "category": "website",
+    "version": "14.0.1.0.0",
+    "depends": ["website_sale"],
+    "data": ["views/website_sale.xml"],
+    "installable": True,
+}
---
+++ website_sale_category_seo_title/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+The module displays the category name to the page title when the user is inside a category in the web store. This change improve SEO.
