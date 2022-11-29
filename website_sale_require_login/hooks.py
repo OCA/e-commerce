69d71563bcc45b9740ac727f5bbcb19b00f11640
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-29 12:19:27 +0100

    [IMP] website_sale_require_login: Avoid deprecation warning with `Environment.manage()`

--- website_sale_require_login/hooks.py
+++ website_sale_require_login/hooks.py
@@ -6,7 +6,6 @@ from odoo.tools import config
 
 def post_init_hook(cr, registry):
     if config["test_enable"] or config["test_file"]:
-        with api.Environment.manage():
-            env = api.Environment(cr, SUPERUSER_ID, {})
-            env.ref("website_sale_require_login.cart").active = False
-            env.ref("website_sale_require_login.short_cart_summary").active = False
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        env.ref("website_sale_require_login.cart").active = False
+        env.ref("website_sale_require_login.short_cart_summary").active = False
