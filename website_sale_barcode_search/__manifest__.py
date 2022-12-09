5d928da59360b0ee454a4c7b0d0b7642dd046146
miguels73 <55379877+miguels73@users.noreply.github.com>
2022-12-09 13:43:40 +0100

    [ADD] website_sale_barcode_search: search by barcode in website

---
+++ website_sale_barcode_search/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Studio73 - Miguel Gandia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Barcode Search",
+    "category": "E-Commerce",
+    "summary": "It improve website product search adding search by barcode",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["website_sale"],
+    "author": "Studio73, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "installable": True,
+}
