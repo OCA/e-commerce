70b38014a2c46287e175de2b235dc9c54fe05f79
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-02-23 09:55:22 +0100

    [FIX] website_sale_require_legal: remove dependency to website_legal_page
    TT41842
    
    The dependency on website_legal_page has been removed because it has been merged into account

--- website_sale_require_legal/__manifest__.py
+++ website_sale_require_legal/__manifest__.py
@@ -5,14 +5,14 @@
 {
     "name": "Require accepting legal terms to checkout",
     "summary": "Force the user to accept legal tems to buy in the web shop",
-    "version": "15.0.1.0.0",
+    "version": "15.0.2.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["website_legal_page", "website_sale"],
+    "depends": ["website_sale"],
     "data": ["templates/website_sale.xml"],
     "assets": {
         "web.assets_frontend": [
