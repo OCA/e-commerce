a1dab81ae7589abdc43aeb3fc769193d94e095fa
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-20 18:12:05 +0530

    [MIG][15.0]website_sale_category_breadcrumb:Migration to 15.0

--- website_sale_category_breadcrumb/__manifest__.py
+++ website_sale_category_breadcrumb/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "eCommerce Product Category Breadcrumb",
     "category": "Website",
     "summary": "Displays Product Category Breadcrumb(s) in eCommerce",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Radovan Skolnik, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/e-commerce",
