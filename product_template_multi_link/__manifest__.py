8e684ce94388de4bc244d3dff2de96511fa2dacd
sbejaoui <souheil.bejaoui@acsone.eu>
2023-03-20 14:31:15 +0100

    [16.0][MIG] - product_template_multi_link

--- product_template_multi_link/__manifest__.py
+++ product_template_multi_link/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Multi Links (Template)",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Generic Modules",
     "author": "GRAP, ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
