412c1ce8c3d57fca33be9bb260115558bbd630b0
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-20 17:15:10 +0530

    [MIG][15.0]website_sale_attribute_filter_form_submit:Migration to 15.0

--- website_sale_attribute_filter_form_submit/__manifest__.py
+++ website_sale_attribute_filter_form_submit/__manifest__.py
@@ -3,15 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website manual attribute filters",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "E-Commerce",
     "summary": "Allow to apply manually the filters on the e-commerce",
     "license": "AGPL-3",
     "depends": ["website_sale"],
     "data": [
-        "templates/assets.xml",
-        "templates/website_sale.xml",
+        "views/website_sale.xml",
     ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_sale_attribute_filter_form_submit/static/src/js/website_sale.js"
+        ],
+    },
     "author": "Studio73, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
     "installable": True,
