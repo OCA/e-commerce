8cdda39b1b8cf1b92d0abb442d0b50a16674b041
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-21 18:14:20 +0530

    [MIG][15.0]website_sale_attribute_filter_multiselect:Migration to 15.0

--- website_sale_attribute_filter_multiselect/__manifest__.py
+++ website_sale_attribute_filter_multiselect/__manifest__.py
@@ -4,14 +4,22 @@
     "name": "Website Sale Attribute Filter Multiselect",
     "category": "E-Commerce",
     "summary": "Add multiselect display type for product and new filter for it",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["website_sale"],
     "data": [
-        "templates/assets.xml",
         "templates/sale.xml",
         "templates/website_sale.xml",
     ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_sale_attribute_filter_multiselect/static/src/lib/multiple-select/multiple-select.js",  # noqa: B950
+            "website_sale_attribute_filter_multiselect/static/src/lib/multiple-select/multiple-select-locale-all.js",  # noqa: B950
+            "website_sale_attribute_filter_multiselect/static/src/js/website_sale.js",
+            "website_sale_attribute_filter_multiselect/static/src/lib/multiple-select/multiple-select.css",  # noqa: B950
+            "website_sale_attribute_filter_multiselect/static/src/scss/website_sale.scss",
+        ]
+    },
     "author": "Studio73, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
     "installable": True,
