8e69fc425eaeb6e9a21b44ef6d1b70e73c885255
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-04-03 15:26:37 +0630

    [ADD] website_sale_wishlist_menuitem

---
+++ website_sale_wishlist_menuitem/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sales Wishlist Menuitem",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "category": "Website",
+    "license": "AGPL-3",
+    "depends": ["website_sale_wishlist"],
+    "data": ["views/product_wishlist_views.xml"],
+    "installable": True,
+}
