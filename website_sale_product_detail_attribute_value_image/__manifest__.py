355f441abbc6d3e92ff98dc6f790c14b171c54df
dsolanki <dhara.solanki@initos.com>
2023-04-13 14:30:29 +0530

    [IMP] website_sale_product_detail_attribute_value_image: pre-commit stuff

--- website_sale_product_detail_attribute_value_image/__manifest__.py
+++ website_sale_product_detail_attribute_value_image/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Website Sale Product Detail Attribute Value Image",
     "summary": "Display attributes values images in shop product detail",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
