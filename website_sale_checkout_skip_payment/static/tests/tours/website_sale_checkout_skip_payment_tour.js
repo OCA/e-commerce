d0ce13c69d2b8738c0e1237096949c8a81874dc5
Taylor Locke <kisuka@kisuka.com>
2023-05-22 11:41:46 -0700

    [MIG] website_sale_checkout_skip_payment: Migration to 16.0

---
+++ website_sale_checkout_skip_payment/static/tests/tours/website_sale_checkout_skip_payment_tour.js
@@ -0,0 +1,42 @@
+odoo.define("website_sale_checkout_skip_payment.tour", function (require) {
+    "use strict";
+
+    var tour = require("web_tour.tour");
+    const tourUtils = require("website_sale.tour_utils");
+
+    tour.register(
+        "website_sale_checkout_skip_payment",
+        {
+            test: true,
+            url: "/shop?search=Storage%20Box",
+        },
+        [
+            {
+                content: "select Storage Box",
+                extra_trigger: ".oe_search_found",
+                trigger: '.oe_product_cart a:contains("Storage Box")',
+            },
+            {
+                content: "Add Storage Box into cart",
+                trigger: "a:contains(ADD TO CART)",
+            },
+            tourUtils.goToCart(),
+            {
+                content: "go to checkout",
+                extra_trigger: "#cart_products input.js_quantity:propValue(1)",
+                trigger: 'a[href*="/shop/checkout"]',
+            },
+            {
+                trigger: '.btn-primary:contains("Confirm")',
+            },
+            {
+                trigger: ".btn:contains('Confirm')",
+                extra_trigger: "b:contains('Billing & Shipping:')",
+            },
+            {
+                trigger: "a[href='/shop']",
+                extra_trigger: "strong:contains('Payment Information:')",
+            },
+        ]
+    );
+});
