b63668e788162411bfde6349c3b5026859a647e6
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-09-07 16:55:32 +0200

    [IMP-FIX] website_sale_vat_required: Remove dependency on payment methods in test
    Modify the tests so that we don't have to rely on the payment methods
    module to run the tests for the functionality of this module.
    
    TT44388

--- website_sale_vat_required/tests/test_website_sale_vat_required.py
+++ website_sale_vat_required/tests/test_website_sale_vat_required.py
@@ -1,22 +1,10 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import odoo.tests
 from odoo.tests.common import HttpCase
 
 
+@odoo.tests.tagged("post_install", "-at_install")
 class TestWebsiteSaleVatRequired(HttpCase):
     def test_website_sale_vat_required(self):
-        if self.env["ir.module.module"]._get("payment_custom").state != "installed":
-            self.skipTest("Transfer provider is not installed")
-
-        transfer_provider = self.env.ref("payment.payment_provider_transfer")
-        transfer_provider.write({"state": "enabled", "is_published": True})
-        transfer_provider._transfer_ensure_pending_msg_is_set()
-
-        self.env.user.partner_id.vat = False
-        self.browser_js(
-            url_path="/",
-            code="odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product')",
-            ready="odoo.__DEBUG__.services['web_tour.tour']"
-            ".tours.shop_buy_product.ready",
-            login="admin",
-        )
+        self.start_tour("/shop", "website_sale_vat_required_tour")
