8f462062cf788094c5d3373d0f5a8d2f7ddecdd7
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-09-07 16:55:32 +0200

    [IMP-FIX] website_sale_vat_required: Remove dependency on payment methods in test
    Modify the tests so that we don't have to rely on the payment methods
    module to run the tests for the functionality of this module.
    
    TT44388

--- website_sale_vat_required/static/src/js/website_sale_vat_required.tour.js
+++ website_sale_vat_required/static/src/js/website_sale_vat_required.tour.js
@@ -3,27 +3,52 @@
 odoo.define("website_sale_vat_required.tour", function (require) {
     "use strict";
 
-    require("website_sale.tour");
     var tour = require("web_tour.tour");
-    var steps = tour.tours.shop_buy_product.steps;
 
-    steps.splice(
-        _.findIndex(steps, {content: "go to checkout"}) + 1,
-        0,
+    var steps = [
+        {
+            trigger: "a:contains('Chair floor protection')",
+        },
+        {
+            trigger: "#add_to_cart",
+        },
+        {
+            trigger: "a[href='/shop/cart']",
+            extra_trigger: "sup.my_cart_quantity:contains('1')",
+        },
+        {
+            trigger: ".btn-primary:contains('Process Checkout')",
+        },
         {
             content: "Next",
             trigger: ".btn-primary :contains('Next')",
         },
         {
             content: "Set VAT",
-            trigger: "input[name~='no_country_field'], [name~='vat'].is-invalid",
-            extra_trigger: "input[name~='no_country_field'], [name~='vat'].is-invalid",
+            trigger: "[name~='vat'].is-invalid",
+            extra_trigger: "[name~='vat'].is-invalid",
             run: function () {
                 $("input[name~='no_country_field'], [name~='vat'].is-invalid").val(
                     "US01234567891"
                 );
+                if ($(".div_name").hasClass("o_has_error")) {
+                    $(".div_name input").val("Mr. Test");
+                }
+                if ($("#div_email").hasClass("o_has_error")) {
+                    $("#div_email input").val("test@test.com");
+                }
                 if ($("#div_phone").hasClass("o_has_error")) {
                     $("#div_phone input").val("11111111");
+                }
+                if ($(".div_street").hasClass("o_has_error")) {
+                    $(".div_street input").val("Test Street");
+                }
+                if ($(".div_city").hasClass("o_has_error")) {
+                    $(".div_city input").val("Test City");
+                }
+                $(".div_zip input").val("12345");
+                if ($(".div_country").hasClass("o_has_error")) {
+                    $("#country_id option:eq(1)").attr("selected", true);
                 }
                 $('div.o_has_error input[name="accepted_legal_terms"]').prop(
                     "checked",
@@ -35,9 +60,16 @@ odoo.define("website_sale_vat_required.tour", function (require) {
             content: "Next",
             trigger: ".btn-primary :contains('Next')",
         },
+    ];
+    tour.register(
+        "website_sale_vat_required_tour",
         {
-            content: "Confirm",
-            trigger: ".btn-primary :contains('Confirm')",
-        }
+            url: "/shop",
+            test: true,
+        },
+        steps
     );
+    return {
+        steps: steps,
+    };
 });
