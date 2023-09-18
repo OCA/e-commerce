9b2f615bec10cc31cc554fb8d1b72e4fd85b8c9a
ernesto-garcia-tecnativa <ernesto.garcia@tecnativa.com>
2023-09-18 01:50:25 -0600

    [MIG] website_sale_stock_provisioning_date: Migration to 16.0

--- website_sale_stock_provisioning_date/tests/test_website_sale_stock_provisioning_date.py
+++ website_sale_stock_provisioning_date/tests/test_website_sale_stock_provisioning_date.py
@@ -2,13 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from datetime import datetime, timedelta
 
-from odoo.tests.common import Form, HttpCase
+from odoo.tests.common import Form, HttpCase, tagged
 
 
+@tagged("post_install", "-at_install")
 class WebsiteSaleStockProvisioningDate(HttpCase):
-    def setUp(self):
-        super().setUp()
-        product = self.env["product.product"].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        product = cls.env["product.product"].create(
             {
                 "name": "product test - provisioning date",
                 "type": "product",
@@ -16,17 +18,17 @@ class WebsiteSaleStockProvisioningDate(HttpCase):
                 "show_next_provisioning_date": True,
             }
         )
-        incoming_picking_type = self.env["stock.picking.type"].search(
+        incoming_picking_type = cls.env["stock.picking.type"].search(
             [
                 ("code", "=", "incoming"),
                 "|",
-                ("warehouse_id.company_id", "=", self.env.user.company_id.id),
+                ("warehouse_id.company_id", "=", cls.env.user.company_id.id),
                 ("warehouse_id", "=", False),
             ],
             limit=1,
         )
         picking_form = Form(
-            recordp=self.env["stock.picking"].with_context(
+            recordp=cls.env["stock.picking"].with_context(
                 default_picking_type_id=incoming_picking_type.id
             ),
             view="stock.view_picking_form",
@@ -42,13 +44,8 @@ class WebsiteSaleStockProvisioningDate(HttpCase):
 
     def test_ui_website(self):
         """Test frontend tour."""
-        tour = (
-            "odoo.__DEBUG__.services['web_tour.tour']",
+        self.start_tour(
+            "/shop",
             "website_sale_stock_provisioning_date",
-        )
-        self.browser_js(
-            url_path="/shop",
-            code="%s.run('%s')" % tour,
-            ready="%s.tours['%s'].ready" % tour,
             login="admin",
         )
