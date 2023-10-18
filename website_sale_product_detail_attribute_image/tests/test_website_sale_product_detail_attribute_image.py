7c0808762762a5a847b61a37b7dec63e7be253e5
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-18 11:28:49 +0200

    [IMP] website_sale_product_detail_attribute_image: test performance improvement - Switch to setUpClass for avoiding repeat the same setup for each test. - Include context keys for avoiding mail operations overhead.

--- website_sale_product_detail_attribute_image/tests/test_website_sale_product_detail_attribute_image.py
+++ website_sale_product_detail_attribute_image/tests/test_website_sale_product_detail_attribute_image.py
@@ -2,18 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.tests import HttpCase, tagged
 
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
 
 @tagged("post_install", "-at_install")
 class WebsiteSaleHttpCase(HttpCase):
-    def setUp(self):
-        super().setUp()
-        self.ProductAttribute = self.env["product.attribute"]
-        self.ProductAttributeValue = self.env["product.attribute.value"]
-        self.ProductAttributeLine = self.env["product.template.attribute.line"]
-
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+        cls.ProductAttribute = cls.env["product.attribute"]
+        cls.ProductAttributeValue = cls.env["product.attribute.value"]
+        cls.ProductAttributeLine = cls.env["product.template.attribute.line"]
         detail_image = "R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-
-        attribute_dangerous = self.ProductAttribute.create(
+        attribute_dangerous = cls.ProductAttribute.create(
             {
                 "name": "Dangerousness",
                 "create_variant": "no_variant",
@@ -21,24 +23,22 @@ class WebsiteSaleHttpCase(HttpCase):
                 "website_product_detail_image": detail_image,
             }
         )
-        product_attribute_image_high = self.ProductAttributeValue.create(
+        product_attribute_image_high = cls.ProductAttributeValue.create(
             {"name": "High dangerousness", "attribute_id": attribute_dangerous.id}
         )
-        self.ProductAttributeValue.create(
+        cls.ProductAttributeValue.create(
             {"name": "Low dangerousness", "attribute_id": attribute_dangerous.id}
         )
-        self.product_template = self.env.ref(
-            "product.product_product_4_product_template"
-        )
-        attribute_line = self.ProductAttributeLine.create(
+        cls.product_template = cls.env.ref("product.product_product_4_product_template")
+        attribute_line = cls.ProductAttributeLine.create(
             {
-                "product_tmpl_id": self.product_template.id,
+                "product_tmpl_id": cls.product_template.id,
                 "attribute_id": attribute_dangerous.id,
                 "value_ids": [(6, 0, product_attribute_image_high.ids)],
             }
         )
-        self.product_template.write({"attribute_line_ids": [(4, attribute_line.id)]})
-        attribute_image_policy_one = self.ProductAttribute.create(
+        cls.product_template.write({"attribute_line_ids": [(4, attribute_line.id)]})
+        attribute_image_policy_one = cls.ProductAttribute.create(
             {
                 "name": "Policy One",
                 "website_name": "Website Policy One",
@@ -47,23 +47,22 @@ class WebsiteSaleHttpCase(HttpCase):
                 "website_product_detail_image": detail_image,
             }
         )
-        value_image_policy_one_1 = self.ProductAttributeValue.create(
+        value_image_policy_one_1 = cls.ProductAttributeValue.create(
             {
                 "name": "Policy One Value 1",
                 "website_name": "Policy One Value 1 for website",
                 "attribute_id": attribute_image_policy_one.id,
             }
         )
-        attribute_line = self.ProductAttributeLine.create(
+        attribute_line = cls.ProductAttributeLine.create(
             {
-                "product_tmpl_id": self.product_template.id,
+                "product_tmpl_id": cls.product_template.id,
                 "attribute_id": attribute_image_policy_one.id,
                 "value_ids": [(6, 0, value_image_policy_one_1.ids)],
             }
         )
-        self.product_template.write({"attribute_line_ids": [(4, attribute_line.id)]})
-
-        attribute_image_policy_two = self.ProductAttribute.create(
+        cls.product_template.write({"attribute_line_ids": [(4, attribute_line.id)]})
+        attribute_image_policy_two = cls.ProductAttribute.create(
             {
                 "name": "Policy Two",
                 "create_variant": "no_variant",
@@ -71,20 +70,20 @@ class WebsiteSaleHttpCase(HttpCase):
                 "website_product_detail_image": detail_image,
             }
         )
-        value_image_policy_two_1 = self.ProductAttributeValue.create(
+        value_image_policy_two_1 = cls.ProductAttributeValue.create(
             {
                 "name": "Policy Two Value 1",
                 "attribute_id": attribute_image_policy_two.id,
             }
         )
-        attribute_line = self.ProductAttributeLine.create(
+        attribute_line = cls.ProductAttributeLine.create(
             {
-                "product_tmpl_id": self.product_template.id,
+                "product_tmpl_id": cls.product_template.id,
                 "attribute_id": attribute_image_policy_two.id,
                 "value_ids": [(6, 0, value_image_policy_two_1.ids)],
             }
         )
-        self.product_template.write({"attribute_line_ids": [(4, attribute_line.id)]})
+        cls.product_template.write({"attribute_line_ids": [(4, attribute_line.id)]})
 
     def test_ui_website(self):
         """Test frontend tour."""
