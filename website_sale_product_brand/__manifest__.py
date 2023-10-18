a7649ff344744f37e606b8a7b1c4aa8027ac2874
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-18 11:26:23 +0200

    [IMP] website_sale_product_brand: test performance improvement - Create setUpClass method - Include context keys for avoiding mail operations overhead.

--- website_sale_product_brand/__manifest__.py
+++ website_sale_product_brand/__manifest__.py
@@ -8,7 +8,7 @@
     "Tecnativa, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "depends": ["product_brand", "website_sale"],
     "data": [
