7308ade265763fedb4ed557f888a5ffd864c7ba0
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-10-20 13:03:24 +0200

    [FIX] website_sale_product_attribute_value_filter_existing: fix test to check the real functionality of the module
    TT45590

--- website_sale_product_attribute_value_filter_existing/static/src/js/website_sale_product_attribute_value_filter_existing_tour.esm.js
+++ website_sale_product_attribute_value_filter_existing/static/src/js/website_sale_product_attribute_value_filter_existing_tour.esm.js
@@ -8,26 +8,36 @@ tour.register(
         test: true,
         url: "/shop",
     },
+    // No product has the yellow colour attribute defined.
+    // When enter to "/shop" the attribute "test yellow" should not appear in the list of filters by attribute.
     [
+        // For the following steps it is checked that the attribute "test yellow" is
+        // not present in the list but the attributes "test red", "test blue" and
+        // "test green" must be present.
         {
-            content: "1: browse shop page",
+            content:
+                "Search a product. Ensure 'test red', 'test blue' and 'test green' attributes are present while 'Test yellow' is not.",
+            trigger: "form input[name=search]",
+            run: "text desk",
+            extra_trigger:
+                ".js_attributes:not(:contains('Test yellow')):has(label:contains('Test red')), .js_attributes:not(:contains('Test yellow')):has(label:contains('Test blue')), .js_attributes:not(:contains('Test yellow')):has(label:contains('Test green'))",
+        },
+        {
+            content:
+                "Submit search button. Ensure 'test red', 'test blue' and 'test green' attributes are present while 'Test yellow' is not.",
+            trigger: 'form:has(input[name="search"]) .oe_search_button',
+            extra_trigger:
+                ".js_attributes:not(:contains('Test yellow')):has(label:contains('Test red')), .js_attributes:not(:contains('Test yellow')):has(label:contains('Test blue')), .js_attributes:not(:contains('Test yellow')):has(label:contains('Test green'))",
+        },
+        // After searching, the attributes "test red" and "test green" must be present.
+        // "Test yelow" should not be present as it is not used in any product and
+        // "test blue" should not be present as it is not used in the products shown.
+        {
+            content:
+                "Go to /shop after the search. Ensure 'test red' and 'test green' attributes are present while 'Test yellow' and 'test blue' are not.",
             trigger: "a[href='/shop']",
-        },
-        {
-            content: "2: search a product",
-            trigger: "input[name=search]",
-            run: "text Ipod",
-            extra_trigger: ".js_attributes:has(label:contains('Test blue'))",
-        },
-        {
-            content: "3: submit search button",
-            trigger: ".oe_search_button",
-            extra_trigger: ".js_attributes:has(label:contains('Test blue'))",
-        },
-        {
-            content: "4: browse shop page after search",
-            trigger: "a[href='/shop']",
-            extra_trigger: ".js_attributes:not(:has(label:contains('Test blue')))",
+            extra_trigger:
+                ".js_attributes:not(:contains('Test blue'), :contains('Test yellow')):has(label:contains('Test green')), .js_attributes:not(:contains('Test blue'), :contains('Test yellow')):has(label:contains('Test red'))",
         },
     ]
 );
