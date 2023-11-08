c835880c77aa3aeeac12007f3b2bf13e3734c33a
Pablo De Andrés <pablo.deandres@factorlibre.com>
2023-11-08 15:19:20 +0100

    [16.0][FIX] website_sale_hide_price: Error loading view after update

--- website_sale_hide_price/__manifest__.py
+++ website_sale_hide_price/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Sale Hide Price",
-    "version": "16.0.1.3.0",
+    "version": "16.0.1.4.0",
     "category": "Website",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
