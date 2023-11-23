62338e49cb37095510d8e845cec6727271cb71f6
lav-adhoc <lav@adhoc.com.ar>
2023-11-23 15:20:31 -0300

    [MIG] website_sale_product_multi_website: Migration to 16.0

--- website_sale_product_multi_website/tests/website_sale_product_multi_website.py
+++ website_sale_product_multi_website/tests/website_sale_product_multi_website.py
@@ -1,22 +1,23 @@
-from odoo.tests
+import odoo.tests
 
 
-class TestWebsiteSaleProductMultiWebsite(tests.HttpCase):
-
+class TestWebsiteSaleProductMultiWebsite(odoo.tests.HttpCase):
     def setUp(self):
         super().setUp()
         # Create multiple websites
-        self.website0 = self.env['website'].create({'name': 'web0'})
-        self.website1 = self.env['website'].create({'name': 'web1'})
-        self.website2 = self.env['website'].create({'name': 'web2'})
+        self.website0 = self.env["website"].create({"name": "web0"})
+        self.website1 = self.env["website"].create({"name": "web1"})
+        self.website2 = self.env["website"].create({"name": "web2"})
 
         # create a product template
-        self.product_template = self.env['product.template'].create({
-            'name': 'Test Product',
-            'is_published': True,
-            'list_price': 750,
-        })
+        self.product_template = self.env["product.template"].create(
+            {
+                "name": "Test Product",
+                "is_published": True,
+                "list_price": 750,
+            }
+        )
 
     def test_01(self):
-        """ Prueba que si el producto no tiene websites dicho producto esta disponible en todos los websites
-        """
+        """Prueba que si el producto no tiene websites
+        dicho producto esta disponible en todos los websites"""
