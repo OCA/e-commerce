ffc6bcdec6e2e64a338a672a23244f6310c56701
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2024-01-08 13:19:37 +0100

    [ADD] website_sale_wishlist_hide_price: New module
    TT46960

---
+++ website_sale_wishlist_hide_price/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Tecnativa - Pilar Vargas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Wishlist Hide Price",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "license": "AGPL-3",
+    "summary": "Hide product prices on the shop",
+    "depends": ["website_sale_hide_price", "website_sale_wishlist"],
+    "data": ["views/website_sale_template.xml"],
+    "installable": True,
+    "auto_install": True,
+    "assets": {
+        "web.assets_tests": [
+            "/website_sale_wishlist_hide_price/static/tests/tours/*.js",
+        ]
+    },
+}
