5a8360544dbdd28cc5fd7333e0c2f6dd38eeebd7
dessanhemrayev <dessanhemrayev@gmail.com>
2024-01-17 02:49:57 +0300

    [IMP] website_sale_product_item_cart_custom_qty
    Add a setting in /shop page > customize: Always display qty buttons
    Qty and add to cart buttons are always visible (not only on rollover)
    To improve the visualization, move the buttons below the image so that they do not overlap it, now it occupies about 20% of the image height.

--- website_sale_product_item_cart_custom_qty/tests/test_website_sale_product_item_cart_custom_qty.py
+++ website_sale_product_item_cart_custom_qty/tests/test_website_sale_product_item_cart_custom_qty.py
@@ -1,18 +1,27 @@
 # Copyright 2021 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import HttpCase
+from odoo.tests import HttpCase, tagged
 
 
-class TestWebsiteSaleProductItemCartCustomQty(HttpCase):
+@tagged("post_install", "-at_install")
+class TestWebsiteSaleProductItemCartCustomQtyHttp(HttpCase):
     def setUp(self):
         super().setUp()
-        view = self.env.ref("website_sale.products_add_to_cart")
-        view.active = True
+        self.products_add_to_cart = self.env["website"].viewref(
+            "website_sale.products_add_to_cart", raise_if_not_found=False
+        )
+        self.always_display_qty_buttons = self.env["website"].viewref(
+            "website_sale_product_item_cart_custom_qty.always_display_qty_buttons",
+            raise_if_not_found=False,
+        )
         self.env["product.template"].create(
             {"name": "Test Product", "is_published": True, "website_sequence": 1}
         )
 
-    def test_ui_website(self):
+        self.products_add_to_cart.active = True
+        self.always_display_qty_buttons.active = True
+
+    def test_frontend_website(self):
         """Test frontend tour."""
         self.start_tour(
             "/shop", "website_sale_product_item_cart_custom_qty", login="admin"
