b2364154d072486870659c4e629dfd2ae74fc79e
Daniel Duque <daniel.duque@factorlibre.com>
2024-02-06 12:02:11 +0100

    [FIX] website_sale_cart_clear: event inheritance

--- website_sale_cart_clear/static/src/js/widget.js
+++ website_sale_cart_clear/static/src/js/widget.js
@@ -4,9 +4,9 @@ odoo.define("website_sale_cart_clear.widget", function (require) {
     var websiteSaleWidgets = require("website_sale.website_sale");
 
     websiteSaleWidgets.websiteSaleCart.include({
-        events: {
+        events: _.extend(websiteSaleWidgets.websiteSaleCart.prototype.events, {
             "click .js_clear_cart": "_onClickClearCart",
-        },
+        }),
 
         _onClickClearCart() {
             return this._rpc({
