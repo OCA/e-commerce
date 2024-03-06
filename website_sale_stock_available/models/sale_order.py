54da69cdda75ab4f0b9c0c54191e7663a9dd0809
Daniel Duque <daniel.duque@factorlibre.com>
2024-03-06 13:14:42 +0100

    [FIX] website_sale_stock_available: respect args
    Keep args and kwargs as they are, given also that module's
    inheritance does not use them

--- website_sale_stock_available/models/sale_order.py
+++ website_sale_stock_available/models/sale_order.py
@@ -7,10 +7,6 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _cart_update(
-        self, product_id=None, line_id=None, add_qty=0, set_qty=0, **kwargs
-    ):
+    def _cart_update(self, *args, **kwargs):
         order = self.with_context(website_sale_stock_available=True)
-        return super(SaleOrder, order)._cart_update(
-            product_id, line_id, add_qty, set_qty, **kwargs
-        )
+        return super(SaleOrder, order)._cart_update(*args, **kwargs)
