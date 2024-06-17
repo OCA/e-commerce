5c0493b67f2f823d3ef05ef51bf2a20b77fcdb25
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2024-06-17 09:05:15 +0200

    [FIX] website_sale_vat_required: fix test
    The tests were using a demo data product so if at any time this product
    does not exist the tests start to fail. To avoid this, a new product is
    created and used for testing. Also to locate the checkout button it is
    better to locate it by its container element as the classes can change
    depending on the configuration and modules used in the instance and it
    can be a problem to locate it.

--- website_sale_vat_required/tests/test_website_sale_vat_required.py
+++ website_sale_vat_required/tests/test_website_sale_vat_required.py
@@ -6,5 +6,16 @@ from odoo.tests.common import HttpCase
 
 @odoo.tests.tagged("post_install", "-at_install")
 class TestWebsiteSaleVatRequired(HttpCase):
+    def setUp(self):
+        super().setUp()
+        self.product = self.env["product.template"].create(
+            {
+                "name": "Test Product Vat Required",
+                "is_published": True,
+                "website_sequence": 1,
+                "type": "consu",
+            }
+        )
+
     def test_website_sale_vat_required(self):
         self.start_tour("/shop", "website_sale_vat_required_tour")
