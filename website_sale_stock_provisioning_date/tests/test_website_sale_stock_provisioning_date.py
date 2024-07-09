c0895662df7c92fc4b1896d72d79327e626d061f
arantxa-s73 <arantxa@studio73.es>
2024-07-09 08:06:54 +0200

    [MIG] website_sale_stock_provisioning_date: Migration to 17.0

--- website_sale_stock_provisioning_date/tests/test_website_sale_stock_provisioning_date.py
+++ website_sale_stock_provisioning_date/tests/test_website_sale_stock_provisioning_date.py
@@ -28,7 +28,7 @@ class WebsiteSaleStockProvisioningDate(HttpCase):
             limit=1,
         )
         picking_form = Form(
-            recordp=cls.env["stock.picking"].with_context(
+            cls.env["stock.picking"].with_context(
                 default_picking_type_id=incoming_picking_type.id
             ),
             view="stock.view_picking_form",
@@ -42,6 +42,53 @@ class WebsiteSaleStockProvisioningDate(HttpCase):
         picking = picking_form.save()
         picking.action_confirm()
 
+        cls.warehouse_1 = cls.env["stock.warehouse"].search(
+            [("company_id", "=", cls.env.company.id)]
+        )
+        # Create two stockable products
+        cls.product_A = cls.env["product.product"].create(
+            {
+                "name": "Product A",
+                "allow_out_of_stock_order": False,
+                "type": "product",
+                "default_code": "E-COM1",
+            }
+        )
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test Partner", "email": "prueba@pruebae.es"}
+        )
+        # Add 10 Product A in WH1 and 15 Product 1 in WH2
+        quants = (
+            cls.env["stock.quant"]
+            .with_context(inventory_mode=True)
+            .create(
+                [
+                    {
+                        "product_id": cls.product_A.id,
+                        "inventory_quantity": 10.0,
+                        "location_id": cls.warehouse_1.lot_stock_id.id,
+                    }
+                ]
+            )
+        )
+        quants.action_apply_inventory()
+        sale = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product_A.id,
+                            "product_uom_qty": 3,
+                        },
+                    )
+                ],
+            }
+        )
+        sale.action_confirm()
+
     def test_ui_website(self):
         """Test frontend tour."""
         self.start_tour(
@@ -49,3 +96,9 @@ class WebsiteSaleStockProvisioningDate(HttpCase):
             "website_sale_stock_provisioning_date",
             login="admin",
         )
+
+    def test_search_qty_free(self):
+        templates = self.env["product.template"].search(
+            [("free_qty", ">", 0), ("id", "=", self.product_A.product_tmpl_id.id)]
+        )
+        self.assertEqual(templates.free_qty, 7)
