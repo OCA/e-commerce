21b9a3361f30a4e9fa1151ac8388161f531909b0
Sakthivel <sakthivel@sodexis.com>
2024-08-22 12:28:41 +0530

    [MIG] website_sale_wishlist_hide_price: Migration to 17.0

--- website_sale_wishlist_hide_price/__manifest__.py
+++ website_sale_wishlist_hide_price/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Sale Wishlist Hide Price",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Website",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
--- website_sale_wishlist_hide_price/readme/DESCRIPTION.rst
+++
@@ -1,2 +0,0 @@
-This module extends the base of `website_sale_hide_price` to hide prices in the
-wishlist views as well.
