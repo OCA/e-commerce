19e6eb3ddd8e3d66376e312a2f56f0461c94efd2
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-01-15 09:35:19 -0500

    [MIG] website_sale_product_brand: Migration to version 17.0

--- website_sale_product_brand/static/src/js/tour.esm.js
+++ website_sale_product_brand/static/src/js/tour.esm.js
@@ -1,11 +1,12 @@
+/** @odoo-module **/
 /* License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("website_sale_product_brand.tour", function (require) {
-    "use strict";
+import {registry} from "@web/core/registry";
 
-    var tour = require("web_tour.tour");
-
-    var steps = [
+registry.category("web_tour.tours").add("website_sale_product_brand", {
+    test: true,
+    url: "/",
+    steps: () => [
         {
             trigger: "a[href='/page/product_brands']",
             content: "Go to 'Product brand' page",
@@ -26,14 +27,5 @@ odoo.define("website_sale_product_brand.tour", function (require) {
             content: "select Apple",
             trigger: 'section a div:contains("Apple")',
         },
-    ];
-
-    tour.register(
-        "website_sale_product_brand",
-        {
-            url: "/",
-            test: true,
-        },
-        steps
-    );
+    ],
 });
