ef20f97cc18cb5598accae9883c5e9d92f7f0498
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-02-26 07:58:06 -0500

    [MIG] website_sale_vat_required: Migration to version 18.0 Adapt according to this commit: https://github.com/odoo/odoo/commit/453cfab758505ae15135703fb7be8bdb54981444

--- website_sale_vat_required/__manifest__.py
+++ website_sale_vat_required/__manifest__.py
@@ -5,13 +5,12 @@
 {
     "name": "e-commerce required VAT",
     "summary": "VAT number required in checkout form",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Website",
     "author": "Agile Business Group, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
     "license": "AGPL-3",
     "depends": ["website_sale", "base_vat"],
-    "data": ["views/templates.xml"],
     "installable": True,
     "auto_install": False,
     "assets": {
