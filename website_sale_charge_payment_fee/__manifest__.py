9230cec95700176b175423438cdbe49d8c203107
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2025-02-27 09:05:27 -0500

    [MIG] website_sale_charge_payment_fee: Migration to 16.0

--- website_sale_charge_payment_fee/__manifest__.py
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "eCommerce: charge payment fee",
     "summary": "Payment fee charged to customer",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "maintainers": ["miguel-S73"],
