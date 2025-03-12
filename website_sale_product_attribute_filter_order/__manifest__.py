1fe528344c756e460a52576ed143290f45953b98
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-03-12 08:47:11 -0500

    website_sale_product_attribute_filter_order: Migration to version 17.0

--- website_sale_product_attribute_filter_order/__manifest__.py
+++ website_sale_product_attribute_filter_order/__manifest__.py
@@ -2,14 +2,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Website Sale Attribute Filter Order",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Website",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
     "license": "LGPL-3",
     "summary": "Move active checkbox options to the first place of the list",
     "depends": ["website_sale"],
-    "data": ["templates/website_sale.xml"],
+    "data": ["templates/website_sale.xml", "views/snippets/snippets.xml"],
     "installable": True,
     "maintainers": ["Tardo"],
 }
