971a6ef14e9c6f861ab3d9b978743fc3f056414b
maciej-wichowski <maciej@versada.eu>
2025-04-07 14:45:52 +0200

    [ADD] website_sale_hide_price_wishlist
    Hide prices on wishlist

---
+++ website_sale_hide_price_wishlist/__manifest__.py
@@ -0,0 +1,19 @@
+# Author: Maciej Wichowski. Copyright: Versada UAB.
+# See LICENSE and COPYRIGHT files for details.
+{
+    "name": "Website Sale Hide Price on Wishlist",
+    "version": "17.0.1.0.0",
+    "summary": "Hide product prices on the wishlist",
+    "license": "AGPL-3",
+    "author": "Versada, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "category": "Website",
+    "depends": [
+        # odoo
+        "website_sale_hide_price",
+        "website_sale_wishlist",
+    ],
+    "data": ["views/website_sale_templates.xml"],
+    "installable": True,
+    "auto_install": True,
+}
