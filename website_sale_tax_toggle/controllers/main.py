fb7b9d168ec5f59364e7faf351bb99b58c427d61
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-04-10 11:37:51 +0200

    [IMP-FIX] website_sale_tax_toggle: Set the value of the option to display taxes for the entire website.
    TT55895

--- website_sale_tax_toggle/controllers/main.py
+++ website_sale_tax_toggle/controllers/main.py
@@ -9,31 +9,6 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class WebsiteSaleTaxToggle(WebsiteSale):
-    @http.route()
-    def shop(
-        self,
-        page=0,
-        category=None,
-        search="",
-        min_price=0.0,
-        max_price=0.0,
-        ppg=False,
-        **post
-    ):
-        res = super().shop(
-            page=page,
-            category=category,
-            search=search,
-            min_price=min_price,
-            max_price=max_price,
-            ppg=ppg,
-            **post
-        )
-        if request.session.get("tax_toggle_taxed") is None:
-            tax_toggle_preactivated = request.website.tax_toggle_preactivated
-            request.session["tax_toggle_taxed"] = tax_toggle_preactivated
-        return res
-
     @http.route(["/website/tax_toggle"], type="json", auth="public", website=True)
     def tax_toggle(self):
         if request.session.get("tax_toggle_taxed") is None:
