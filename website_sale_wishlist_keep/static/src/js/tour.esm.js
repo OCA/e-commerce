5428caf1eff7c7b736f885baf17240ed43f6dd1e
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-05-19 14:10:31 +0200

    [MIG] website_sale_wishlist_keep: Migration to version 18.0
    TT54400

--- website_sale_wishlist_keep/static/src/js/tour.esm.js
+++ website_sale_wishlist_keep/static/src/js/tour.esm.js
@@ -1,5 +1,3 @@
-/** @odoo-module **/
-
 /* Copyright 2021 Carlos Roca
  * Copyright 2025 Tecnativa - Pilar Vargas
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
@@ -8,34 +6,65 @@ import {registry} from "@web/core/registry";
 
 registry.category("web_tour.tours").add("website_sale_wishlist_keep", {
     test: true,
-    url: "/shop",
+    url: "/shop?search=Test Product",
     steps: () => [
         {
-            content: "Add Test Product to wishlist from /shop",
-            extra_trigger: '.oe_product_cart:contains("Test Product")',
-            trigger: '.oe_product_cart:contains("Test Product") .o_add_wishlist',
+            content: "hover card && click on add to wishlist",
+            trigger: ".o_wsale_product_grid_wrapper:contains(Test Product)",
+            run: "hover && click .o_add_wishlist",
+        },
+        {
+            content: "The product has been successfully added to your wishlist.",
+            trigger: 'a[href="/shop/wishlist"] .badge:contains(1)',
         },
         {
             content: "go to wishlist",
-            extra_trigger: 'a[href="/shop/wishlist"] .badge:contains(1)',
             trigger: 'a[href="/shop/wishlist"]',
+            run: "click",
         },
         {
+            content: "Ensure that the option keep in wish list is ticked.",
+            trigger: "#b2b_wish[checked='True']",
+        },
+        {
+            content: "get out of the wish list",
+            trigger: "a[href='/shop']",
+            run: "click",
+        },
+        {
+            content: "go back to wishlist",
+            trigger: 'a[href="/shop/wishlist"]',
+            run: "click",
+        },
+        {
+            content: "Ensure that the option to keep in the wish list remains ticked.",
+            trigger: "#b2b_wish[checked='True']",
+        },
+        {
+            content: "Add product to cart",
             trigger: ".o_wish_add",
+            run: "click",
         },
         {
-            trigger: "#b2b_wish",
-            extra_trigger: "a:contains('Test Product')",
+            content: "Ensure that the option to keep in the wish list remains ticked.",
+            trigger: "#b2b_wish[checked='True']",
         },
         {
-            trigger: ".o_wish_add",
-        },
-        {
+            content: "Go to cart",
             trigger: "a[href='/shop/cart']",
+            run: "click",
         },
         {
             trigger: "a:contains('Test Product')",
-            extra_trigger: "a:contains('Checkout')",
+        },
+        {
+            content: "go back to wishlist",
+            trigger: 'a[href="/shop/wishlist"]',
+            run: "click",
+        },
+        {
+            content: "Ensure that the option to keep in the wish list remains ticked.",
+            trigger: "#b2b_wish[checked='True']",
         },
     ],
 });
