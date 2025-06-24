5793dc4ed2a0f4f8245fa99d94bbc66cb5708bb1
Jacques-Etienne Baudoux <je@bcim.be>
2025-06-24 10:31:16 +0200

    [ADD] website_sale_product_public_category_searchpanel

---
+++ website_sale_product_public_category_searchpanel/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product public category Search panel",
+    "category": "e-commerce",
+    "author": "BCIM, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["website_sale"],
+    "data": [
+        "views/product_public_category_views.xml",
+    ],
+    "installable": True,
+}
---
+++ website_sale_product_public_category_searchpanel/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+Add a search panel on the ecommerce category list view to help navigate across
+categories.
