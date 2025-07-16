ac54d65eaa9c79689f208d128020883b73d4cb6d
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-07-16 13:59:03 +0200

    [MIG] website_sale_stock_product_matrix: Migration to version 17.0
    TT57084

--- website_sale_stock_product_matrix/__manifest__.py
+++ website_sale_stock_product_matrix/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock hints in eCommerce product matrix",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/e-commerce",
