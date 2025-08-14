d24ba4d19fb649cf0ac323ef300c2941a0e0690e
sergio <sergio.martinez@studio73.es>
2025-08-14 13:09:53 +0200

    [MIG] website_sale_stock_provisioning_date: Migration to 18.0

--- website_sale_stock_provisioning_date/static/src/js/website_sale_stock_provisioning_date_tour.esm.js
+++ website_sale_stock_provisioning_date/static/src/js/website_sale_stock_provisioning_date_tour.esm.js
@@ -1,23 +1,21 @@
-/** @odoo-module **/
 /* Copyright 2020 Tecnativa - Ernesto Tejeda
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
 import {registry} from "@web/core/registry";
-import tourUtils from "@website_sale/js/tours/tour_utils";
-import wTourUtils from "@website/js/tours/tour_utils";
+import {searchProduct} from "@website_sale/js/tours/tour_utils";
+import {clickOnElement} from "@website/js/tours/tour_utils";
 
 registry.category("web_tour.tours").add("website_sale_stock_provisioning_date", {
     test: true,
     url: "/shop",
     steps: () => [
-        ...tourUtils.searchProduct("provisioning date"),
-        wTourUtils.clickOnElement(
+        ...searchProduct("provisioning date"),
+        clickOnElement(
             "click on product test",
             '.oe_product_cart a:contains("provisioning date")'
         ),
         {
-            trigger: "a#add_to_cart",
-            extra_trigger:
+            trigger:
                 ".availability_messages:has(span:contains('Next provisioning date:'))",
         },
     ],
