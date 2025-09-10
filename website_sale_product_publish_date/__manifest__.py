914140888fa28eb459567f086509f36d9ee74b50
Aungkokolin1997 <lin@quartile.co>
2025-09-10 08:13:26 +0000

    [MIG] website_sale_product_publish_date: Migration to 18.0

--- website_sale_product_publish_date/__manifest__.py
+++ website_sale_product_publish_date/__manifest__.py
@@ -3,8 +3,10 @@
 
 {
     "name": "Website Sale Product Publish Date",
+    "summary": "Control product visibility on the website shop with publish/unpublish "
+    "dates",
     "category": "Website",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/e-commerce",
     "license": "AGPL-3",
@@ -12,5 +14,6 @@
     "data": [
         "views/product_template_views.xml",
     ],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
     "installable": True,
 }
