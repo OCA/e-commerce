b3b90f164c535a8899e7d5c87fd08f4705d35eab
cristina-hidalgo-tecnativa <cristina.hidalgo@tecnativa.com>
2026-03-11 19:55:50 +0100

    [MIG] website_sale_category_breadcrumb: Migration to 18.0

--- website_sale_category_breadcrumb/__manifest__.py
+++ website_sale_category_breadcrumb/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "eCommerce Product Category Breadcrumb",
     "category": "Website",
     "summary": "Displays Product Category Breadcrumb(s) in eCommerce",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Radovan Skolnik, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/e-commerce",
