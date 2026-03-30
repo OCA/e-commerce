001eb0061274535615828295e85725164fd70a44
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2026-03-30 17:27:26 +0200

    [MIG] website_sale_wishlist_hide_price: Continue migration to 17.0
    TT61653

--- website_sale_wishlist_hide_price/static/tests/tours/website_sale_wishlist_hide_price_tour.esm.js
+++ website_sale_wishlist_hide_price/static/tests/tours/website_sale_wishlist_hide_price_tour.esm.js
@@ -1,19 +1,26 @@
-/** @odoo-module */
+/** @odoo-module **/
+
 import {registry} from "@web/core/registry";
 
 registry.category("web_tour.tours").add("website_sale_wishlist_hide_price_tour", {
     test: true,
-    url: "/shop",
+    checkDelay: 250,
+    url: "/shop?search=Customizable Desk",
     steps: () => [
         {
-            trigger: ".oe_product_cart:contains('Customizable') .o_add_wishlist",
+            content: "click on add to wishlist",
+            trigger: ".o_add_wishlist",
         },
         {
-            trigger: "a[href='/shop/wishlist']",
+            content: "go to wishlist",
+            extra_trigger: 'a[href="/shop/wishlist"] .badge:contains(1)',
+            trigger: 'a[href="/shop/wishlist"]',
         },
         {
+            content:
+                "verify that the product 'Customizable Desk' is in the wishlist without the 'Add to Wishlist' button or price displayed",
             trigger:
-                "tr:has(a:contains('Customizable Desk')):not(:has(button.o_wish_add)):not(:has(span.oe_currency_value))",
+                "tr:has(a:contains('Customizable Desk')):not(:has(button.o_wish_add)):not(:has(span.oe_currency_value)):visible",
         },
     ],
 });
