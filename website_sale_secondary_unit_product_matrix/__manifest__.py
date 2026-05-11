ebb6fe50d08acc1ce046ae40be8a876e4f26b65b
sergio-teruel <sergio.teruel@tecnativa.com>
2026-05-11 16:08:03 +0200

    [FIX] website_sale_secondary_unit_product_matrix: allow_uom_sell not injected in product matrix

--- website_sale_secondary_unit_product_matrix/__manifest__.py
+++ website_sale_secondary_unit_product_matrix/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product matrix with secondary units in eCommerce",
-    "version": "17.0.1.0.1",
+    "version": "17.0.1.0.2",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/e-commerce",
@@ -11,6 +11,7 @@
     "depends": [
         "website_sale_product_matrix",
         "sale_product_matrix_secondary_unit",
+        "website_sale_secondary_unit",
     ],
     "data": [
         "templates/product_template.xml",
