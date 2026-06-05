3828ac1380a5d95f6a7b097b0b45cce23b49abff
Pilar Vargas <pilar.vargas@tecnativa.com>
2026-06-05 18:05:09 +0200

    [ADD] website_sale_product_minimal_price_hide_price: New module
    TT62925

---
+++ website_sale_product_minimal_price_hide_price/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Tecnativa - Pilar Vargas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Product Minimal Price Hide Price",
+    "summary": "Hide the product price scale when website prices are hidden",
+    "version": "17.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "website_sale_hide_price",
+        "website_sale_product_minimal_price",
+    ],
+}
