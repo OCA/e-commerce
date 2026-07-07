d2df3ae91aec3566513d0b1c72c4bde0cc77393e
Anmol Garg <anmolgarg12203@gmail.com>
2026-07-07 14:29:44 +0530

    [IMP] website_sale_affiliate: pre-commit auto fixes

--- website_sale_affiliate/models/sale_affiliate_request.py
+++ website_sale_affiliate/models/sale_affiliate_request.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
@@ -9,60 +8,60 @@ from odoo.http import request
 
 
 class AffiliateRequest(models.Model):
-    _name = 'sale.affiliate.request'
-    _order = 'create_date desc'
+    _name = "sale.affiliate.request"
+    _order = "create_date desc"
 
     name = fields.Char(
         required=True,
         index=True,
         help='Name corresponds with the "aff_key" value of the url used, if '
-        'present. Otherwise, name is determined by the sequence selected in '
-        'the parent affiliate record',
+        "present. Otherwise, name is determined by the sequence selected in "
+        "the parent affiliate record",
     )
     affiliate_id = fields.Many2one(
-        'sale.affiliate',
-        string='Affiliate',
+        "sale.affiliate",
+        string="Affiliate",
         required=True,
-        help='Affiliate that referred request',
+        help="Affiliate that referred request",
     )
     date = fields.Datetime(
-        string='Start Date',
+        string="Start Date",
         required=True,
         default=lambda self: fields.Datetime.now(),
-        help='Date and time of initial request',
+        help="Date and time of initial request",
     )
     ip = fields.Char(
-        string='Client IP',
+        string="Client IP",
         required=True,
         default=lambda self: request.httprequest.headers.environ.get(
-            'REMOTE_ADDR',
+            "REMOTE_ADDR",
         ),
     )
     referrer = fields.Char(
         default=lambda self: request.httprequest.headers.environ.get(
-            'HTTP_REFERER',
+            "HTTP_REFERER",
         ),
-        help='Request session referrer header',
+        help="Request session referrer header",
     )
     user_agent = fields.Char(
         required=True,
         default=lambda self: request.httprequest.headers.environ.get(
-            'HTTP_USER_AGENT',
+            "HTTP_USER_AGENT",
         ),
-        help='Request session user agent',
+        help="Request session user agent",
     )
     accept_language = fields.Char(
         required=True,
         default=lambda self: request.httprequest.headers.environ.get(
-            'HTTP_ACCEPT_LANGUAGE',
+            "HTTP_ACCEPT_LANGUAGE",
         ),
-        help='Request session accept language',
+        help="Request session accept language",
     )
     sale_ids = fields.One2many(
-        'sale.order',
-        'affiliate_request_id',
-        string='Sales',
-        help='Qualified conversions generated as a result of affiliate request'
+        "sale.order",
+        "affiliate_request_id",
+        string="Sales",
+        help="Qualified conversions generated as a result of affiliate request",
     )
 
     @api.multi
@@ -83,8 +82,8 @@ class AffiliateRequest(models.Model):
     @api.model_cr_context
     def current_qualified(self):
         try:
-            current_id = request.session['affiliate_request']
-            current = self.search([('id', '=', current_id)], limit=1)
+            current_id = request.session["affiliate_request"]
+            current = self.search([("id", "=", current_id)], limit=1)
         except (KeyError, RuntimeError):
             # KeyError if session exists, but no request
             # RuntimeError if session is non-existent (XML record creations)
